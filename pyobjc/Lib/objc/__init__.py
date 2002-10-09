e184613dbbbcfd2306c39903424248e325e395eb
Ronald Oussoren <ronaldoussoren@mac.com>
2002-10-09 21:07:07 +0000

    Disable gc module when loading the objc module when on Python 2.2.0. At least this allows one to use PyObjC with Apple's python on 10.2

--- pyobjc/Lib/objc/__init__.py
+++ pyobjc/Lib/objc/__init__.py
@@ -4,10 +4,14 @@ new-style pyobjc
 from _objc import *
 from _objc import __version__
 
-# Backward compat stuff, Python 2.2.0 doesn't have 'True' and 'False'
+# Backward compat stuff
 try:
 	getattr(__builtins__, 'True')
 except AttributeError:
+	# Python 2.2.0 doesn't have 'True' and 'False'
+	# It also has a GC that interacts badly with us...
+	import gc
+	gc.disable()
 	True=1
 	False=0
 
