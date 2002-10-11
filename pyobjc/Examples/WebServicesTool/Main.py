ad67992f5974988bd0b48e4cfa9e1b87851634c5
Bill Bumgarner <bbum@mac.com>
2002-10-11 14:53:49 +0000

    Added ability to be run with Apple supplied python.

--- pyobjc/Examples/WebServicesTool/Main.py
+++ pyobjc/Examples/WebServicesTool/Main.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python
+
+import sys
 import objc
+from AppKit import NSApplicationMain
 
 import WSTApplicationDelegateClass
 import WSTConnectionWindowControllerClass
+
+sys.exit(NSApplicationMain(sys.argv))
