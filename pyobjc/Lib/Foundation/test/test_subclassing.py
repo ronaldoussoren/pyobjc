43f5e7fecb139da0ee4454772e4a6f4a6bf047cd
Bill Bumgarner <bbum@mac.com>
2002-10-23 03:35:56 +0000

    Split the Foundation tests appropriately.

--- pyobjc/Lib/Foundation/test/test_subclassing.py
+++ pyobjc/Lib/Foundation/test/test_subclassing.py
@@ -24,25 +24,8 @@ class TestSubclassing( unittest.TestCase ):
         self.assert_( subclassInstance is subclassInstance, "Identity check failed." )
         self.assert_( subclassInstance is subclassInstance.self(), "Identity check failed." )
     
-
-class TestNSObjectInteraction( unittest.TestCase ):
-    def testNSObjectClass( self ):
-        self.assert_( NSObject.instancesRespondToSelector_( "description" ), "NSObject class claims it doesn't respond to a selector that it does." )
-        # self.assert_( NSObject.description(), "NSObject class failed to respond to +description selector." )
-
-    def testNSObjectInstance( self ):
-        instance = NSObject.new()
-
-        self.assert_( instance, "Failed to instantiate an instance" )
-        self.assert_( instance.description(), "NSObject instance didn't respond to -description selector." )
-        self.assert_( not instance.isProxy(), "Instance of NSObject claimed it was a proxy.   That seems odd." )
-        self.assert_( isinstance( instance, objc.runtime.NSObject ), "Instantiated object not an instance of NSObject." )
-        self.assert_( instance == instance, "Python identity check failed." )
-        self.assert_( instance.isEqual_( instance ), "Obj-C identity check failed." )
-        
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest( unittest.makeSuite( TestNSObjectInteraction ) )
     suite.addTest( unittest.makeSuite( TestSubclassing ) )
     return suite
 
