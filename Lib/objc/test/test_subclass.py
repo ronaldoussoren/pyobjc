b528e4041a28885444b32227c2929b5d629d2f21
Bill Bumgarner <bbum@mac.com>
2002-10-30 22:16:54 +0000

    Added a couple of tests

--- Lib/objc/test/test_subclass.py
+++ Lib/objc/test/test_subclass.py
@@ -1,15 +1,12 @@
 import unittest
-
 import objc
 
 # Most usefull systems will at least have 'NSObject'.
 NSObject = objc.lookup_class('NSObject')
 
-
-
 class TestSubclassing(unittest.TestCase):
-
 	def testSubclassOfSubclass(self):
+		import string
 		class Level1Class (NSObject):
 			def hello(self):
 				return "level1"
@@ -18,13 +15,26 @@ class TestSubclassing(unittest.TestCase):
 			def hello(self):
 				return "level2"
 
+			def superHello(self):
+				return super(Level2Class, self).hello()
+
+			def description(self):
+				return super(Level2Class, self).description()
+
 		obj = Level1Class.alloc().init()
 		v = obj.hello()
-		self.assert_(v == "level1")
+		self.assertEquals(v, "level1")
 
 		obj = Level2Class.alloc().init()
 		v = obj.hello()
-		self.assert_(v == "level2")
+		self.assertEquals(v, "level2")
+
+		v = obj.superHello()
+		self.assertEquals(v, "level1")
+
+		v = obj.description()
+		# this may be a bit hardwired for comfort
+		self.assert_( string.index(v, "<Level2Class") == 0 )
 	
 	def testUniqueNames(self):
 		class SomeClass (NSObject): pass
@@ -32,18 +42,14 @@ class TestSubclassing(unittest.TestCase):
 		try:
 			class SomeClass (NSObject): pass
 
-			assert_(0)
+			fail("Should not have been able to redefine the SomeClass class!")
 		except objc.error, msg:
-			assert(str(msg) == "Class already exists in Objective-C runtime")
-
-
-
-	
-
+			self.assertEquals(str(msg), "Class already exists in Objective-C runtime")
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest( unittest.makeSuite( TestInstantiation ) )
+    suite.addTest( unittest.makeSuite( TestSubclassing ) )
+    return suite
 
 if __name__ == '__main__':
     unittest.main( )
