09688c434cfbb492fa6a70c515d71f499e8fd481
Bill Bumgarner <bbum@mac.com>
2002-11-24 04:30:31 +0000

    Refactored intialization code, fixing a couple of bugs and moving a bunch of code out of __main__.py.

--- Examples/WebServicesTool/__main__.py
+++ Examples/WebServicesTool/__main__.py
@@ -1,29 +1,10 @@
 import sys
-import os.path
-
-sys.path.insert(0, os.path.join(sys.path[0], "pyobjc"))
 
 import objc
 import Foundation
 import AppKit
 
-# automatically load frameworks that were linked into the bootstrap tooll
-pyFrameworkPathsIndex = sys.argv.index("-PyFrameworkPaths")
-if not (pyFrameworkPathsIndex == -1):
-  import string
-  from Foundation import NSBundle
-  paths = string.split(sys.argv[pyFrameworkPathsIndex + 1], ":")
-  count = 0
-  for path in paths:
-    bundle = NSBundle.bundleWithPath_(path)
-    bundle.principalClass()
-    sys.path.insert(count, bundle.resourcePath())
-    count = count + 1
-    
-    initPath = bundle.pathForResource_ofType_( "Init", "py")
-    if initPath:
-      execfile(initPath, globals(), locals())
-
+# import classes required to start application
 import WSTApplicationDelegateClass
 import WSTConnectionWindowControllerClass
 
