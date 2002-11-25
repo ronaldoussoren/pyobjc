f33fa3431a9b9c47c1cc01d2da1f3a5673a7708f
Just van Rossum <just@letterror.com>
2002-11-25 15:46:31 +0000

    clarify better why bundlebuilder.py and plistlib.py are here, and when you should install them

--- MPCompat/setup.py
+++ MPCompat/setup.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 """
-A number of usefull modules from MacPython. These are necessary to build
-.app bundles using python scripts (e.g. Examples/TableModel/buildpyapp.py)
+A couple of useful modules from the (Mac)Python CVS tree. They are
+necessary to build .app bundles using python scripts (e.g.
+Examples/TableModel/buildpyapp.py).
 
-Install this only if you do not have a MacPython installation!
+There are two reasons they are included here:
+a) They're not available in Python 2.2
+b) They're not installed by default in a static (non-framework) build
+   of CVS Python. Note: this is (probably/hopefully) going to change
+   before the release of Python 2.3.
+
+So: install these only if either of the above situations apply to you.
 """
 
 from distutils.core import setup
 
-setup (name = "pyobjc-macpython",
-          version = '0.1',
-           description = "PyObjC: Modules borrowed from MacPython",
-           author_email = "pyobjc-dev@lists.sourceforge.net",
-	   url = "http://pyobjc.sourceforge.net/",
-           py_modules = [ 'bundlebuilder', 'plistlib' ],
-           package_dir = { '': '' },
-           )
-
-
+setup(name = "pyobjc-macpython",
+      version = '0.1',
+      description = "PyObjC: Modules borrowed from MacPython",
+      author_email = "pyobjc-dev@lists.sourceforge.net",
+      url = "http://pyobjc.sourceforge.net/",
+      py_modules = ['bundlebuilder', 'plistlib'],
+      package_dir = {'': ''},
+)
