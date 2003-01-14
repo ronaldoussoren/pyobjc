d51ebbd9594a7cb96e2c33e894f9d5c78a5d3ccb
Bill Bumgarner <bbum@mac.com>
2003-01-14 04:49:57 +0000

    forgot to commit unit test that was really written first.

---
+++ Lib/Foundation/test/test_nsenumerator.py
@@ -0,0 +1,25 @@
+import unittest
+import objc
+
+from Foundation import *
+
+class TestNSEnumeratorInteraction( unittest.TestCase ):
+    def setUp(self):
+        self.arrayContainer = NSArray.arrayWithArray_(range(100))
+
+    def testInOperator(self):
+        y = []
+        for x in self.arrayContainer.objectEnumerator():
+            y.append(x)
+
+        self.assertEquals(len(y), len(self.arrayContainer))
+        for i in range(len(y)):
+            self.assertEquals(y[i], self.arrayContainer[i])        
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest( unittest.makeSuite( TestNSEnumeratorInteraction ) )
+    return suite
+
+if __name__ == '__main__':
+    unittest.main( )
