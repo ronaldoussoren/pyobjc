51415f3b6beeb9022a81c3058398c4f596c62eac
Bill Bumgarner <bbum@mac.com>
2003-01-15 00:06:32 +0000

    Added Cocoa-Python Document-based Application template.

---
+++ pyobjc/Project Templates/Cocoa-Python Document-based Application/MyAppDelegate.py
@@ -0,0 +1,19 @@
+#
+#  MyAppDelegate.py
+#  ÇPROJECTNAMEÈ
+#
+#  Created by ÇFULLUSERNAMEÈ on ÇDATEÈ.
+#  Copyright (c) ÇYEARÈ ÇORGANIZATIONNAMEÈ. All rights reserved.
+#
+
+from objc import YES, NO
+
+from AppKit import NibClassBuilder, NSApplicationDelegate
+from AppKit.NibClassBuilder import AutoBaseClass
+
+# create ObjC classes as defined in MainMenu.nib
+NibClassBuilder.extractClasses("MainMenu")
+class MyAppDelegate(AutoBaseClass, NSApplicationDelegate):
+    def applicationShouldOpenUntitledFile_(self, sender):
+        # return NO if you don't want untitled document to be opened on app launch
+        return YES
