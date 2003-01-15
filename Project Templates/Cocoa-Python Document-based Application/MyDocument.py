209579602534e3e0baae253c27be57d0b5390888
Bill Bumgarner <bbum@mac.com>
2003-01-15 00:06:32 +0000

    Added Cocoa-Python Document-based Application template.

---
+++ Project Templates/Cocoa-Python Document-based Application/MyDocument.py
@@ -0,0 +1,42 @@
+#
+#  MyDocument.py
+#  «PROJECTNAME»
+#
+#  Created by «FULLUSERNAME» on «DATE».
+#  Copyright (c) «YEAR» «ORGANIZATIONNAME». All rights reserved.
+#
+
+from objc import YES, NO
+from Foundation import *
+from AppKit import *
+
+from AppKit import NibClassBuilder
+from AppKit.NibClassBuilder import AutoBaseClass
+
+NibClassBuilder.extractClasses("MyDocument")
+class MyDocument(AutoBaseClass):
+    def init(self):
+        self = super(MyDocument, self).init()
+        if self:
+            # subclass specific initialization here
+            # nib not loaded yet
+            pass
+        return self
+
+    def windowNibName(self):
+        # the name of the NIB file containing the document UI
+        return "MyDocument"
+
+    def windowControllerDidLoadNib_(self, aController):
+        super(MyDocument, self).windowControllerDidLoadNib_(aController)
+        # initialize document UI after window controller loads NIB
+
+    def dataRepresentationOfType_(self, aType):
+        # return an NSData containing the document's data represented as
+        # the type identified by aType.
+        return nil
+
+    def loadDataRepresentation_ofType_(self, data, aType):
+        # load document from data (NSData instance) by interpreting
+        # it as the type identified by aType.
+        return YES
