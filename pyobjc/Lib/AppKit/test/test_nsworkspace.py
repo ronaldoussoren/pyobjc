a04be8bbc6345c1eb3fd4952813fc2f21aa5c646
Ronald Oussoren <ronaldoussoren@mac.com>
2003-01-31 10:26:20 +0000

    Expand testsuite

---
+++ pyobjc/Lib/AppKit/test/test_nsworkspace.py
@@ -0,0 +1,31 @@
+import unittest
+import objc
+import os
+
+from objc import YES, NO
+from AppKit import NSWorkspace
+
+
+class TestNSWorkspace(unittest.TestCase):
+    def testInfoForFile(self):
+        ws = NSWorkspace.sharedWorkspace()
+
+        # A method with 2 output parameters, this means the result
+        # is a tuple with 3 elements (return value, param1, param2)
+        res = ws.getInfoForFile_application_type_(
+            os.getcwd())
+        self.assert_(isinstance(res, tuple))
+        self.assert_(len(res) == 3)
+        self.assert_(res[0] == 1)
+        self.assert_(res[1] == '/System/Library/CoreServices/Finder.app')
+        self.assert_(res[2] == '')
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestNSWorkspace))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main( )
+
