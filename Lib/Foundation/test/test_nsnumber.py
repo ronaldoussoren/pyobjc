44cf97ff515b81a801bd7668c5c2b75cb5be151f
Ronald Oussoren <ronaldoussoren@mac.com>
2003-01-31 10:26:20 +0000

    Expand testsuite

--- Lib/Foundation/test/test_nsnumber.py
+++ Lib/Foundation/test/test_nsnumber.py
@@ -15,6 +15,28 @@ class TestNSNumber( unittest.TestCase ):
         self.assertEquals( x[8] * x[7], y[7] * y[8] )
         self.assertEquals( y[8] * x[7], x[7] * y[8] )
 
+    def testAsBool(self):
+        trues = (
+            NSNumber.numberWithFloat_(1.0),
+            NSNumber.numberWithInt_(1),
+            NSNumber.numberWithFloat_(-0.01),
+            NSNumber.numberWithInt_(-4),
+        )
+        falses = (
+            NSNumber.numberWithFloat_(0.0),
+            NSNumber.numberWithInt_(0),
+        )
+
+        for a in trues:
+            if a:
+                pass
+            else:
+                raise AssertionError, "%s is not true"%(a.description())
+
+        for a in falses:
+            if a:
+                raise AssertionError, "%s is true"%(a.description())
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest( unittest.makeSuite( TestNSNumber ) )
