cc7d37088f3e87b3c8cb47e9d2ded0d1dd60c9bf
Bill Bumgarner <bbum@mac.com>
2003-02-02 16:38:09 +0000

    testing notifications.

--- Project Templates/Cocoa-Python Document-based Application/MyAppDelegate.py
+++ Project Templates/Cocoa-Python Document-based Application/MyAppDelegate.py
@@ -17,4 +17,3 @@ class MyAppDelegate(AutoBaseClass, NSApplicationDelegate):
     def applicationShouldOpenUntitledFile_(self, sender):
         # return NO if you don't want untitled document to be opened on app launch
         return YES
-
