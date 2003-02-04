24563c262eee8eca5ca727ee0fca0348e3f2f9e8
Bill Bumgarner <bbum@mac.com>
2003-02-04 19:42:54 +0000

    modified as per David's followup.

--- pyobjc/Lib/AppKit/test/test_nscell.py
+++ pyobjc/Lib/AppKit/test/test_nscell.py
@@ -5,32 +5,27 @@ from AppKit import NSCell
 
 class TestNSCell(unittest.TestCase):
    def testString(self):
-      cell = NSCell.alloc().init()
       s = 'string'
-      cell.setStringValue_(s)
+      cell = NSCell.alloc().initTextCell_(s)
       self.assertEquals(cell.stringValue(), s)
-      self.assertEquals(str(cell.stringValue()), s)
 
    def testUnicode(self):
-      cell = NSCell.alloc().init()
       u = u'\xc3\xbc\xc3\xb1\xc3\xae\xc3\xa7\xc3\xb8d\xc3\xa8'
+      cell = NSCell.alloc().initTextCell_(u)
       cell.setStringValue_(u)
       self.assertEquals(cell.stringValue(), u)
-      self.assertEquals(unicode(cell.stringValue()), u)
 
    def testInt(self):
-      cell = NSCell.alloc().init()
       i = 17
+      cell = NSCell.alloc().initTextCell_("")
       cell.setIntValue_(i)
       self.assertEquals(cell.intValue(), i)
-      self.assertEquals(int(cell.intValue()), i)
 
    def testFloat(self):
-      cell = NSCell.alloc().init()
       f = 3.14159
+      cell = NSCell.alloc().initTextCell_("")
       cell.setFloatValue_(f)
       self.assertEquals(cell.floatValue(), f)
-      self.assertEquals(float(cell.floatValue()), f)
 
 def suite():
    suite = unittest.TestSuite()
