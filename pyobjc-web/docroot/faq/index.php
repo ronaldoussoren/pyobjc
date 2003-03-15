ec79f8b7274f3e64b7138466023c977125f5e1c5
Ronald Oussoren <ronaldoussoren@mac.com>
2003-03-15 15:38:05 +0000

    Added most common error to the faq: forgetting to subclass table models from NSTableDataSource.

--- pyobjc-web/docroot/faq/index.php
+++ pyobjc-web/docroot/faq/index.php
@@ -1,13 +1,24 @@
 <?
 $title = "F.A.Q.";
-$cvs_author = '$Author: bbum $';
-$cvs_date = '$Date: 2002/11/12 16:26:30 $';
+$cvs_author = '$Author: ronaldoussoren $';
+$cvs_date = '$Date: 2003/03/15 15:38:05 $';
 
 include "header.inc";
 ?>
 
 <i>A FAQ would be nice, too.  There has to be a way to automatically generate this based on some more usefully structured text/data.  <b>Links at left are broken pending something useful.</b></i>
 
+<DL>
+<DT><B>I use an <code>NSTableView</code> and my program crashes, what's up?</B></DT>
+<DT>
+Your table model should be a subclass of another ObjC class, <B>and</B> of 
+<code>AppKit.NSTableDataSource</code>. This is necessary to enable the bridge
+to deduce the correct method signatures for your methods.
+<P>The same is true of other data-models and informal protocols: You must 
+multiple-inherit from NSObject (or a subclass of that class) and the 
+corresponding protocol pseudo-class.
+</DT>
+</DL>
 <?
 include "footer.inc";
 ?>
