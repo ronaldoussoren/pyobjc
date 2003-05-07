1b7d2d5e83d3041391c01666546b46e494f32bbe
Ronald Oussoren <ronaldoussoren@mac.com>
2003-05-07 19:23:49 +0000

    The links in the navigation box of the Examples section are dead. Remove the links and mention how to find the links when you have installed PyObjC.

--- pyobjc-web/docroot/examples/index.php
+++ pyobjc-web/docroot/examples/index.php
@@ -1,7 +1,7 @@
 <?
 $title = "Documentation";
-$cvs_author = '$Author: bbum $';
-$cvs_date = '$Date: 2002/11/12 07:41:06 $';
+$cvs_author = '$Author: ronaldoussoren $';
+$cvs_date = '$Date: 2003/05/07 19:23:49 $';
 
 include "header.inc";
 ?>
@@ -9,9 +9,11 @@ include "header.inc";
 <h1>Examples</h1>
 
 <p>
-Collected examples for the PyObjC module.  Includes command line tools, examples using Project Builder, and various examples of using the numerous APIs provided by Apple (and, eventually, GnuStep).  <i>Should use source-highlight to dump the html-ized version of the commented source</i>.
-</p>
-
+Collected examples for the PyObjC module.  Includes command line tools, examples using Project Builder, and various examples of using the numerous APIs provided by Apple (and, eventually, GnuStep).
+<p>
+The examples are included in the source tarball and are installed in 
+/Library/Developer/Examples/PyObjC when using the installer package. We will add
+them to this website some time in the future.
 
 <?
 include "footer.inc";
