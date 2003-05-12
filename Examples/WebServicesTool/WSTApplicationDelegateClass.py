fd036e55d0096a29090fab32c752ff4e833d6203
Just van Rossum <just@letterror.com>
2003-05-12 20:33:17 +0000

    - fixed indentation - pythonified somewhat:   - use Python lists and dicts instead of NSMutableArray and -Dictionary   - use classmethod() instead of objc.selector(..., isClassMEthod=1): this     method is called from Python only anyway. (But: it would be nice if     classmethod() would also work to define a "proper" ObjC class method.     Perhaps it doesm but I didn't try, nor do I know _how_ to try ;-)

--- Examples/WebServicesTool/WSTApplicationDelegateClass.py
+++ Examples/WebServicesTool/WSTApplicationDelegateClass.py
@@ -1,7 +1,9 @@
 """
 WSTApplicationDelegateClass
 
-An instance of this class is instantiated in the MainMenu.nib default NIB file.  All outlets and the base class are automatically derived at runtime by the AutoBaseClass mechanism provided by the NibClassBuilder.
+An instance of this class is instantiated in the MainMenu.nib default NIB file.
+All outlets and the base class are automatically derived at runtime by the
+AutoBaseClass mechanism provided by the NibClassBuilder.
 """
 
 from PyObjCTools import NibClassBuilder
@@ -15,22 +17,22 @@ NibClassBuilder.extractClasses( "MainMenu" )
 # NIB file(s) that define the class. 
 class WSTApplicationDelegate(NibClassBuilder.AutoBaseClass):
 
-  def newConnectionAction_(self, sender):
-    """Action method fired when the user selects the 'new connection'
-    menu item.  Note that the WSTConnectionWindowControllerClass is
-    defined the first time this method is invoked.
+    def newConnectionAction_(self, sender):
+        """Action method fired when the user selects the 'new connection'
+        menu item.  Note that the WSTConnectionWindowControllerClass is
+        defined the first time this method is invoked.
 
-    This kind of lazy evaluation is generally recommended;  it speeds
-    app launch time and it ensures that cycles aren't wasted loading
-    functionality that will never be used.
+        This kind of lazy evaluation is generally recommended;  it speeds
+        app launch time and it ensures that cycles aren't wasted loading
+        functionality that will never be used.
 
-    (In this case, it is largely moot due to the implementation of
-    applicationDidFinishLaunching_().
-    """
-    from WSTConnectionWindowControllerClass import WSTConnectionWindowController
-    WSTConnectionWindowController.connectionWindowController().showWindow_(sender)
+        (In this case, it is largely moot due to the implementation of
+        applicationDidFinishLaunching_().
+        """
+        from WSTConnectionWindowControllerClass import WSTConnectionWindowController
+        WSTConnectionWindowController.connectionWindowController().showWindow_(sender)
 
-  def applicationDidFinishLaunching_(self, aNotification):
-    """Create and display a new connection window
-    """
-    self.newConnectionAction_(None)
+    def applicationDidFinishLaunching_(self, aNotification):
+        """Create and display a new connection window
+        """
+        self.newConnectionAction_(None)
