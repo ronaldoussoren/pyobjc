79cdc6f2696d8cf0dcd751691dbe1313c96ecc3e
Just van Rossum <just@letterror.com>
2003-05-13 13:23:06 +0000

    more notes, hard-wrapped readme

--- pyobjc/Examples/WebServicesTool/WSTConnectionWindowControllerClass.py
+++ pyobjc/Examples/WebServicesTool/WSTConnectionWindowControllerClass.py
@@ -279,6 +279,7 @@ class WSTConnectionWindowController(NibClassBuilder.AutoBaseClass,
         """
         if not aMessage:
             aMessage = "Displaying information about %d methods." % len(self._methods)
+        # All UI calls should be directed to the main thread
         self.statusTextField.performSelectorOnMainThread_withObject_waitUntilDone_(
             "setStringValue:", aMessage, 0)
     
