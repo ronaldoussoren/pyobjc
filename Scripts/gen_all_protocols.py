6c1e6fb27c27e35c584dac0fbebf4fd0964c6c1e
Just van Rossum <just@letterror.com>
2003-06-08 15:53:32 +0000

    - added gen_all_protocols.py script, which generate protocols for the frameworks that PyObjC ships wrappers for. (Except PreferencePanes, as it doesn't seem to define any protocols.)
    - fixed gen_protocols.py so it also handles selectors defined over
    multiple lines in the header file.

---
+++ Scripts/gen_all_protocols.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""Use this script to generate protocol definitions for the frameworks as
+shipped by PyObjC.
+"""
+
+import sys
+import os
+from gen_protocols import genProtocols
+
+script = os.path.abspath(sys.argv[0])
+libdir = os.path.join(os.path.dirname(os.path.dirname(script)), "Lib")
+
+for framework in ["AppKit", "Foundation", "AddressBook", "InterfaceBuilder"]:
+    path = "/System/Library/Frameworks/%s.framework" % framework
+    protfile = file(os.path.join(libdir, framework, "protocols.py"), "w")
+    print "generating protocols for", framework
+    genProtocols(path, protfile)
