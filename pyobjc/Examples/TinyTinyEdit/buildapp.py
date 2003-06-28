d34798c8ee9682013533b7e0f2481f4e04964813
Just van Rossum <just@letterror.com>
2003-06-28 19:59:45 +0000

    new doc-based example

---
+++ pyobjc/Examples/TinyTinyEdit/buildapp.py
@@ -0,0 +1,22 @@
+from bundlebuilder import buildapp
+from plistlib import Plist, Dict
+
+
+plist = Plist(
+    CFBundleDocumentTypes = [
+        Dict(
+            CFBundleTypeExtensions = ["txt", "text", "*"],
+            CFBundleTypeName = "Text File",
+            CFBundleTypeRole = "Editor",
+            NSDocumentClass = "TinyTinyDocument",
+        ),
+    ]
+)
+
+
+buildapp(
+	mainprogram = "TinyTinyEdit.py",
+	resources = ["MainMenu.nib", "TinyTinyDocument.nib"],
+	nibname = "MainMenu",
+    plist = plist,
+)
