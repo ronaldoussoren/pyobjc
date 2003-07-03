a255ba9f8ce2fdaeeb9bcec2e9645bf1209acf63
Ronald Oussoren <ronaldoussoren@mac.com>
2003-07-03 20:40:43 +0000

    Don't clobber the cvs snapshots.

--- pyobjc-web/deploy.sh
+++ pyobjc-web/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # We don't use '-a', because we don't want to muck with permissions on the
 # server.
-rsync --progress -C -e ssh --delete  -v -z -rltgoD docroot/ "${1:-bbum}@shell.sourceforge.net:/home/groups/p/py/pyobjc/htdocs/"
+rsync --progress -C -e ssh --delete  -v -z -rltgoD --exclude 'cvs-snapshots' docroot/ "${1:-bbum}@shell.sourceforge.net:/home/groups/p/py/pyobjc/htdocs/"
 
