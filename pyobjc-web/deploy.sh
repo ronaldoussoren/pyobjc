729a290ac6dde4ae173dcbd79bafd0c616270c11
Ronald Oussoren <ronaldoussoren@mac.com>
2003-07-05 15:10:10 +0000

    Buglet in the deploy script: the files/directories on the server should be group-writable, otherwise the other maintainers cannot upload changed documentation.

--- pyobjc-web/deploy.sh
+++ pyobjc-web/deploy.sh
@@ -3,3 +3,7 @@
 # server.
 rsync --progress -C -e ssh --delete  -v -z -rltgoD --exclude 'cvs-snapshots' docroot/ "${1:-bbum}@shell.sourceforge.net:/home/groups/p/py/pyobjc/htdocs/"
 
+# Permissions are hosed anyway, fix them up afterwards
+# The redirection of stderr was added to ignore error messages about not 
+# being able to change the permissions of files owned by others
+ssh "${1:-bbum}@shell.sourceforge.net" "chmod -R g+w /home/groups/p/py/pyobjc/htdocs  2>/dev/null"
