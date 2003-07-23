8a778847111ddfc5c4fa49e5497769ae9194d2b2
Ronald Oussoren <ronaldoussoren@mac.com>
2003-07-23 20:58:03 +0000

    ReST-ified NEWS, generate NEWS.html

--- Scripts/update_html.py
+++ Scripts/update_html.py
@@ -18,11 +18,11 @@ PYTHON=sys.executable
 
 def rest2HTML(irrelevant, dirName, names):
     for aName in names:
-        if aName.endswith('.txt'):
+        if aName.endswith('.txt') or aName == 'NEWS':
             anInputPath = os.path.join(dirName, aName)
             if irrelevant is not None and anInputPath in irrelevant:
                 continue
-            anOutputPath = anInputPath[:-4] + '.html'
+            anOutputPath = os.path.splitext(anInputPath)[0] + '.html'
             print '- %s'%(anInputPath)
             os.system("docarticle.py '%s' > '%s' || rm '%s'"%(
                 escquotes(anInputPath), escquotes(anOutputPath), 
@@ -33,5 +33,5 @@ def escquotes(val):
 
 print "Generateing HTML documentation"
 os.path.walk('Doc', rest2HTML, ['Doc/announcement.txt'])
-rest2HTML(None, '.', ['Install.txt', 'ReadMe.txt', 'Examples/00ReadMe.txt', 'Installer Package/ReadMe.txt', 'ProjectBuilder Extras/Project Templates/00README.txt'], )
+rest2HTML(None, '.', ['NEWS', 'Install.txt', 'ReadMe.txt', 'Examples/00ReadMe.txt', 'Installer Package/ReadMe.txt', 'ProjectBuilder Extras/Project Templates/00README.txt'], )
 os.rename('ProjectBuilder Extras/Project Templates/00README.html', 'Doc/ProjectBuilder-Templates.html')
