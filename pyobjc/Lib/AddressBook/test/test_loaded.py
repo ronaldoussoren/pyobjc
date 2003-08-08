ce573263b06f902d9a3dbb1e8be4d8fa8a39178b
Ronald Oussoren <ronaldoussoren@mac.com>
2003-08-08 11:48:21 +0000

    Test that the addressbook framework can be used.

---
+++ pyobjc/Lib/AddressBook/test/test_loaded.py
@@ -0,0 +1,28 @@
+"""
+AddressBook doesn't add 'interesting' behaviour, just check that the 
+module loaded correctly.
+"""
+
+import unittest
+import AddressBook
+import objc
+
+class ABTest (unittest.TestCase):
+
+    def testConstants(self):
+        # Test one string and one integer, to check if the constant-extraction
+        # script worked.
+        self.assert_(hasattr(AddressBook, 'kABMultiDictionaryProperty'))
+        self.assert_(hasattr(AddressBook, 'kABPhoneMainLabel'))
+
+        self.assertEquals(AddressBook.kABMultiDictionaryProperty, 262)
+        self.assertEquals(AddressBook.kABPhoneMainLabel, '_$!<Main>!$_')
+
+    def testClasses(self):
+        # Check that we loaded the AddressBook framework by looking for a
+        # class that should exist
+        self.assert_(hasattr(AddressBook, 'ABPerson'))
+        self.assert_(isinstance(AddressBook.ABPerson, objc.objc_class))
+
+if __name__ == "__main__":
+    unittest.main()
