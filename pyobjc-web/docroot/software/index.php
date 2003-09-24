9ec5c287c169441cd436bf1b50e59e04c7763ca1
Ronald Oussoren <ronaldoussoren@mac.com>
2003-09-24 11:13:37 +0000

    Fix link to the latest libFFI snapshot.

--- pyobjc-web/docroot/software/index.php
+++ pyobjc-web/docroot/software/index.php
@@ -1,7 +1,7 @@
 <?
 $title = "Documentation";
 $cvs_author = '$Author: ronaldoussoren $';
-$cvs_date = '$Date: 2003/07/05 14:59:47 $';
+$cvs_date = '$Date: 2003/09/24 11:13:37 $';
 
 include "header.inc";
 ?>
@@ -16,7 +16,7 @@ include "header.inc";
 <p>Older releases are available in the <a href="http://sourceforge.net/project/showfiles.php?group_id=14534">Files section of our SourceForge project</a>.
 
 <p>If you want to install PyObjC from CVS you'll have to download the latest 
-archive with libFFI <a href="http://prdownloads.sourceforge.net/pyobjc/gcc-libffi-snapshot20030119-patched.tar.gz?download">here</a>. This is not an official libffi release, it contains a (small) number of changes to make it work correctly with PyObjC.
+archive with libFFI <a href="http://prdownloads.sourceforge.net/pyobjc/libffi-src-20030921.tar.gz?download">here</a>. This is not an official libffi release, it contains a (small) number of changes to make it work correctly with PyObjC.
 
 <p>You can also download a snapshot of the CVS <a href="/cvs-snapshots">here</a>.
 
