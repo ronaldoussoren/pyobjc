d31bc46cc20161c526b911d8c85f01b79083759b
Bob Ippolito <bob@redivi.com>
2004-01-17 19:31:17 +0000

    (formatting only) detabbed, removed end of line whitespace and \ continuation

--- Examples/Todo/SelectionNotifyMatrix.py
+++ Examples/Todo/SelectionNotifyMatrix.py
@@ -6,22 +6,22 @@ RowSelectedNotification = "RowSelectedNotification"
 
 class  SelectionNotifyMatrix (AutoBaseClass):
     def mouseDown_(self, theEvent):
-    	super(SelectionNotifyMatrix, self).mouseDown_(theEvent)
+        super(SelectionNotifyMatrix, self).mouseDown_(theEvent)
 
-    	row = self.selectedRow()
-    	#print "mouseDown_", theEvent, row
-    	if row != -1:
-    		NSNotificationCenter.defaultCenter(
-    			).postNotificationName_object_userInfo_(
-    				RowSelectedNotification,
-    				self,
-    				None)
+        row = self.selectedRow()
+        #print "mouseDown_", theEvent, row
+        if row != -1:
+            NSNotificationCenter.defaultCenter(
+                ).postNotificationName_object_userInfo_(
+                    RowSelectedNotification,
+                    self,
+                    None)
 
     def selectCellAtRow_column_(self, row, col):
-    	super(SelectionNotifyMatrix, self).selectCellAtRow_column_(row, col)
+        super(SelectionNotifyMatrix, self).selectCellAtRow_column_(row, col)
 
-    	NSNotificationCenter.defaultCenter(
-    		).postNotificationName_object_userInfo_(
-    			RowSelectedNotification,
-    			self,
-    			None)
+        NSNotificationCenter.defaultCenter(
+            ).postNotificationName_object_userInfo_(
+                RowSelectedNotification,
+                self,
+                None)
--- Examples/Todo/buildapp.py
+++ Examples/Todo/buildapp.py
@@ -3,7 +3,7 @@
 #
 import os
 
-from bundlebuilder import buildapp 
+from bundlebuilder import buildapp
 from plistlib import Plist, Dict
     
 images = [ os.path.join('Images', fn) for fn in os.listdir('Images') if fn.lower().endswith('.tiff') ]
@@ -12,16 +12,16 @@ src = [ fn for fn in os.listdir('.') if fn.endswith('.py') and fn not in ('main.
 
 infoPlist = Plist(
     NSHelpFile='',
-    CFBundleShortVersionString='To Do v1', 
+    CFBundleShortVersionString='To Do v1',
     CFBundleSignature='????',
     CFBundleInfoDictionaryVersion='6.0',
     CFBundleIconFile='ToDoApp.icns',
     NSMainNibFile='MainMenu',
     CFBundleGetInfoString='To Do v1',
-    CFBundleIdentifier='com.apple.ToDo', 
+    CFBundleIdentifier='com.apple.ToDo',
     CFBundleDocumentTypes=[
         Dict(
-            CFBundleTypeName='To Do list', 
+            CFBundleTypeName='To Do list',
             CFBundleTypeRole='Editor',
             NSDocumentClass='ToDoDocument',
             CFBundleTypeIconFile='ToDoDoc.icns',
@@ -37,10 +37,10 @@ infoPlist = Plist(
     CFBundleVersion='0.1')
 
 buildapp(
-    name = "ToDo", 
+    name = "ToDo",
     mainprogram = "main.py",
     resources = ["English.lproj" ] + images + icons + src,
     nibname = "MainMenu",
     #plist = Plist.fromFile('Info.plist'),
     plist = infoPlist
-)   
+)
