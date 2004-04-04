9cd0cca4ea0a2429073054474c65f83b954eb3b2
Bob Ippolito <bob@redivi.com>
2004-04-04 08:08:34 +0000

    More cleaning up

--- Examples/CocoaBindings/TableModel/TableModelAppDelegate.py
+++ Examples/CocoaBindings/TableModel/TableModelAppDelegate.py
@@ -1,26 +1,18 @@
-#
-#  TableModelAppDelegate.py
-#  TableModel
-#
-#  Created by Bob Ippolito on Sun Apr 04 2004.
-#  Copyright (c) 2004 Bob Ippolito. All rights reserved.
-#
-
 from Foundation import NSLog
 from PyObjCTools import NibClassBuilder
 import os
 
+FIELDS = "name password uid gid class change expire gecos home_dir shell".split()
+def getPasswords():
+    return [
+        dict(zip(FIELDS, line.rstrip().split(':')))
+        for line in os.popen('/usr/bin/nidump passwd .')
+        if not line.startswith('#')
+    ]
+    
 NibClassBuilder.extractClasses("MainMenu")
 class TableModelAppDelegate(NibClassBuilder.AutoBaseClass):
-    def applicationDidFinishLaunching_(self, aNotification):
-        NSLog( "Application did finish launching." )
-        
     def passwords(self):
         if not hasattr(self, '_cachedpasswords'):
-            FIELDS = "name password uid gid class change expire gecos home_dir shell".split()
-            self._cachedpasswords = [
-                dict(zip(FIELDS, line.rstrip().split(':')))
-                for line in os.popen('/usr/bin/nidump passwd .')
-                if not line.startswith('#')
-            ]
+            self._cachedpasswords = getPasswords()
         return self._cachedpasswords
