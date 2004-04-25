9caf85e1c8b9b136cd3210222e0b54c490fa16b2
Ronald Oussoren <ronaldoussoren@mac.com>
2004-04-25 19:03:06 +0000

    - Run a spell-checker - Replace subclass-objective-c.py by a doctest - Remove using-nscoder.py, it is a lame example and was actually only used   as a test.

--- pyobjc/Examples/subclassing-objective-c.py
+++ pyobjc/Examples/subclassing-objective-c.py
@@ -1,52 +1,62 @@
-#
-# Subclassing an objective-C class from python.
-#
-# This shows that we can override a method in an objective-C class, and have
-# it called from objective-C. 
-#
-import sys
-import objc
+#!/usr/bin/env python
+# This is a doctest
+"""
+=========================================
+Subclassing Objective-C classes in Python
+=========================================
 
-NSEnumerator = objc.lookUpClass('NSEnumerator')
+It is possible to subclass any existing Objective-C class in python.
 
-class Demo (NSEnumerator):
-    __slots__ = ('cnt',)
+We start by importing the interface to the Objective-C runtime, although
+you'd normally use wrappers for the various frameworks, and then locate
+the class we'd like to subclass::
 
-    def init(self):
-        self.cnt = 10
-        return self
+    >>> import objc
+    >>> NSEnumerator = objc.runtime.NSEnumerator
+    >>> NSEnumerator
+    <objective-c class NSEnumerator at 0xa0a039a8>
 
-    def nextObject(self):
-        print "nextObject" ,  self.retainCount()
-        if self.cnt == 0:
-            return None
-        self.cnt -= 1
-        return self.cnt
+You can then define a subclass of this class using the usual syntax::
 
-    def __del__(self):
-        print "Bye from Demo instance"
+    >>> class MyEnumerator (NSEnumerator):
+    ...    __slots__ = ('cnt',)
+    ...    # 
+    ...    # Start of the method definitions:
+    ...    def init(self):
+    ...        self.cnt = 10
+    ...        return self
+    ...    #
+    ...    def nextObject(self):
+    ...        if self.cnt == 0:
+    ...            return None
+    ...        self.cnt -= 1
+    ...        return self.cnt
+    ...    #
+    ...    def __del__(self):
+    ...        global DEALLOC_COUNT
+    ...        DEALLOC_COUNT = DEALLOC_COUNT + 1
 
-print Demo.alloc
+    >>> MyEnumerator
+    <objective-c class MyEnumerator at 0x350fb0>
 
-x = Demo.alloc().init()
-print x.retainCount()
-del x
+To check that our instances our deallocated we maintain a ``DEALLOC_COUNT``::
 
-obj = Demo.alloc()
-print "->", obj.retainCount();
+    >>> DEALLOC_COUNT=0
 
-obj = obj.init()
-print "->", obj.retainCount();
-print "next->", obj.nextObject();
-print "->", obj.retainCount();
+As always, the creation of instances of Objective-C classes looks a bit odd
+for Python programs:
 
-x = obj.allObjects()
-print '->', obj.retainCount()
-print "all->", x.description()
-x=None
+    >>> obj = MyEnumerator.alloc().init()
+    >>> obj.allObjects()
+    (9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
-print "->", obj.retainCount()
-obj = None
-print "Done"
+Destroy our reference to the object, to check if it will be deallocated::
 
-objc.recycleAutoreleasePool()
+   >>> del obj
+   >>> DEALLOC_COUNT
+   1
+
+"""
+import doctest
+import __main__
+doctest.testmod(__main__)
