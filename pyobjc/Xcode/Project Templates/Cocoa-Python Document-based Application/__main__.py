fde567595a07fcfcf24ceaa16dd86a4eb1baf869
Ronald Oussoren <ronaldoussoren@mac.com>
2004-05-09 20:23:42 +0000

    Add a document-based application template, based on the plain application template and TinyTinyEdit.

---
+++ pyobjc/Xcode/Project Templates/Cocoa-Python Document-based Application/__main__.py
@@ -0,0 +1,24 @@
+#
+#  __main__.py
+#  ÇPROJECTNAMEÈ
+#
+#  Created by ÇFULLUSERNAMEÈ on ÇDATEÈ.
+#  Copyright (c) ÇYEARÈ ÇORGANIZATIONNAMEÈ. All rights reserved.
+#
+
+try:
+    # scan for pth files that made it into the bundle
+    import os, site
+    site.addsitedir(os.path.dirname(os.path.realpath(__file__)))
+except ImportError:
+    pass
+
+from PyObjCTools import AppHelper
+from Foundation import NSProcessInfo
+
+# import classes required to start application
+#import ÇPROJECTNAMEASIDENTIFIERÈAppDelegate
+import ÇPROJECTNAMEASIDENTIFIERÈDocument
+
+# start the event loop
+AppHelper.runEventLoop(argv=[])
