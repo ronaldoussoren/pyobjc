60f8d65d54764f7966162ec856106b34173dd8c8
Bob Ippolito <bob@redivi.com>
2004-05-22 22:53:52 +0000

    loaded test for ExceptionHandling framework

---
+++ Lib/ExceptionHandling/test/test_loaded.py
@@ -0,0 +1,21 @@
+"""
+ExceptionHandling doesn't add 'interesting' behaviour, just check that the 
+module loaded correctly.
+"""
+
+import unittest
+import objc
+
+class EHTest (unittest.TestCase):
+
+
+    def testClasses(self):
+        import ExceptionHandling
+
+        # Check that we loaded the PreferencePanes framework by looking for a
+        # class that should exist
+        self.assert_(hasattr(ExceptionHandling, 'NSExceptionHandler'))
+        self.assert_(isinstance(ExceptionHandling.NSExceptionHandler, objc.objc_class))
+
+if __name__ == "__main__":
+    unittest.main()
