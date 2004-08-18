d87bc085cc5cfe48d785de30b22392ce3473876e
Ronald Oussoren <ronaldoussoren@mac.com>
2004-08-18 18:36:42 +0000

    Apply patch 986395: buildapp.py doesn't include all required modules

--- Examples/PyDocURLProtocol/buildapp.py
+++ Examples/PyDocURLProtocol/buildapp.py
@@ -2,6 +2,7 @@ from bundlebuilder import buildapp
 
 buildapp(
         mainprogram = "PyDocBrowser.py",
-        resources = ["PyDocBrowser.nib"],
+        resources = ["PyDocBrowser.nib", 
+                     "PyDocURLProtocol.py", "pydochelper.py"],
         nibname = "PyDocBrowser",
 )
