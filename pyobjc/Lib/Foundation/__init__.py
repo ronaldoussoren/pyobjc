6191292abdc91f09cee2497793d20fdfdce3d79d
Bob Ippolito <bob@redivi.com>
2004-09-13 05:55:55 +0000

    Remove autoGIL reference from Foundation

--- pyobjc/Lib/Foundation/__init__.py
+++ pyobjc/Lib/Foundation/__init__.py
@@ -35,19 +35,6 @@ def _initialize():
             if initPath:
                 execfile(initPath, globals(), locals())
 
-    # Install an observer callback in the current CFRunLoop that will
-    # automatically release and acquire the Global Interpreter Lock
-    # when needed. This is needed so other Python threads get a chance
-    # to run while we're inside the event loop.
-    #
-    # The autoGIL module is only present when using Python 2.2 on MacOS X
-    if sys.version_info[:2] == (2, 2):
-        try:
-            import autoGIL
-        except ImportError:
-            pass
-        else:
-            autoGIL.installAutoGIL()
 _initialize()
 
 import protocols  # no need to export these, just register with PyObjC
