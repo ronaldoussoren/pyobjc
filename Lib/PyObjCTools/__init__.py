125ebe6540aa89651efbf7311f3aa6abd683eb40
Bob Ippolito <bob@redivi.com>
2004-12-07 06:50:50 +0000

    py2app and documentation changes

--- Lib/PyObjCTools/__init__.py
+++ Lib/PyObjCTools/__init__.py
@@ -3,9 +3,10 @@ Various utility modules
 
 This package provides a number of useful utility modules.
 
-Conversion      -- Tools for converting between Python and Objective-C objects.
+Conversion      -- Tools for converting between Python and Objective-C objects
 NibClassBuilder -- Tools for working with class definitions in
     "Next Interface Builder" files ("nibs")
 Signals         -- Signal handling functions that are useful during development
-pluginbuilder   -- bundlebuilder extension for build plugin bundles in Python
+MachSignals     -- Integrate signal handling into the main runloop
+Debugging       -- Log stack traces of NSExceptions, useful during development
 """
