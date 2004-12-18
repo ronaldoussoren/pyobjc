ac41e08afd2ec043217a903f7ab9ba2eaaf90cf9
Bob Ippolito <bob@redivi.com>
2004-12-18 04:32:50 +0000

    I don't think anyone plans on working on this..

--- Examples/NonFunctional/Twisted/Packman/buildapp.py
+++
@@ -1,9 +0,0 @@
-from bundlebuilder import buildapp
-
-buildapp(
-    mainprogram = "PackMan.py",
-    resources = ["MainMenu.nib"],
-    includeModules=['newclient', 'newpimp'],
-    symlink = True,
-    nibname = "MainMenu"
-)
