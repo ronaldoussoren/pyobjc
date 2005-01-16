c8ab10b9e6044b4da7af7d76af8bd5c2d0f01d89
Bob Ippolito <bob@redivi.com>
2005-01-16 16:42:31 +0000

    add the console remote interpreter (no GUI integration yet)

---
+++ Examples/NonFunctional/RemotePyInterpreter/remote_pipe.py
@@ -0,0 +1,82 @@
+import itertools
+
+def as_unicode(s):
+    typ = type(s)
+    if typ is not unicode:
+        if issubclass(typ, unicode):
+            s = unicode(s)
+    elif issubclass(typ, str):
+        s = unicode(s, 'utf-8', 'replace')
+    else:
+        raise TypeError, 'expecting basestring, not %s' % (typ.__name__,)
+
+class RemotePipe(object):
+    def __init__(self, runcode, clientfile, netrepr, namespace, pool):
+        self.runcode = runcode
+        self.pool = pool
+        self.clientfile = clientfile
+        self.namespace = namespace
+        self.result = self.namespace['__result__'] = {}
+        self.netrepr = netrepr
+        self.sequence = itertools.count()
+        self.stdin = RemoteFileLike(self, 'stdin')
+        self.stdout = RemoteFileLike(self, 'stdout')
+        self.stderr = RemoteFileLike(self, 'stderr')
+
+    def send(self, *args):
+        self.clientfile.write(repr(map(self.netrepr, args)) + '\n')
+        self.clientfile.flush()
+
+    def expect(self, *args):
+        self.pool.push()
+        try:
+            return self._expect(*args)
+        finally:
+            self.pool.pop()
+
+    def _expect(self, *args):
+        ident = self.sequence.next()
+        self.send('expect', ident, *args)
+        while ident not in self.result:
+            self.runcode(self.clientfile, self.namespace)
+        return self.result.pop(ident)
+
+
+class RemoteFileLike(object):
+    softspace = 0
+    closed = False
+    def __init__(self, pipe, ident):
+        self.pipe = pipe
+        self.ident = ident
+
+    def __iter__(self):
+        while True:
+            rval = self.readline()
+            if not rval:
+                break
+            yield rval
+
+    def write(self, s):
+        self.pipe.expect('RemoteFileLike.write', self.ident, s)
+
+    def writelines(self, lines):
+        for line in lines:
+            self.write(line)
+    
+    def close(self):
+        self.closed = True
+
+    def flush(self):
+        pass
+
+    def isatty(self):
+        return True
+
+    def read(self, size=-1):
+        return self.pipe.expect('RemoteFileLike.read', self.ident, size)
+
+    def readline(self, size=-1):
+        return self.pipe.expect('RemoteFileLike.readline', self.ident, size)
+
+    def readlines(self):
+        return list(self)
