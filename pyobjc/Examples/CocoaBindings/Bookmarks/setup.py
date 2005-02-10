98954584b4d7fa903d6e48f8bc763ef4d4fc9bf6
Bob Ippolito <bob@redivi.com>
2005-02-10 21:15:23 +0000

    the Bookmarks example, demonstrating drag & drop... from u.fiedler originally in mmalc's examples

---
+++ pyobjc/Examples/CocoaBindings/Bookmarks/setup.py
@@ -0,0 +1,27 @@
+"""
+Script for building the example:
+
+Usage:
+    python setup.py py2app
+"""
+from distutils.core import setup
+import py2app
+
+plist = dict(
+    CFBundleDocumentTypes = [
+        dict(
+            CFBundleTypeExtensions=[u'Bookmarks', u'*'],
+            CFBundleTypeName=u'Bookmarks File',
+            CFBundleTypeRole=u'Editor',
+            NSDocumentClass=u'BookmarksDocument',
+        ),
+    ],
+)
+
+setup(
+    app=["Bookmarks.py"],
+    data_files=["English.lproj"],
+    options=dict(py2app=dict(
+        plist=plist,
+    )),
+)
