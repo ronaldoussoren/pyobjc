5fd9467897d1a77bf8791d66e2ec98079d4bf4a3
Bob Ippolito <bob@redivi.com>
2005-02-10 20:42:26 +0000

    ToDos Example converted from Malcolms Binding Examples.
    from u.fielder

---
+++ pyobjc/Examples/CocoaBindings/ToDos/ToDosDocument.py
@@ -0,0 +1,52 @@
+#
+#  ToDosDocument.py
+#  ToDos
+#
+#  Converted by u.fiedler on 09.02.05.
+#
+#  The original version was written in Objective-C by Malcolm Crawford
+#  at http://homepage.mac.com/mmalc/CocoaExamples/controllers.html
+
+import objc
+from PyObjCTools import NibClassBuilder
+from Foundation import *
+
+from Category import Category
+from PriorityToColourTransformer import PriorityToColourTransformer
+from OverdueTransformer import OverdueTransformer
+
+NibClassBuilder.extractClasses("ToDosDocument")
+
+class ToDosDocument(NibClassBuilder.AutoBaseClass):
+    # the actual base class is NSDocument
+    # The following outlets are added to the class: nix
+    toDos = objc.ivar('toDos')
+
+    def init(self):
+        self = super(ToDosDocument, self).init()
+        if self is None:
+            return None
+        self.toDos = NSMutableArray.array()
+        return self # if this line is missing you will get the 
+                    # simple message: "Can't create new document"
+
+    def categories(self):
+        return Category.allCategories()
+
+    def windowNibName(self):
+        return u"ToDosDocument"
+
+    def dataRepresentationOfType_(self, aType):
+        return NSKeyedArchiver.archivedDataWithRootObject_(self.toDos)
+
+    def loadDataRepresentation_ofType_(self, data, aType):
+        self.toDos = NSKeyedUnarchiver.unarchiveObjectWithData_(data)
+        return True
+
+priorityTransformer = PriorityToColourTransformer.alloc().init()
+NSValueTransformer.setValueTransformer_forName_(
+    priorityTransformer, u"PriorityToColourTransformer")
+
+overdueTransformer = OverdueTransformer.alloc().init()
+NSValueTransformer.setValueTransformer_forName_(
+    overdueTransformer, u"OverdueTransformer")
