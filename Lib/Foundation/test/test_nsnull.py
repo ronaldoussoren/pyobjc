b16be6a3c7b47d8be9adb479f8f1e5b64ed04164
Bob Ippolito <bob@redivi.com>
2005-03-01 04:04:41 +0000

    assert NSNull.null() is not None

--- Lib/Foundation/test/test_nsnull.py
+++ Lib/Foundation/test/test_nsnull.py
@@ -5,6 +5,7 @@ class TestNSNull (unittest.TestCase):
     def testBool(self):
         v = NSNull.null()
         self.assert_(not v)
+        self.assert_(v is not None)
 
 if __name__ == "__main__":
     unittest.main()
