f3e59f0c504e0a725b423b97359a3a6b9313802b
Ronald Oussoren <ronaldoussoren@mac.com>
2005-05-01 19:48:10 +0000

    - NSDocument.initWithType_error_ has an output argument - Add a CoreData example: python version of Apple's OutlineEdit   TODO: create the .mom file, I've added the compiled version from the   ObjC edition of the example.

---
+++ Examples/CoreData/OutlineEdit/setup.py
@@ -0,0 +1,48 @@
+"""
+Script for building the example.
+
+Usage:
+        python setup.py py2app
+"""
+from distutils.core import setup
+import py2app
+
+plist = dict(
+    NSMainNibFile="MainMenu",
+    CFBundleDocumentTypes=[
+        dict(
+            CFBundleTypeExtensions=['binary'],
+            CFBundleTypeMIMETypes=['application/octect-stream'],
+            CFBundleTypeName='Binary',
+            CFBundleTypeRole='Editor',
+            LSTypeIsPackage=False,
+            NSDocumentClass='MyDocument',
+            NSPersistentStoreTypeKey='Binary',
+        ),
+        dict(
+            CFBundleTypeExtensions=['sql'],
+            CFBundleTypeMIMETypes=['application/octect-stream'],
+            CFBundleTypeName='SQL',
+            CFBundleTypeRole='Editor',
+            LSTypeIsPackage=False,
+            NSDocumentClass='MyDocument',
+            NSPersistentStoreTypeKey='SQLite',
+        ),
+        dict(
+            CFBundleTypeExtensions=['xml'],
+            CFBundleTypeMIMETypes=['text/xml'],
+            CFBundleTypeName='XML',
+            CFBundleOSTypes=['????'],
+            CFBundleTypeRole='Editor',
+            LSTypeIsPackage=False,
+            NSDocumentClass='MyDocument',
+            NSPersistentStoreTypeKey='XML',
+        ),
+    ],
+)
+
+setup(
+    app=["main.py"],
+    data_files=["English.lproj", 'MyDocument.mom'],
+    options=dict(py2app=dict(plist=plist)),
+)
