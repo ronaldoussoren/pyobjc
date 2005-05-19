0386a68e41cf4acac2c88fc2c52a8a55ce0bace1
Bob Ippolito <bob@redivi.com>
2005-05-19 08:09:18 +0000

    Fix plugin bug, should be done as a "1.3.6" release.. doh

--- Examples/Plugins/ProgressViewPalette/ProgressCell.py
+++ Examples/Plugins/ProgressViewPalette/ProgressCell.py
@@ -7,15 +7,14 @@ class ProgressCell (NSCell):
     """
     # float   percentageIncrement;
     # float   percentage;
-    # NSView *controlView;
     # NSColor *color;
-    # int     tag;
 
     def init(self):
         self = super(ProgressCell, self).init()
-        if self is None: return None
-
+        if self is None:
+            return None
         self._color = NSColor.redColor()
+        self._controlView = None
         self._percentageIncrement = 5.0
         self._percentage = 0.0
         self._tag = 0
@@ -46,19 +45,17 @@ class ProgressCell (NSCell):
         NSColor.controlDarkShadowColor().set()
         NSFrameRect(cellFrame)
 
+    def tag(self):
+        return self._tag
+
+    def setTag_(self, value):
+        self._tag = value
+
     def controlView(self):
         return self._controlView
 
-    def setControlView_(self, view):
-        self._controlView = view
-
-    def tag(self):
-        return self._tag
-    tag = objc.selector(tag, signature='i@:')
-
-    def setTag_(self, tag):
-        self._tag = tag
-    setTag_ = objc.selector(setTag_, signature='v@:i')
+    def setControlView_(self, value):
+        self._controlView = value
 
     def percentageIncrement(self):
         return self._percentageIncrement
@@ -70,37 +67,37 @@ class ProgressCell (NSCell):
         return self._percentage
 
     def setPercentage_(self, value):
-        if value > 100: value = 100
-        if value < 0: value = 0
+        value = max(0, min(value, 100))
 
         if self._percentage != value:
             self._percentage = value
-            self.controlView.updateCellInside_(self)
+            self.controlView().updateCellInside_(self)
 
     def color(self):
         return self._color
 
-    def setColor(self, value):
+    def setColor_(self, value):
         self._color = value
-        self.controlView.updateCellInside_(self)
+        self.controlView().updateCellInside_(self)
 
     def increment_(self, sender):
         self.setPercentage_(self.percentage() + self.percentageIncrement())
 
     def initWithCoder_(self, coder):
         self = super(ProgressCell, self).initWithCoder_(coder)
-        if self is None: return None
+        if self is None:
+            return None
 
         if coder.allowsKeyedCoding():
             self._percentageIncrement = coder.decodeFloatForKey_(
                     "percentageIncrement")
             self._percentage = coder.decodeFloatForKey_("percentage")
-            self._tag = coder.decodeFloatForKey_("tag")
+            self.setTag_(coder.decodeFloatForKey_("tag"))
             self._color = coder.decodeObjectForKey_("color")
         else:
             self._percentageIncrement = coder.decodeValueOfObjCType_at_("f")
             self._percentage = coder.decodeValueOfObjCType_at_("f")
-            self._tag = coder.decodeValueOfObjCType_at_("f")
+            self.setTag_(coder.decodeValueOfObjCType_at_("i"))
             self._color = coder.decodeObject()
         return self;
 
@@ -109,12 +106,12 @@ class ProgressCell (NSCell):
 
         if (coder.allowsKeyedCoding()):
             coder.encodeFloat_forKey_(
-                    self._percentageIncrement, "percentageIncrement")
-            coder.encodeFloat_forKey_(self._percentage, "percentage")
-            coder.encodeFloat_forKey_(self._tag, "tag")
-            coder.encodeObject_forKey_(self._color, "color")
+                    self.percentageIncrement(), "percentageIncrement")
+            coder.encodeFloat_forKey_(self.percentage(), "percentage")
+            coder.encodeFloat_forKey_(self.tag(), "tag")
+            coder.encodeObject_forKey_(self.color(), "color")
         else:
-            coder.encodeValueOfObjCType_at_("f", self._percentageIncrement)
-            coder.encodeValueOfObjCType_at_("f", self._percentage)
-            coder.encodeValueOfObjCType_at_("f", self._tag)
-            coder.encodeObject_(self._color)
+            coder.encodeValueOfObjCType_at_("f", self.percentageIncrement())
+            coder.encodeValueOfObjCType_at_("f", self.percentage())
+            coder.encodeValueOfObjCType_at_("i", self.tag())
+            coder.encodeObject_(self.color())
