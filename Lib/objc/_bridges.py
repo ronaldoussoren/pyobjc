8be9c2036d81e9e3ea8b952bc6516c12aa6cb24e
Ronald Oussoren <ronaldoussoren@mac.com>
2006-05-25 13:30:14 +0000

    Attempt to bridge more types

--- Lib/objc/_bridges.py
+++ Lib/objc/_bridges.py
@@ -7,7 +7,7 @@ BRIDGED_TYPES = []
 
 try:
     from array import array
-    from Carbon.File import FSRef
+    from Carbon.File import FSRef, FSSpec
 except ImportError:
     pass
 else:
@@ -18,6 +18,14 @@ else:
     def FSRef_to_struct(fsRef):
         return (tuple(array('B').fromstring(fsRef.data)),)
     BRIDGED_TYPES.append((FSRef, FSRef_to_struct))
+
+    def struct_to_FSRef(s):
+        return FSRef(rawdata=array('B', s[0]).tostring())
+    BRIDGED_STRUCTURES['{FSRef=[80C]}'] = struct_to_FSRef
+
+    def FSSpec_to_struct(fsSpec):
+        return (tuple(array('B').fromstring(fsSpec.data)),)
+    BRIDGED_TYPES.append((FSSpec, FSSpec_to_struct))
 
 def _bridgePythonTypes():
     # python TO Obj-C
