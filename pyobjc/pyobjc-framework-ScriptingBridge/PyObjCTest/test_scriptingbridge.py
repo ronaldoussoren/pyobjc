fbcadc408d9445f0723d7840ad9fd67be095b6c7
Ronald Oussoren <ronaldoussoren@mac.com>
2009-01-22 12:31:21 +0000

    Full API test coverage on Leopard

--- pyobjc/pyobjc-framework-ScriptingBridge/PyObjCTest/test_scriptingbridge.py
+++ pyobjc/pyobjc-framework-ScriptingBridge/PyObjCTest/test_scriptingbridge.py
@@ -1,22 +1,14 @@
-'''
-Some simple tests to check that the framework is properly wrapped.
-'''
-import objc
-import unittest
-import ScriptingBridge
 
-class TestScriptingBridge (unittest.TestCase):
-    def testClasses(self):
-        self.assert_( hasattr(ScriptingBridge, 'SBApplication') )
-        self.assert_( isinstance(ScriptingBridge.SBApplication, objc.objc_class) )
+from PyObjCTools.TestSupport import *
+from ScriptingBridge import *
+
+class TestSBApplication (TestCase):
+    def testMethods(self):
+        self.failUnlessResultIsBOOL(SBApplication.isRunning)
+
     def testProtocols(self):
-        self.assert_( hasattr(ScriptingBridge, 'protocols') )
-
-        self.assert_( hasattr(ScriptingBridge.protocols, 'SBApplicationDelegate') )
-        self.assert_( isinstance(ScriptingBridge.protocols.SBApplicationDelegate, objc.informal_protocol) )
-
+        self.failUnlessIsInstance(protocols.SBApplicationDelegate, objc.informal_protocol)
 
 
 if __name__ == "__main__":
-    unittest.main()
-
+    main()
--- pyobjc/pyobjc-framework-ScriptingBridge/setup.py
+++ pyobjc/pyobjc-framework-ScriptingBridge/setup.py
@@ -18,7 +18,7 @@ except ImportError:
 
 setup(
     name='pyobjc-framework-ScriptingBridge',
-    version='2.0',
+    version='2.2b1',
     description = "Wrappers for the framework ScriptingBridge on Mac OS X",
     long_description = __doc__,
     author='Ronald Oussoren',
