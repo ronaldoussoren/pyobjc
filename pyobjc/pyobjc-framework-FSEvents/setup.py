af877de588426573a18c08b265a24b8bdfeb830e
Ronald Oussoren <ronaldoussoren@mac.com>
2009-03-08 16:43:43 +0000

    Update metadata

--- pyobjc/pyobjc-framework-FSEvents/setup.py
+++ pyobjc/pyobjc-framework-FSEvents/setup.py
@@ -30,8 +30,8 @@ setup(
     setup_requires = [ 
     ],
     install_requires = [ 
-        'pyobjc-core>=2.0',
-        'pyobjc-framework-Cocoa>=2.0',
+        'pyobjc-core>=2.2b1',
+        'pyobjc-framework-Cocoa>=2.2b1',
     ],
     dependency_links = [],
     package_data = { 
