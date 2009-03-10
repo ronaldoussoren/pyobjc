e2062429140e33ff0e407c46d70e69316796d3b1
Ronald Oussoren <ronaldoussoren@mac.com>
2009-03-10 19:10:50 +0000

    Add modules with useful functionality. I might move AppCategory to the AppKit wrapper instead.

---
+++ pyobjc-framework-Cocoa/Lib/PyObjCTools/AppCategories.py
@@ -0,0 +1,20 @@
+"""
+A number of usefull categories on AppKit classes
+"""
+__all__ = ()
+import objc
+from AppKit import  NSGraphicsContext
+
+class _ctxHelper(object):
+    def __enter__(self):
+        NSGraphicsContext.saveGraphicsState()
+
+    def __exit__(self, exc_type, exc_value, exc_tp):
+        NSGraphicsContext.restoreGraphicsState()
+        return False
+
+
+class NSGraphicsContext (objc.Category(NSGraphicsContext)):
+    @classmethod
+    def savedGraphicsState(self):
+        return _ctxHelper()
