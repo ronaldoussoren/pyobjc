310bf1bfda20d69b596ad0b01e624a8f79ddd18f
Ronald Oussoren <ronaldoussoren@mac.com>
2009-03-22 17:28:21 +0000

    Remove NibClassBuilder usage

--- pyobjc-framework-Cocoa/Examples/AppKit/PyObjCLauncher/MyDocument.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/PyObjCLauncher/MyDocument.py
@@ -5,7 +5,20 @@ import os
 from FileSettings import *
 from doscript import doscript
 
-class MyDocument(NibClassBuilder.AutoBaseClass):
+class MyDocument(NSDocument):
+    commandline = objc.IBOutlet()
+    debug = objc.IBOutlet()
+    honourhashbang = objc.IBOutlet()
+    inspect = objc.IBOutlet()
+    interpreter = objc.IBOutlet()
+    nosite = objc.IBOutlet()
+    optimize = objc.IBOutlet()
+    others = objc.IBOutlet()
+    scriptargs = objc.IBOutlet()
+    tabs = objc.IBOutlet()
+    verbose = objc.IBOutlet()
+    with_terminal = objc.IBOutlet()
+
     def init(self):
         self = super(MyDocument, self).init()
         if self is not None:
@@ -25,7 +38,7 @@ class MyDocument(NibClassBuilder.AutoBaseClass):
     def load_defaults(self):
         self.settings = FileSettings.newSettingsForFileType_(self.filetype)
 
-    def update_display(self):
+    def updateDisplay(self):
         dct = self.settings.fileSettingsAsDict()
         self.interpreter.setStringValue_(dct['interpreter'])
         self.honourhashbang.setState_(dct['honourhashbang'])
@@ -57,7 +70,7 @@ class MyDocument(NibClassBuilder.AutoBaseClass):
     def windowControllerDidLoadNib_(self, aController):
         super(MyDocument, self).windowControllerDidLoadNib_(aController)
         self.load_defaults()
-        self.update_display()
+        self.updateDisplay()
 
     def dataRepresentationOfType_(self, aType):
         return None
@@ -68,33 +81,37 @@ class MyDocument(NibClassBuilder.AutoBaseClass):
         self.filetype = typ
         self.settings = FileSettings.newSettingsForFileType_(typ)
         if show_ui:
-            self.update_display()
+            self.updateDisplay()
             return True
         else:
             self.run()
             self.close()
             return False
 
+    @objc.IBAction
     def doRun_(self, sender):
         self.update_settings()
-        self.update_display()
+        self.updateDisplay()
         if self.run():
             self.close()
 
+    @objc.IBAction
     def doCancel_(self, sender):
         self.close()
 
+    @objc.IBAction
     def doReset_(self, sender):
         self.settings.reset()
-        self.update_display()
+        self.updateDisplay()
 
+    @objc.IBAction
     def doApply_(self, sender):
         self.update_settings()
-        self.update_display()
+        self.updateDisplay()
 
     def controlTextDidChange_(self, aNotification):
         self.update_settings()
-        self.update_display()
+        self.updateDisplay()
 
     def fileSettingsAsDict(self):
         return dict(
