720ca3685ec7ff707573004706433d75043c50c5
Ronald Oussoren <ronaldoussoren@mac.com>
2009-05-02 08:29:46 +0000

    * Minor tweaks to setup.py * Improve svn:ignore settings

--- pyobjc/pyobjc-framework-ExceptionHandling/setup.py
+++ pyobjc/pyobjc-framework-ExceptionHandling/setup.py
@@ -1,5 +1,10 @@
 ''' 
-Wrappers for framework 'ExceptionHandling'. 
+Wrappers for the "ExceptionHandling" framework on MacOSX. The ExceptionHandling
+framework provides facilities for monitoring and debugging exceptional 
+conditions in Cocoa programs. 
+
+PyObjC also provides low-level debugging utilities beyond the core 
+ExceptionHandling framework in the module ``PyObjCTools.Debugging``.
 
 These wrappers don't include documentation, please check Apple's documention
 for information on how to use this framework and PyObjC's documentation
@@ -28,13 +33,10 @@ setup(
     packages = [ "PyObjCTools", "ExceptionHandling" ],
     namespace_packages = [ "PyObjCTools" ],
     package_dir = { '': 'Lib' },
-    setup_requires = [ 
-    ],
     install_requires = [ 
-        'pyobjc-core>=2.2b1',
-        'pyobjc-framework-Cocoa>=2.2b1',
+        'pyobjc-core>=2.2b2',
+        'pyobjc-framework-Cocoa>=2.2b2',
     ],
-    dependency_links = [],
     package_data = { 
         '': ['*.bridgesupport'] 
     },
