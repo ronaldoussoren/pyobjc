4952ee147a5ef815af197982a54982eca2785df6
Ronald Oussoren <ronaldoussoren@mac.com>
2009-05-03 07:13:00 +0000

    add trivial API Notes document, small tweaks to setup.py

--- pyobjc/pyobjc-framework-InstallerPlugins/setup.py
+++ pyobjc/pyobjc-framework-InstallerPlugins/setup.py
@@ -1,5 +1,8 @@
 ''' 
-Wrappers for framework 'InstallerPlugins'. 
+Wrappers for the "InstallerPlugins" framework on MacOSX. This framework 
+allows you to develop plugin's for the "Installer.app" application, and those
+make it possible to add new functionality to ".pkg" and ".mpkg" installers
+on MacOSX.
 
 These wrappers don't include documentation, please check Apple's documention
 for information on how to use this framework and PyObjC's documentation
@@ -27,13 +30,10 @@ setup(
     platforms = [ "MacOS X" ],
     packages = [ "InstallerPlugins" ],
     package_dir = { '': 'Lib' },
-    setup_requires = [ 
-    ],
     install_requires = [ 
-        'pyobjc-core>=2.2b1',
-        'pyobjc-framework-Cocoa>=2.2b1',
+        'pyobjc-core>=2.2b2',
+        'pyobjc-framework-Cocoa>=2.2b2',
     ],
-    dependency_links = [],
     package_data = { 
         '': ['*.bridgesupport'] 
     },
