8e95f8f1f6ae3cbca5ca1d665faab7bcc3814bf7
Ronald Oussoren <ronaldoussoren@mac.com>
2009-05-03 16:38:01 +0000

    add trivial API Notes document, small tweaks to setup.py

--- pyobjc/pyobjc-framework-PreferencePanes/setup.py
+++ pyobjc/pyobjc-framework-PreferencePanes/setup.py
@@ -1,5 +1,6 @@
 ''' 
-Wrappers for framework 'PreferencePanes'. 
+Wrappers for the "PreferencePanes" framework on MacOSX. This framework allows
+you to write Preference Panes for the "System Preferences" application.
 
 These wrappers don't include documentation, please check Apple's documention
 for information on how to use this framework and PyObjC's documentation
@@ -28,16 +29,14 @@ setup(
     packages = [ "PreferencePanes" ],
     package_dir = { '': 'Lib' },
     install_requires = [ 
-        'pyobjc-core>=2.2b1',
-        'pyobjc-framework-Cocoa>=2.2b1',
+        'pyobjc-core>=2.2b2',
+        'pyobjc-framework-Cocoa>=2.2b2',
     ],
-    dependency_links = [],
     package_data = { 
         '': ['*.bridgesupport'] 
     },
     cmdclass = extra_cmdclass,
     options = extra_options('PreferencePanes'),
-
     test_suite='PyObjCTest',
     zip_safe=True,
 )
