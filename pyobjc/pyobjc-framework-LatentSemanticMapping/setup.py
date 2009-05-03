72b89c095855290ac7244d6b8df797f7a51f885b
Ronald Oussoren <ronaldoussoren@mac.com>
2009-05-03 07:28:03 +0000

    small tweaks to setup.py

--- pyobjc/pyobjc-framework-LatentSemanticMapping/setup.py
+++ pyobjc/pyobjc-framework-LatentSemanticMapping/setup.py
@@ -1,10 +1,15 @@
 ''' 
-Wrappers for framework 'LatentSemanticMapping'. 
+Wrappers for the framework "LatentSemanticMapping" on MacOSX 10.5 or later.
+
+The Latent Semantic Mapping framework supports the classification of text and other token-based content into developer-defined categories.
 
 These wrappers don't include documentation, please check Apple's documention
 for information on how to use this framework and PyObjC's documentation
 for general tips and tricks regarding the translation between Python
-and (Objective-)C frameworks
+and (Objective-)C frameworks.  
+
+NOTE: Apple's documentation for this framework is very minimal at the moment,
+making it very hard to actually use the framework.
 '''
 import ez_setup
 ez_setup.use_setuptools()
@@ -27,13 +32,10 @@ setup(
     platforms = [ "MacOS X" ],
     packages = [ "LatentSemanticMapping" ],
     package_dir = { '': 'Lib' },
-    setup_requires = [ 
-    ],
     install_requires = [ 
         'pyobjc-core>=2.0',
         'pyobjc-framework-Cocoa>=2.0',
     ],
-    dependency_links = [],
     package_data = { 
         '': ['*.bridgesupport'] 
     },
