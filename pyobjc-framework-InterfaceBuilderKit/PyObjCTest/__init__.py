38b8c4c8f70e44420fb915a22081ff1316e29a78
Ronald Oussoren <ronaldoussoren@mac.com>
2009-09-01 18:56:32 +0000

    Port to SnowLeopard

--- pyobjc-framework-InterfaceBuilderKit/PyObjCTest/__init__.py
+++ pyobjc-framework-InterfaceBuilderKit/PyObjCTest/__init__.py
@@ -2,7 +2,7 @@
 import os
 
 if 'DYLD_FRAMEWORK_PATH' not in os.environ or \
-    '/Developer/Library/PrivateFrameworks/' not in os.environ['DYLD_FRAMEWORK_PATH']: 
+    'Library/PrivateFrameworks/' not in os.environ['DYLD_FRAMEWORK_PATH']: 
         # Check for:
         # DYLD_FRAMEWORK_PATH='/Developer/Library/PrivateFrameworks/'
 
