5caf9665fe347380c134de7b852b56f3cae131f3
Ronald Oussoren <ronaldoussoren@mac.com>
2009-09-29 19:58:20 +0000

    bump version

--- pyobjc/pyobjc-framework-QTKit/setup.py
+++ pyobjc/pyobjc-framework-QTKit/setup.py
@@ -21,7 +21,7 @@ except ImportError:
 
 setup(
     name='pyobjc-framework-QTKit',
-    version='2.2b3',
+    version='2.2b4',
     description = "Wrappers for the framework QTKit on Mac OS X",
     long_description = __doc__,
     author='Ronald Oussoren',
