f15e33a4b3a489e1fc7ae6b2b61f6493bafb4cc0
Ronald Oussoren <ronaldoussoren@mac.com>
2009-09-29 15:27:55 +0000

    bump version

--- pyobjc-framework-ExceptionHandling/setup.py
+++ pyobjc-framework-ExceptionHandling/setup.py
@@ -23,7 +23,7 @@ except ImportError:
 
 setup(
     name='pyobjc-framework-ExceptionHandling',
-    version='2.2b3',
+    version='2.2b4',
     description = "Wrappers for the framework ExceptionHandling on Mac OS X",
     long_description = __doc__,
     author='Ronald Oussoren',
