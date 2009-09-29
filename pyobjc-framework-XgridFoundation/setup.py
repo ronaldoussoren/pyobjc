d48a15b82e7f5b207ca6c7e9daa32c588d513f72
Ronald Oussoren <ronaldoussoren@mac.com>
2009-09-29 20:07:30 +0000

    bump version

--- pyobjc-framework-XgridFoundation/setup.py
+++ pyobjc-framework-XgridFoundation/setup.py
@@ -20,7 +20,7 @@ except ImportError:
 
 setup(
     name='pyobjc-framework-XgridFoundation',
-    version='2.2b3',
+    version='2.2b4',
     description = "Wrappers for the framework XgridFoundation on Mac OS X",
     long_description = __doc__,
     author='Ronald Oussoren',
@@ -30,8 +30,8 @@ setup(
     packages = [ "XgridFoundation" ],
     package_dir = { '': 'Lib' },
     install_requires = [ 
-        'pyobjc-core>=2.2b3',
-        'pyobjc-framework-Cocoa>=2.2b3',
+        'pyobjc-core>=2.2b4',
+        'pyobjc-framework-Cocoa>=2.2b4',
     ],
     package_data = { 
         '': ['*.bridgesupport'] 
