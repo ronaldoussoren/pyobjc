84c02e684c57123cf47a157d289a91daba6c19b9
Ronald Oussoren <ronaldoussoren@mac.com>
2009-09-29 19:59:22 +0000

    bump version

--- pyobjc/pyobjc-framework-ScreenSaver/setup.py
+++ pyobjc/pyobjc-framework-ScreenSaver/setup.py
@@ -19,7 +19,7 @@ except ImportError:
 
 setup(
     name='pyobjc-framework-ScreenSaver',
-    version='2.2b3',
+    version='2.2b4',
     description = "Wrappers for the framework ScreenSaver on Mac OS X",
     long_description = __doc__,
     author='Ronald Oussoren',
