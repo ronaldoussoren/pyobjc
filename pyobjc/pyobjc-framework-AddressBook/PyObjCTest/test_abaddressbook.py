62f3bf34ceff94a18b81e228f0e6bc9016d0cf14
Ronald Oussoren <ronaldoussoren@mac.com>
2009-10-13 12:08:54 +0000

    Ensure tests pass on 10.4

--- pyobjc/pyobjc-framework-AddressBook/PyObjCTest/test_abaddressbook.py
+++ pyobjc/pyobjc-framework-AddressBook/PyObjCTest/test_abaddressbook.py
@@ -5,9 +5,12 @@ from AddressBook import *
 class TestABAddressBook (TestCase):
     def testMethods(self):
         self.failUnlessResultIsBOOL(ABAddressBook.save)
+        self.failUnlessResultIsBOOL(ABAddressBook.hasUnsavedChanges)
+
+    @min_os_level('10.5')
+    def testMethods10_5(self):
         self.failUnlessResultIsBOOL(ABAddressBook.saveAndReturnError_)
         self.failUnlessArgIsOut(ABAddressBook.saveAndReturnError_, 0)
-        self.failUnlessResultIsBOOL(ABAddressBook.hasUnsavedChanges)
 
 if __name__ == "__main__":
     main()
