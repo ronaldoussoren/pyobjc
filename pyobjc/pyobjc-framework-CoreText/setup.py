49e2e8f21e344919606ece13f4bb7425e72e9ca4
Ronald Oussoren <ronaldoussoren@mac.com>
2009-11-24 15:04:39 +0000

    Bump version to 2.2

--- pyobjc/pyobjc-framework-CoreText/setup.py
+++ pyobjc/pyobjc-framework-CoreText/setup.py
@@ -13,7 +13,7 @@ from pyobjc_setup import setup, Extension
 setup(
     min_os_level='10.5',
     name='pyobjc-framework-CoreText',
-    version='2.2b4',
+    version='2.2',
     description = "Wrappers for the framework CoreText on Mac OS X",
     packages = [ "CoreText" ],
     ext_modules = [
@@ -23,8 +23,8 @@ setup(
             ),
     ],
     install_requires = [ 
-        'pyobjc-core>=2.2b4',
-        'pyobjc-framework-Cocoa>=2.2b4',
-        'pyobjc-framework-Quartz>=2.2b4',
+        'pyobjc-core>=2.2',
+        'pyobjc-framework-Cocoa>=2.2',
+        'pyobjc-framework-Quartz>=2.2',
     ],
 )
