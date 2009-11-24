eaf897ca82dd6224f8b634440ff9e6f10591c77b
Ronald Oussoren <ronaldoussoren@mac.com>
2009-11-24 14:42:53 +0000

    Bump version to 2.2

--- pyobjc/pyobjc-framework-AddressBook/setup.py
+++ pyobjc/pyobjc-framework-AddressBook/setup.py
@@ -13,11 +13,11 @@ from pyobjc_setup import setup
 
 setup(
     name='pyobjc-framework-AddressBook',
-    version="2.2b4",
+    version="2.2",
     description = "Wrappers for the framework AddressBook on Mac OS X",
     packages = [ "AddressBook" ],
     install_requires = [ 
-        'pyobjc-core>=2.2b4',
-        'pyobjc-framework-Cocoa>=2.2b4',
+        'pyobjc-core>=2.2',
+        'pyobjc-framework-Cocoa>=2.2',
     ],
 )
