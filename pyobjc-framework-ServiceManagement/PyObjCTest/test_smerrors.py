17c30ff517784ad6f439a32bd463f5ee05974619
Ronald Oussoren <ronaldoussoren@mac.com>
2010-04-05 09:38:24 +0000

    Port to py3k

--- pyobjc-framework-ServiceManagement/PyObjCTest/test_smerrors.py
+++ pyobjc-framework-ServiceManagement/PyObjCTest/test_smerrors.py
@@ -5,18 +5,18 @@ from ServiceManagement import *
 class TestSMErrors (TestCase):
     @min_os_level('10.6')
     def testConstants(self):
-        self.failUnlessIsInstance(kSMErrorDomainIPC, unicode)
-        self.failUnlessIsInstance(kSMErrorDomainFramework, unicode)
-        self.failUnlessIsInstance(kSMErrorDomainLaunchd, unicode)
+        self.assertIsInstance(kSMErrorDomainIPC, unicode)
+        self.assertIsInstance(kSMErrorDomainFramework, unicode)
+        self.assertIsInstance(kSMErrorDomainLaunchd, unicode)
 
-        self.failUnlessEqual(kSMErrorInternalFailure, 2)
-        self.failUnlessEqual(kSMErrorInvalidSignature, 3)
-        self.failUnlessEqual(kSMErrorAuthorizationFailure, 4)
-        self.failUnlessEqual(kSMErrorToolNotValid, 5)
-        self.failUnlessEqual(kSMErrorJobNotFound, 6)
-        self.failUnlessEqual(kSMErrorServiceUnavailable, 7)
-        self.failUnlessEqual(kSMErrorJobPlistNotFound, 8)
-        self.failUnlessEqual(kSMErrorJobMustBeEnabled, 9)
+        self.assertEqual(kSMErrorInternalFailure, 2)
+        self.assertEqual(kSMErrorInvalidSignature, 3)
+        self.assertEqual(kSMErrorAuthorizationFailure, 4)
+        self.assertEqual(kSMErrorToolNotValid, 5)
+        self.assertEqual(kSMErrorJobNotFound, 6)
+        self.assertEqual(kSMErrorServiceUnavailable, 7)
+        self.assertEqual(kSMErrorJobPlistNotFound, 8)
+        self.assertEqual(kSMErrorJobMustBeEnabled, 9)
 
 if __name__ == "__main__":
     main()
