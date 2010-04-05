c58ddba0c16b3963efc2ef2cd9b6326f6df8147c
Ronald Oussoren <ronaldoussoren@mac.com>
2010-04-05 08:38:14 +0000

    Port to py3k

--- pyobjc-framework-LaunchServices/PyObjCTest/test_lsquarantine.py
+++ pyobjc-framework-LaunchServices/PyObjCTest/test_lsquarantine.py
@@ -4,18 +4,18 @@ from LaunchServices import *
 
 class TestLSQuarantine (TestCase):
     def testConstants(self):
-        self.failUnlessIsInstance(kLSQuarantineAgentNameKey, unicode)
-        self.failUnlessIsInstance(kLSQuarantineAgentBundleIdentifierKey, unicode)
-        self.failUnlessIsInstance(kLSQuarantineTimeStampKey, unicode)
-        self.failUnlessIsInstance(kLSQuarantineTypeKey, unicode)
-        self.failUnlessIsInstance(kLSQuarantineTypeWebDownload, unicode)
-        self.failUnlessIsInstance(kLSQuarantineTypeOtherDownload, unicode)
-        self.failUnlessIsInstance(kLSQuarantineTypeEmailAttachment, unicode)
-        self.failUnlessIsInstance(kLSQuarantineTypeInstantMessageAttachment, unicode)
-        self.failUnlessIsInstance(kLSQuarantineTypeCalendarEventAttachment, unicode)
-        self.failUnlessIsInstance(kLSQuarantineTypeOtherAttachment, unicode)
-        self.failUnlessIsInstance(kLSQuarantineOriginURLKey, unicode)
-        self.failUnlessIsInstance(kLSQuarantineDataURLKey, unicode)
+        self.assertIsInstance(kLSQuarantineAgentNameKey, unicode)
+        self.assertIsInstance(kLSQuarantineAgentBundleIdentifierKey, unicode)
+        self.assertIsInstance(kLSQuarantineTimeStampKey, unicode)
+        self.assertIsInstance(kLSQuarantineTypeKey, unicode)
+        self.assertIsInstance(kLSQuarantineTypeWebDownload, unicode)
+        self.assertIsInstance(kLSQuarantineTypeOtherDownload, unicode)
+        self.assertIsInstance(kLSQuarantineTypeEmailAttachment, unicode)
+        self.assertIsInstance(kLSQuarantineTypeInstantMessageAttachment, unicode)
+        self.assertIsInstance(kLSQuarantineTypeCalendarEventAttachment, unicode)
+        self.assertIsInstance(kLSQuarantineTypeOtherAttachment, unicode)
+        self.assertIsInstance(kLSQuarantineOriginURLKey, unicode)
+        self.assertIsInstance(kLSQuarantineDataURLKey, unicode)
 
 if __name__ == "__main__":
     main()
