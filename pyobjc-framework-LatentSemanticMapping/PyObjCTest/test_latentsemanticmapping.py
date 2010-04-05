6667662dc2eb1e0a82a3d7d32a4cca77a0efa592
Ronald Oussoren <ronaldoussoren@mac.com>
2010-04-05 08:30:15 +0000

    Port to py3k

--- pyobjc-framework-LatentSemanticMapping/PyObjCTest/test_latentsemanticmapping.py
+++ pyobjc-framework-LatentSemanticMapping/PyObjCTest/test_latentsemanticmapping.py
@@ -5,41 +5,41 @@ import os
 
 class TestLatentSemanticMapping (TestCase):
     def testConstants(self):
-        self.failUnlessEqual(kLSMMapOutOfState, -6640)
-        self.failUnlessEqual(kLSMMapNoSuchCategory, -6641)
-        self.failUnlessEqual(kLSMMapWriteError, -6642)
-        self.failUnlessEqual(kLSMMapBadPath, -6643)
-        self.failUnlessEqual(kLSMMapBadCluster, -6644)
+        self.assertEqual(kLSMMapOutOfState, -6640)
+        self.assertEqual(kLSMMapNoSuchCategory, -6641)
+        self.assertEqual(kLSMMapWriteError, -6642)
+        self.assertEqual(kLSMMapBadPath, -6643)
+        self.assertEqual(kLSMMapBadCluster, -6644)
 
-        self.failUnlessEqual(kLSMMapPairs, 1)
-        self.failUnlessEqual(kLSMMapTriplets, 2)
-        self.failUnlessEqual(kLSMMapHashText, 256)
+        self.assertEqual(kLSMMapPairs, 1)
+        self.assertEqual(kLSMMapTriplets, 2)
+        self.assertEqual(kLSMMapHashText, 256)
 
-        self.failUnlessEqual(kLSMAlgorithmKey, "LSMAlgorithm")
-        self.failUnlessEqual(kLSMAlgorithmDense, "LSMAlgorithmDense")
-        self.failUnlessEqual(kLSMAlgorithmSparse, "LSMAlgorithmSparse")
-        self.failUnlessEqual(kLSMPrecisionKey, "LSMPrecision")
-        self.failUnlessEqual(kLSMPrecisionFloat, "LSMPrecisionFloat")
-        self.failUnlessEqual(kLSMPrecisionDouble, "LSMPrecisionDouble")
-        self.failUnlessEqual(kLSMDimensionKey, "LSMDimension")
-        self.failUnlessEqual(kLSMIterationsKey, "LSMIterations")
-        self.failUnlessEqual(kLSMSweepAgeKey, "LSMSweepAge")
-        self.failUnlessEqual(kLSMSweepCutoffKey, "LSMSweepCutoff")
+        self.assertEqual(kLSMAlgorithmKey, "LSMAlgorithm")
+        self.assertEqual(kLSMAlgorithmDense, "LSMAlgorithmDense")
+        self.assertEqual(kLSMAlgorithmSparse, "LSMAlgorithmSparse")
+        self.assertEqual(kLSMPrecisionKey, "LSMPrecision")
+        self.assertEqual(kLSMPrecisionFloat, "LSMPrecisionFloat")
+        self.assertEqual(kLSMPrecisionDouble, "LSMPrecisionDouble")
+        self.assertEqual(kLSMDimensionKey, "LSMDimension")
+        self.assertEqual(kLSMIterationsKey, "LSMIterations")
+        self.assertEqual(kLSMSweepAgeKey, "LSMSweepAge")
+        self.assertEqual(kLSMSweepCutoffKey, "LSMSweepCutoff")
 
-        self.failUnlessEqual(kLSMClusterCategories, 0)
-        self.failUnlessEqual(kLSMClusterWords, 1)
-        self.failUnlessEqual(kLSMClusterTokens, 2)
-        self.failUnlessEqual(kLSMClusterKMeans, 0)
-        self.failUnlessEqual(kLSMClusterAgglomerative, 4)
+        self.assertEqual(kLSMClusterCategories, 0)
+        self.assertEqual(kLSMClusterWords, 1)
+        self.assertEqual(kLSMClusterTokens, 2)
+        self.assertEqual(kLSMClusterKMeans, 0)
+        self.assertEqual(kLSMClusterAgglomerative, 4)
 
-        self.failUnlessEqual(kLSMResultBestWords, 1)
+        self.assertEqual(kLSMResultBestWords, 1)
 
-        self.failUnlessEqual(kLSMMapDiscardCounts, 1)
-        self.failUnlessEqual(kLSMMapLoadMutable,  2)
+        self.assertEqual(kLSMMapDiscardCounts, 1)
+        self.assertEqual(kLSMMapLoadMutable,  2)
 
-        self.failUnlessEqual(kLSMTextPreserveCase, 1)
-        self.failUnlessEqual(kLSMTextPreserveAcronyms, 2)
-        self.failUnlessEqual(kLSMTextApplySpamHeuristics, 4)
+        self.assertEqual(kLSMTextPreserveCase, 1)
+        self.assertEqual(kLSMTextPreserveAcronyms, 2)
+        self.assertEqual(kLSMTextApplySpamHeuristics, 4)
 
 
 
@@ -47,97 +47,97 @@ class TestLatentSemanticMapping (TestCase):
 
 
     def testTypes(self):
-        self.failUnlessIsInstance(LSMMapRef, objc.objc_class)
-        self.failUnlessIsInstance(LSMTextRef, objc.objc_class)
-        self.failUnlessIsInstance(LSMResultRef, objc.objc_class)
+        self.assertIsInstance(LSMMapRef, objc.objc_class)
+        self.assertIsInstance(LSMTextRef, objc.objc_class)
+        self.assertIsInstance(LSMResultRef, objc.objc_class)
 
     def testFunctions(self):
-        self.failUnlessIsInstance(LSMMapGetTypeID(), (int, long))
-        self.failUnlessIsInstance(LSMTextGetTypeID(), (int, long))
-        self.failUnlessIsInstance(LSMResultGetTypeID(), (int, long))
+        self.assertIsInstance(LSMMapGetTypeID(), (int, long))
+        self.assertIsInstance(LSMTextGetTypeID(), (int, long))
+        self.assertIsInstance(LSMResultGetTypeID(), (int, long))
 
-        self.failUnlessResultIsCFRetained(LSMMapCreate)
+        self.assertResultIsCFRetained(LSMMapCreate)
         map = LSMMapCreate(None, kLSMMapPairs)
-        self.failUnlessIsInstance(map, LSMMapRef)
+        self.assertIsInstance(map, LSMMapRef)
 
         v = LSMMapGetProperties(map)
-        self.failUnlessIsInstance(v, CFDictionaryRef)
+        self.assertIsInstance(v, CFDictionaryRef)
 
         LSMMapSetProperties(map, {u"key": u"value"})
         v = LSMMapGetProperties(map)
-        self.failUnlessIsInstance(v, CFDictionaryRef)
+        self.assertIsInstance(v, CFDictionaryRef)
 
         v = LSMMapStartTraining(map)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
         cat = LSMMapAddCategory(map)
-        self.failUnlessIsInstance(cat, (int, long))
+        self.assertIsInstance(cat, (int, long))
 
         cat2 = LSMMapAddCategory(map)
-        self.failUnlessIsInstance(cat2, (int, long))
+        self.assertIsInstance(cat2, (int, long))
 
         v = LSMMapGetCategoryCount(map)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
-        self.failUnlessResultIsCFRetained(LSMTextCreate)
+        self.assertResultIsCFRetained(LSMTextCreate)
         text = LSMTextCreate(None, map)
-        self.failUnlessIsInstance(text, LSMTextRef)
+        self.assertIsInstance(text, LSMTextRef)
         text2 = LSMTextCreate(None, map)
-        self.failUnlessIsInstance(text2, LSMTextRef)
+        self.assertIsInstance(text2, LSMTextRef)
         words = LSMTextCreate(None, map)
-        self.failUnlessIsInstance(words, LSMTextRef)
+        self.assertIsInstance(words, LSMTextRef)
 
         v = LSMTextAddWord(words, "the")
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
         v = LSMTextAddWord(words, "and")
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
         v = LSMTextAddWords(text, "the world goes on and on",
                 CFLocaleCopyCurrent(), 0)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
         v = LSMTextAddWords(text2, "on a bright and sunny morning",
                 CFLocaleCopyCurrent(), 0)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
         v = LSMMapSetStopWords(map, words)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
         v = LSMMapAddText(map, text, cat)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
         fn = __file__
         fn = os.path.splitext(fn)[0] + ".py"
-        for line in open(fn, 'rb'):
+        for line in open(fn, 'r'):
             t = LSMTextCreate(None, map)
             LSMTextAddWords(t, line, CFLocaleCopyCurrent(), 0)
             LSMMapAddText(map, t, cat)
 
 
         v = LSMMapAddTextWithWeight(map, text2, cat2, 2.0)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
         v = LSMMapCompile(map)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
         text3 = LSMTextCreate(None, map)
-        self.failUnlessIsInstance(text2, LSMTextRef)
+        self.assertIsInstance(text2, LSMTextRef)
         v = LSMTextAddWords(text3, "foo the bar and worlds away",
                 CFLocaleCopyCurrent(), 0)
 
         if 0:
-            self.failUnlessResultIsCFRetained(LSMMapCreateClusters)
+            self.assertResultIsCFRetained(LSMMapCreateClusters)
             clusters = LSMMapCreateClusters(None, map, None, 3,  0)
-            self.failUnlessIsInstance(clusters, CFArrayRef)
+            self.assertIsInstance(clusters, CFArrayRef)
 
             v = LSMMapApplyClusters(map, clusters);
-            self.failUnlessIsInstance(clusters, (int, long))
+            self.assertIsInstance(clusters, (int, long))
 
         v = LSMMapStartTraining(map)
 
         mytext = LSMTextCreate(None, map)
         v = LSMTextAddWords(mytext, "the world goes on and on",
                                 CFLocaleCopyCurrent(), 0)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
         v = LSMMapCompile(map)
 
@@ -146,29 +146,29 @@ class TestLatentSemanticMapping (TestCase):
         # framework :-(
         return
 
-        self.failUnlessResultIsCFRetained(LSMResultCreate)
+        self.assertResultIsCFRetained(LSMResultCreate)
         res = LSMResultCreate(None, map, mytext, 2, 0)
-        self.failUnlessIsInstance(res, LSMResultRef)
+        self.assertIsInstance(res, LSMResultRef)
 
         v = LSMResultGetCount(res);
-        self.failUnlessIsInstance(v, (int, long))
-        self.failUnless(v)
+        self.assertIsInstance(v, (int, long))
+        self.assertTrue(v)
 
 
         v = LSMResultGetCategory(res, 0)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
         v = LSMResultGetScore(res, 0)
-        self.failUnlessIsInstance(v, float)
+        self.assertIsInstance(v, float)
 
 
-        self.failUnlessResultIsCFRetained(LSMResultCopyWord)
+        self.assertResultIsCFRetained(LSMResultCopyWord)
         v = LSMResultCopyWord(res, 0)
-        self.failUnlessIsInstance(v, unicode)
+        self.assertIsInstance(v, unicode)
 
-        self.failUnlessResultIsCFRetained(LSMResultCopyToken)
+        self.assertResultIsCFRetained(LSMResultCopyToken)
         v = LSMResultCopyToken(res, 0)
-        self.failUnlessIsInstance(v, CFDataRef)
+        self.assertIsInstance(v, CFDataRef)
 
         v = LSMMapStartTraining(map)
         mytext = LSMTextCreate(None, map)
@@ -176,35 +176,35 @@ class TestLatentSemanticMapping (TestCase):
 
 
         v = LSMTextAddToken(mytext, v)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
-        self.failUnlessResultIsCFRetained(LSMResultCopyWordCluster)
+        self.assertResultIsCFRetained(LSMResultCopyWordCluster)
         v = LSMResultCopyWordCluster(res, 0)
-        self.failUnlessIsInstance(v, CFArrayRef)
+        self.assertIsInstance(v, CFArrayRef)
 
-        self.failUnlessResultIsCFRetained(LSMResultCopyTokenCluster)
+        self.assertResultIsCFRetained(LSMResultCopyTokenCluster)
         v = LSMResultCopyTokenCluster(res, 0)
-        self.failUnlessIsInstance(v, CFArrayRef)
+        self.assertIsInstance(v, CFArrayRef)
 
         fn = '/tmp/pyobjc.test'
         url = CFURLCreateWithFileSystemPath(None, fn, kCFURLPOSIXPathStyle, False)
-        self.failUnlessIsInstance(url, CFURLRef)
+        self.assertIsInstance(url, CFURLRef)
         v = LSMMapWriteToURL(map, url, 0)
-        self.failUnlessIsInstance(v, (int, long))
+        self.assertIsInstance(v, (int, long))
 
-        self.failUnlessResultIsCFRetained(LSMMapCreateFromURL)
+        self.assertResultIsCFRetained(LSMMapCreateFromURL)
         map2 = LSMMapCreateFromURL(None, url, 0)
-        self.failUnlessIsInstance(map2, LSMMapRef)
+        self.assertIsInstance(map2, LSMMapRef)
 
         if os.path.exists(fn):
             os.unlink(fn)
 
         fp = CFWriteStreamCreateWithAllocatedBuffers(None, None)
-        self.failUnlessIsInstance(fp, CFWriteStreamRef)
+        self.assertIsInstance(fp, CFWriteStreamRef)
         CFWriteStreamOpen(fp)
 
         v = LSMMapWriteToStream(map, text, fp, 0)
-        sel.failUnlessIsInstance(v, (int, long))
+        sel.assertIsInstance(v, (int, long))
 
 if __name__ == "__main__":
     main()
