6d412fb7a624a06fd01d208505c1b5cea1d9c531
Ronald Oussoren <ronaldoussoren@mac.com>
2010-04-05 09:29:12 +0000

    Port to py3k

--- pyobjc-framework-ScreenSaver/Modules/_ScreenSaver_inlines.m
+++ pyobjc-framework-ScreenSaver/Modules/_ScreenSaver_inlines.m
@@ -1,16 +1,12 @@
 #include "Python.h"
+#include "pyobjc-api.h"
 #import <ScreenSaver/ScreenSaver.h>
 
-typedef void (*FUNCTION)(void);
-
-struct function_map {
-    const char* name;
-    FUNCTION    function;
-} function_map[] = {
-	{"SSCenteredRectInRect", (FUNCTION)&SSCenteredRectInRect },
-	{"SSRandomFloatBetween", (FUNCTION)&SSRandomFloatBetween },
-	{"SSRandomIntBetween", (FUNCTION)&SSRandomIntBetween },
-	{"SSRandomPointForSizeWithinRect", (FUNCTION)&SSRandomPointForSizeWithinRect },
+static PyObjC_function_map function_map[] = {
+	{"SSCenteredRectInRect", (PyObjC_Function_Pointer)&SSCenteredRectInRect },
+	{"SSRandomFloatBetween", (PyObjC_Function_Pointer)&SSRandomFloatBetween },
+	{"SSRandomIntBetween", (PyObjC_Function_Pointer)&SSRandomIntBetween },
+	{"SSRandomPointForSizeWithinRect", (PyObjC_Function_Pointer)&SSRandomPointForSizeWithinRect },
     { 0, 0 }
 };
 
@@ -18,10 +14,17 @@ static PyMethodDef mod_methods[] = {
         { 0, 0, 0, 0 } /* sentinel */
 };
 
-void init_inlines(void)
+PyObjC_MODULE_INIT(_inlines)
 {
-    PyObject* m = Py_InitModule4("_inlines", mod_methods, NULL, NULL, PYTHON_API_VERSION);
+	PyObject* m = PyObjC_MODULE_CREATE(_inlines);
+	if (!m) {
+		PyObjC_INITERROR();
+	}
 
-    PyModule_AddObject(m, "_inline_list_", 
-        PyCObject_FromVoidPtr(function_map, NULL));
+	if (PyModule_AddObject(m, "_inline_list_", 
+		PyObjC_CreateInlineTab(function_map)) < 0) {
+			PyObjC_INITERROR();
+	}
+
+	PyObjC_INITDONE();
 }
