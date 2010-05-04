2d3850dd0af080af263dd71aca08c5880f35b059
Ronald Oussoren <ronaldoussoren@mac.com>
2010-05-04 19:16:47 +0000

    Add comment to coder-category.m that explains why it exists.

--- pyobjc-core/Modules/objc/coder-category.m
+++ pyobjc-core/Modules/objc/coder-category.m
@@ -1,3 +1,11 @@
+/*
+ * This defines a category that is used by the
+ * NSCoding support code. All of these methods
+ * are private, for PyObjC users the NSCoding
+ * wrappers in pyobjc-framework-Cocoa work just
+ * fine but we cannot use that in this package
+ * to avoid creating circular dependencies.
+ */
 #import <Foundation/Foundation.h>
 
 @implementation NSCoder (pyobjc)
