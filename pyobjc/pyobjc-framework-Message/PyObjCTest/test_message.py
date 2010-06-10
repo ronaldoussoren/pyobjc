1223f8da89c874c6e9597476f54e3bf6a46508a1
Ronald Oussoren <ronaldoussoren@mac.com>
2010-06-10 23:30:31 +0000

    Ensure that the tests only work in 32-bit mode, this framework is empty for 64-bit code.

--- pyobjc/pyobjc-framework-Message/PyObjCTest/test_message.py
+++ pyobjc/pyobjc-framework-Message/PyObjCTest/test_message.py
@@ -3,6 +3,7 @@ import Message
 from PyObjCTools.TestSupport import *
 
 class TestMessage (TestCase):
+    @onlyOn32Bit
     def testClasses(self):
         self.assert_( hasattr(Message, 'NSMailDelivery') )
         self.assert_( isinstance(Message.NSMailDelivery, objc.objc_class) )
