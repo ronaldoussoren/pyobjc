cab203997a7aebd1446511f5cec5c424723f1622
Ronald Oussoren <ronaldoussoren@mac.com>
2010-06-10 23:30:31 +0000

    Ensure that the tests only work in 32-bit mode, this framework is empty for 64-bit code.

--- pyobjc-framework-Message/PyObjCTest/test_nsmaildelivery.py
+++ pyobjc-framework-Message/PyObjCTest/test_nsmaildelivery.py
@@ -3,11 +3,14 @@ from PyObjCTools.TestSupport import *
 from Message import *
 
 class TestNSMailDelivery (TestCase):
+
+    @onlyOn32Bit
     def testClasses(self):
         self.assertResultIsBOOL(NSMailDelivery.hasDeliveryClassBeenConfigured)
         self.assertResultIsBOOL(NSMailDelivery.deliverMessage_headers_format_protocol_)
         self.assertResultIsBOOL(NSMailDelivery.deliverMessage_subject_to_)
 
+    @onlyOn32Bit
     def testConstants(self):
         self.assertIsInstance(NSMIMEMailFormat, unicode)
         self.assertIsInstance(NSASCIIMailFormat, unicode)
