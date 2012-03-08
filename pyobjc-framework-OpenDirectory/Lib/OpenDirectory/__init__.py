5929a8e4c7361c7966bf24d07e268afa0b14a28b
Ronald Oussoren <ronaldoussoren@mac.com>
2012-03-08 11:02:46 -0800

    Move to new metadata system

--- pyobjc-framework-OpenDirectory/Lib/OpenDirectory/__init__.py
+++ pyobjc-framework-OpenDirectory/Lib/OpenDirectory/__init__.py
@@ -1,11 +1,18 @@
 """
 Wrappers for the OpenDirectory framework
 """
-import objc as _objc
-from CFOpenDirectory import *
+import sys
+import objc
+import CFOpenDirectory
 
-__bundle__ = _objc.initFrameworkWrapper("OpenDirectory",
-    frameworkIdentifier="com.apple.OpenDirectory",
-    frameworkPath=_objc.pathForFramework(
-        "/System/Library/Frameworks/OpenDirectory.framework"),
-    globals=globals())
+from OpenDirectory import _metadata
+
+sys.modules['OpenDirectory'] = mod = objc.ObjCLazyModule('OpenDirectory',
+    "com.apple.OpenDirectory",
+    objc.pathForFramework("/System/Library/Frameworks/OpenDirectory.framework/Frameworks/OpenDirectory.framework"),
+    _metadata.__dict__, None, {
+       '__doc__': __doc__,
+       '__path__': __path__,
+       'objc': objc,
+    }, (CFOpenDirectory,))
+
