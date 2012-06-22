1863f697ac78e055123a95505c70ab5389b6a4e6
Ronald Oussoren <ronaldoussoren@mac.com>
2012-06-22 09:09:00 +0200

    Ensure that pyobjc-core is pip installable
    Without this patch you cannot install pyobjc-core using pip because
    pip assumes that 'egg_info' in setup.py refers to the setuptools command
    module, before this patch 'egg_info' was a local class in setup.py.
    
    Patch by Marc Abramowitz

--- pyobjc-core/setup.py
+++ pyobjc-core/setup.py
@@ -184,7 +184,7 @@ class oc_test (test.test):
         finally:
             self.remove_from_sys_path()
 
-from setuptools.command import egg_info as orig_egg_info
+from setuptools.command import egg_info
 
 def write_header(cmd, basename, filename):
     data = open(os.path.join('Modules/objc/', os.path.basename(basename)), 'rU').read()
@@ -200,9 +200,9 @@ def write_header(cmd, basename, filename):
 # call, but those don't work when also using a package_base
 # argument as we do.
 # (issue 123 in the distribute tracker)
-class egg_info (orig_egg_info.egg_info):
+class my_egg_info (egg_info.egg_info):
     def run(self):
-        orig_egg_info.egg_info.run(self)
+        egg_info.egg_info.run(self)
 
         for hdr in ("pyobjc-compat.h", "pyobjc-api.h"):
             fn = os.path.join("include", hdr)
@@ -482,7 +482,7 @@ dist = setup(
     namespace_packages = ['PyObjCTools'],
     package_dir = { '': 'Lib', 'PyObjCTest': 'PyObjCTest' },
     extra_path = "PyObjC",
-    cmdclass = {'build_ext': pyobjc_build_ext, 'install_lib': pyobjc_install_lib, 'build_py': oc_build_py, 'test': oc_test, 'egg_info':egg_info },
+    cmdclass = {'build_ext': pyobjc_build_ext, 'install_lib': pyobjc_install_lib, 'build_py': oc_build_py, 'test': oc_test, 'egg_info':my_egg_info },
     options = {'egg_info': {'egg_base': 'Lib'}},
     classifiers = CLASSIFIERS,
     license = 'MIT License',
