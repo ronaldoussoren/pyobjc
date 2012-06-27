0e5982b286988596799a93a3d1ae404f04caa4e0
Ronald Oussoren <ronaldoussoren@mac.com>
2012-06-27 14:23:46 +0200

    Ensure tests pass on OSX 10.8. Als disable tests for 32-bit because the framework is not available there

--- pyobjc-framework-Accounts/PyObjCTest/test_acaccountstore.py
+++ pyobjc-framework-Accounts/PyObjCTest/test_acaccountstore.py
@@ -1,11 +1,14 @@
-from PyObjCTools.TestSupport import *
-import Accounts
+import sys
 
-class TestACAccountStore (TestCase):
-    @min_os_level("10.8")
-    def testConstants(self):
-        self.assertHasAttr(Accounts, "ACAccountStoreDidChangeNotification")
-        self.assertIsInstance(Accounts.ACAccountStoreDidChangeNotification, unicode)
+if sys.maxsize > 2 ** 32:
+    from PyObjCTools.TestSupport import *
+    import Accounts
 
-if __name__ == "__main__":
-    main()
+    class TestACAccountStore (TestCase):
+        @min_os_level("10.8")
+        def testConstants(self):
+            self.assertHasAttr(Accounts, "ACAccountStoreDidChangeNotification")
+            self.assertIsInstance(Accounts.ACAccountStoreDidChangeNotification, unicode)
+
+    if __name__ == "__main__":
+        main()
--- pyobjc-framework-Accounts/PyObjCTest/test_acerror.py
+++ pyobjc-framework-Accounts/PyObjCTest/test_acerror.py
@@ -1,19 +1,27 @@
-from PyObjCTools.TestSupport import *
-import Accounts
+import sys
 
-class TestACError (TestCase):
-    @min_os_level("10.8")
-    def testConstants(self):
-        self.assertIsInstance(Accounts.ACErrorDomain, "unicode")
+if sys.maxsize > 2**32:
+    from PyObjCTools.TestSupport import *
+    import Accounts
 
-        self.assertEqual(Accounts.ACErrorUnknown, 1)
-        self.assertEqual(Accounts.ACErrorAccountMissingRequiredProperty, 2)
-        self.assertEqual(Accounts.ACErrorAccountAuthenticationFailed, 3)
-        self.assertEqual(Accounts.ACErrorAccountTypeInvalid, 4)
-        self.assertEqual(Accounts.ACErrorAccountAlreadyExists, 5)
-        self.assertEqual(Accounts.ACErrorAccountNotFound, 6)
-        self.assertEqual(Accounts.ACErrorPermissionDenied, 7)
-        self.assertEqual(Accounts.ACErrorAccessInfoInvalid, 8)            
+    try:
+        unicode
+    except NameError:
+        unicode = str
 
-if __name__ == "__main__":
-    main()
+    class TestACError (TestCase):
+        @min_os_level("10.8")
+        def testConstants(self):
+            self.assertIsInstance(Accounts.ACErrorDomain, unicode)
+
+            self.assertEqual(Accounts.ACErrorUnknown, 1)
+            self.assertEqual(Accounts.ACErrorAccountMissingRequiredProperty, 2)
+            self.assertEqual(Accounts.ACErrorAccountAuthenticationFailed, 3)
+            self.assertEqual(Accounts.ACErrorAccountTypeInvalid, 4)
+            self.assertEqual(Accounts.ACErrorAccountAlreadyExists, 5)
+            self.assertEqual(Accounts.ACErrorAccountNotFound, 6)
+            self.assertEqual(Accounts.ACErrorPermissionDenied, 7)
+            self.assertEqual(Accounts.ACErrorAccessInfoInvalid, 8)            
+
+    if __name__ == "__main__":
+        main()
