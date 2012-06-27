d3c7c4efaefffd84643c8197a8cec6f5fd0d494e
Ronald Oussoren <ronaldoussoren@mac.com>
2012-06-27 14:17:20 +0200

    Port wrappers to OSX 10.8

--- pyobjc-framework-AddressBook/PyObjCTest/test_abpersonview.py
+++ pyobjc-framework-AddressBook/PyObjCTest/test_abpersonview.py
@@ -7,5 +7,10 @@ class TestABPersonView (TestCase):
         self.assertResultIsBOOL(AddressBook.ABPersonView.editing)
         self.assertArgIsBOOL(AddressBook.ABPersonView.setEditing_, 0)
 
+    @min_os_level('10.8')
+    def testMethods(self):
+        self.assertResultIsBOOL(AddressBook.ABPersonView.shouldShowLinkedPeople)
+        self.assertArgIsBOOL(AddressBook.ABPersonView.setShouldShowLinkedPeople_, 0)
+
 if __name__ == "__main__":
     main()
