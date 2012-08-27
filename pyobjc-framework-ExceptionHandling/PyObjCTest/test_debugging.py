eec9d61326e517b518084cdce2c46c8825cac70d
Ronald Oussoren <ronaldoussoren@mac.com>
2012-08-27 14:14:38 +0200

    PyObjCTools.Debugging doesn't work with Python 3.x (issue #14)
    This checking ensures that PyObjCTools.Debugging can work with python 3.x,
    by removing "u" prefixes before unicode literals, adding "b" prefixes before
    bytes literals and moving to print-as-a-function.
    
    Also adds a minimal testsuite for the Debugging module, which is far from
    perfect right now (it doesn't actually test that exception logging works)

---
+++ pyobjc-framework-ExceptionHandling/PyObjCTest/test_debugging.py
@@ -0,0 +1,55 @@
+'''
+Some simple tests to check that the framework is properly wrapped.
+'''
+import objc
+from PyObjCTools.TestSupport import *
+from PyObjCTools import Debugging
+
+try:
+    long
+except NameError:
+    long = int
+
+class TestDebugging (TestCase):
+    def testConstants(self):
+        for nm in (
+                'LOGSTACKTRACE', 'DEFAULTVERBOSITY', 
+                'DEFAULTMASK', 'EVERYTHINGMASK'):
+            self.assertTrue(hasattr(Debugging, nm))
+            self.assertTrue(isinstance(getattr(Debugging, nm), (int, long)))
+
+    def testHandlerBasic(self):
+        self.assertFalse(Debugging.handlerInstalled())
+
+        Debugging.installExceptionHandler()
+        self.assertTrue(Debugging.handlerInstalled())
+
+        Debugging.removeExceptionHandler()
+        self.assertFalse(Debugging.handlerInstalled())
+
+        Debugging.installVerboseExceptionHandler()
+        self.assertTrue(Debugging.handlerInstalled())
+
+        Debugging.removeExceptionHandler()
+        self.assertFalse(Debugging.handlerInstalled())
+
+        Debugging.installPythonExceptionHandler()
+        self.assertTrue(Debugging.handlerInstalled())
+
+        Debugging.removeExceptionHandler()
+        self.assertFalse(Debugging.handlerInstalled())
+
+
+    @expectedFailure
+    def testMisc(self):
+        self.fail("Actually test this module")
+        # - isPythonException
+        # - nsLogPythonException
+        # - nsLogObjCException
+        #
+        # - Actually trigger handler in various modes
+
+if __name__ == "__main__":
+    main()
+
+
