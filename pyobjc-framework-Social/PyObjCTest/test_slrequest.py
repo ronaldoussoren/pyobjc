5ef5bedd27a47d3745ba819f55047efb34b4210f
Ronald Oussoren <ronaldoussoren@mac.com>
2012-09-13 16:09:13 +0200

    Disable tests for the Social framework in 32-bit mode
    The framework is 64-bit only

--- pyobjc-framework-Social/PyObjCTest/test_slrequest.py
+++ pyobjc-framework-Social/PyObjCTest/test_slrequest.py
@@ -1,18 +1,20 @@
 from PyObjCTools.TestSupport import *
+import sys
 import Social
 
 SLRequestHandler = b'v@@@'
 
-class TestSLRequest (TestCase):
-    @min_os_level("10.8")
-    def testConstants(self):
-        self.assertEqual(Social.SLRequestMethodGET, 0)
-        self.assertEqual(Social.SLRequestMethodPOST, 1)
-        self.assertEqual(Social.SLRequestMethodDELETE, 2)
+if sys.maxsize > 2**32:
+    class TestSLRequest (TestCase):
+        @min_os_level("10.8")
+        def testConstants(self):
+            self.assertEqual(Social.SLRequestMethodGET, 0)
+            self.assertEqual(Social.SLRequestMethodPOST, 1)
+            self.assertEqual(Social.SLRequestMethodDELETE, 2)
 
-    @min_os_level("10.8")
-    def testMethods(self):
-        self.assertArgIsBlock(Social.SLRequest.performRequestWithHandler_, 0, SLRequestHandler)
+        @min_os_level("10.8")
+        def testMethods(self):
+            self.assertArgIsBlock(Social.SLRequest.performRequestWithHandler_, 0, SLRequestHandler)
 
 if __name__ == "__main__":
     main()
--- pyobjc-framework-Social/PyObjCTest/test_slservicetypes.py
+++ pyobjc-framework-Social/PyObjCTest/test_slservicetypes.py
@@ -1,11 +1,16 @@
 from PyObjCTools.TestSupport import *
-import Social
+import sys
 
-class TestSLTypes (TestCase):
-    @min_os_level("10.8")
-    def testConstants(self):
-        self.assertIsInstance(Social.SLServiceTypeTwitter, unicode)
-        self.assertIsInstance(Social.SLServiceTypeSinaWeibo, unicode)
+
+if sys.maxsize >= 2**32:
+
+    import Social
+
+    class TestSLTypes (TestCase):
+        @min_os_level("10.8")
+        def testConstants(self):
+            self.assertIsInstance(Social.SLServiceTypeTwitter, unicode)
+            self.assertIsInstance(Social.SLServiceTypeSinaWeibo, unicode)
 
 if __name__ == "__main__":
     main()
