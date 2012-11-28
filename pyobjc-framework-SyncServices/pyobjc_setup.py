d417ca7c3bdd451e4f556f6b65f7e7cd50b082af
Ronald Oussoren <ronaldoussoren@mac.com>
2012-11-28 13:04:58 +0100

    Add classifier for python 3.3

--- pyobjc-framework-SyncServices/pyobjc_setup.py
+++ pyobjc-framework-SyncServices/pyobjc_setup.py
@@ -169,6 +169,7 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.1
 Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
 Programming Language :: Objective C
 Topic :: Software Development :: Libraries :: Python Modules
 Topic :: Software Development :: User Interfaces
--- pyobjc/setup.py
+++ pyobjc/setup.py
@@ -125,6 +125,13 @@ License :: OSI Approved :: MIT License
 Natural Language :: English
 Operating System :: MacOS :: MacOS X
 Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.1
+Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
 Programming Language :: Objective C
 Topic :: Software Development :: Libraries :: Python Modules
 Topic :: Software Development :: User Interfaces
