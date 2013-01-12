02eb550f2b3c9b419ebc406d847d5cd2f53347b4
Ronald Oussoren <ronaldoussoren@mac.com>
2013-01-12 10:10:10 -0800

    Improve docstring for objc.selector and objc.function
    With this checkin the docstring contains a fairly accurate decription
    of the callable interface as known by the bridge.

--- pyobjc-core/Lib/objc/_callable_docstr.py
+++ pyobjc-core/Lib/objc/_callable_docstr.py
@@ -52,8 +52,8 @@ def describe_type(typestr):
             return nm
         return describe_type(typestr[1:]) + '*'
 
-    if typestr[0] in prefixes:
-        return prefixes[typestr[0]] + describe_type(typestr[1:])
+    if typestr[:1] in prefixes:
+        return prefixes[typestr[:1]] + describe_type(typestr[1:])
 
     if typestr.startswith(objc._C_STRUCT_B):
         nm = _nameForSignature(typestr)
@@ -62,51 +62,108 @@ def describe_type(typestr):
 
     # XXX: handle struct, union, array, bitfield
     return "<?>"
-    
-
 
 def describe_signature(callable):
     name     = callable.__name__
     metadata = callable.__metadata__()
-    if isinstance(callable, objc.selector):
-        class_name = callable.__objclass__
+
+    return describe_callable(name, metadata, ismethod=isinstance(callable, objc.selector))
+
+def describe_callable(name, metadata, offset='', ismethod=False):
+    arg_info = []
+    if ismethod:
+        arg_offset = 2
 
         name_parts = name.split(':')
         hdr_name = []
-        if len(metadata['arguments']) > 2:
-            for idx, (nm, info) in enumerate(zip(name_parts, metadata['arguments'][2:])):
+        if len(metadata['arguments']) > arg_offset:
+            for idx, (nm, info) in enumerate(zip(name_parts, metadata['arguments'][arg_offset:])):
                 hdr_name.append(nm)
                 hdr_name.append(':(')
                 hdr_name.append(describe_type(info['type']))
                 hdr_name.append(')arg%d '%(idx,))
-                if metadata['variadic']:
-                    hdr_name.append(", ...")
+
+                if info['type'][:1] in prefixes and info['type'][:1] not in (objc._C_ONEWAY, objc._C_CONST):
+                    arg_info.append((idx, info))
+                if info.get('printf_format'):
+                    arg_info.append((idx, info))
+                if info.get('callable'):
+                    arg_info.append((idx, info))
+
+
+            if metadata['variadic']:
+                hdr_name.append(", ...")
         else:
             hdr_name.append(name)
 
 
-        header = "%s (%s)%s"%(
-                "+" if callable.isClassMethod else "-",
+        header = "%s (%s)%s;"%(
+                "+" if metadata['classmethod'] else "-",
                 describe_type(metadata['retval']['type']),
                 ''.join(hdr_name))
     else:
-        class_name = None
         hdr_name = []
+        arg_offset = 0
         for idx, info in enumerate(metadata['arguments']):
             if idx != 0:
-                hdr_name.apend(', ')
+                hdr_name.append(', ')
             hdr_name.append(describe_type(info['type']))
             hdr_name.append(' arg%d'%(idx,))
+            if info['type'][:1] in prefixes and info['type'][:1] not in (objc._C_ONEWAY, objc._C_CONST):
+                arg_info.append((idx, info))
+            if info.get('printf_format'):
+                arg_info.append((idx, info))
+            if info.get('callable'):
+                arg_info.append((idx, info))
         if metadata['variadic']:
             hdr_name.append(", ...")
 
-        header = "%s %s(%s)"%(
+        header = "%s %s(%s);"%(
                 describe_type(metadata['retval']['type']),
                 name,
                 ''.join(hdr_name))
 
-    # XXX: add more information about 'hard' arguments
+    result = [header]
+    if metadata.get('suggestion'):
+        result.append('')
+        result.append('WARNING: %s'%(metadata['suggestion'],))
 
-    return header
+    if arg_info:
+        result.append('')
+        for idx, info in arg_info:
+            if info.get('print_format'):
+                result.append('arg%d: %%-style format'%(idx,))
+
+            elif info.get('callable'):
+                result.append('arg%d: %s'%(idx, describe_callable('callback', info['callable'], offset='    ' + offset)))
+
+            else:
+                arg = info.get('c_array_length_in_arg')
+                if arg is not None:
+                    if isinstance(arg, tuple):
+                        result.append('arg%d: array with length on input in arg%d, and output in arg%d'%(idx, arg[0] - arg_offset, arg[1] - arg_offset))
+                    else:
+                        if info.get('c_array_length_in_result'):
+                            result.append('arg%d: array with length on input in arg%d, and output in return value'%(idx, arg - arg_offset))
+                        else:
+                            result.append('arg%d: array with length in arg%d'%(idx, arg - arg_offset))
+                    continue
+
+                if info.get('c_array_length_in_result'):
+                    result.append('arg%d: array with length in return value'%(idx,))
+                    continue
+
+                if info.get('c_array_of_fixed_length'):
+                    result.append('arg%d: array with length %d'%(idx, info.get('c_array_of_fixed_length')))
+                    continue
+
+                if info.get('c_array_of_variable_length'):
+                    result.append('arg%d: array with unknown length'%(idx,))
+                    continue
+
+                result.append('arg%d: pass-by-reference %sargument'%(idx, prefixes.get(info['type'][:1]),))
+
+    return ('\n'+offset).join(result)
 
 _setCallableDoc(describe_signature)
+print(describe_signature(objc.runtime.NSObject.init))
