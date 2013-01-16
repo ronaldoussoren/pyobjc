183d6ea723835396f5d96d934d0ee03fdfe8e1f9
Ronald Oussoren <ronaldoussoren@mac.com>
2013-01-16 15:45:40 +0100

    Fix test failures on OSX 10.5

--- pyobjc-framework-SyncServices/PyObjCTest/test_isyncchange.py
+++ pyobjc-framework-SyncServices/PyObjCTest/test_isyncchange.py
@@ -20,8 +20,9 @@ class TestISyncChange (TestCase):
         self.assertIsInstance(ISyncChangePropertyNameKey, unicode)
         self.assertIsInstance(ISyncChangePropertyValueKey, unicode)
 
-    @min_os_level('10.5')
+    @min_os_level('10.6')
     def testConstants10_5(self):
+        # Document for 10.5, but not actually present there
         self.assertIsInstance(ISyncChangePropertyValueIsDefaultKey, unicode)
 
 if __name__ == "__main__":
--- pyobjc-framework-SyncServices/PyObjCTest/test_syncservices.py
+++ pyobjc-framework-SyncServices/PyObjCTest/test_syncservices.py
@@ -12,6 +12,10 @@ class TestSyncServices (TestCase):
 
     def testProtocols(self):
         objc.protocolNamed('ISyncFiltering')
+
+    @min_os_level('10.6')
+    def testProtocols10_5(self):
+        # Document for 10.5, but not actually present there
         objc.protocolNamed('ISyncSessionDriverDataSource')
         objc.protocolNamed('NSPersistentStoreCoordinatorSyncing')
 
