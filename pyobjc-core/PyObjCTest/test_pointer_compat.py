0ca03f94e54f47569890bb30cbc018f6e5a56a38
Ronald Oussoren <ronaldoussoren@mac.com>
2013-01-16 22:49:48 +0100

    Add more tests for the ctypes/cobject conversion in opaque-pointer types and objc.objc_object
    The API should now be fully tested. This also fixes some bugs in the implementation,
    and adds missing functionality to opaque pointer types.

--- pyobjc-core/PyObjCTest/test_pointer_compat.py
+++ pyobjc-core/PyObjCTest/test_pointer_compat.py
@@ -1,6 +1,9 @@
 from PyObjCTools.TestSupport import *
+from PyObjCTest.pointersupport import opaque_capsule, object_capsule
 import objc
 import ctypes
+
+OpaqueType = objc.createOpaquePointerType("OpaqueType", b"^{OpaqueType}", None)
 
 
 class TestProxySupport (TestCase):
@@ -25,11 +28,50 @@ class TestProxySupport (TestCase):
         v = objc.objc_object(c_void_p=p)
         self.assertIs(v, arr)
 
-    @expectedFailure
-    def test_missing(self):
-        # Add tests simular to the ones above, but
-        # with objects created outside of PyObjC
-        self.fail()
+    def test_voidp_using_ctypes(self):
+        lib = ctypes.CDLL('/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation')
+        func = lib.CFStringCreateWithCString
+        func.restype = ctypes.c_void_p
+        func.argtypes = [ctypes.c_void_p, ctypes.c_char_p, ctypes.c_int]
+
+        kCFStringEncodingISOLatin1 = 0x0201
+        ct_obj = func(None, b"hello world", kCFStringEncodingISOLatin1)
+        
+        value = objc.objc_object(c_void_p=ct_obj)
+        self.assertIsInstance(value, objc.pyobjc_unicode)
+        self.assertEqual(objc.pyobjc_id(value.nsstring()), ct_obj)
+
+    def test_opaque_capsule(self):
+        cap = opaque_capsule()
+
+        value = OpaqueType(cobject=cap)
+        self.assertIsInstance(value, OpaqueType)
+        self.assertEqual(value.__pointer__, 1234)
+
+        self.assertRaises(ValueError, OpaqueType, cobject=object_capsule())
+        self.assertRaises(TypeError, OpaqueType, cobject=42)
+    
+    def test_opaque_ctypes(self):
+        ptr = ctypes.c_void_p(0xabcd)
+
+        value = OpaqueType(c_void_p=ptr)
+        self.assertIsInstance(value, OpaqueType)
+        self.assertEqual(value.__pointer__, 0xabcd)
+
+        value = OpaqueType(c_void_p=0xdefa)
+        self.assertIsInstance(value, OpaqueType)
+        self.assertEqual(value.__pointer__, 0xdefa)
+
+
+    def test_object_capsule(self):
+        NSObject = objc.lookUpClass('NSObject')
+        cap = object_capsule()
+
+        value = NSObject(cobject=cap)
+        self.assertIsInstance(value, NSObject)
+
+        self.assertRaises(ValueError, NSObject, cobject=opaque_capsule())
+        self.assertRaises(TypeError, NSObject, cobject=42)
 
 if __name__ == "__main__":
     main()
