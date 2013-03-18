429ecfbc57c558c02e12ce39710dafa1bbbfec94
Ronald Oussoren <ronaldoussoren@mac.com>
2013-03-18 09:18:54 +0100

    First attempt at StoreKit bindings

---
+++ pyobjc-framework-StoreKit/setup.py
@@ -0,0 +1,36 @@
+'''
+Wrappers for the "StoreKit" framework on MacOS X.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+'''
+
+from pyobjc_setup import setup, Extension
+import os
+
+setup(
+    name='pyobjc-framework-StoreKit',
+    version="3.0a1",
+    description = "Wrappers for the framework StoreKit on Mac OS X",
+    packages = [ "StoreKit" ],
+    setup_requires = [
+        'pyobjc-core>=3.0a1',
+    ],
+    install_requires = [
+        'pyobjc-core>=3.0a1',
+        'pyobjc-framework-Cocoa>=3.0a1',
+    ],
+    ext_modules = [
+        Extension("StoreKit._StoreKit",
+            [ "Modules/_StoreKit.m" ],
+            extra_link_args=["-framework", "StoreKit"],
+            depends=[
+                os.path.join('Modules', fn)
+                    for fn in os.listdir('Modules')
+                    if fn.startswith('_StoreKit')
+            ]
+        ),
+    ]
+)
