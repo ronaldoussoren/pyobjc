1d634488d5f46daec566104037670f72ab96d333
Ronald Oussoren <ronaldoussoren@mac.com>
2013-05-06 18:54:24 +0200

    Add some more tests for method resolution

---
+++ pyobjc-core/Modules/objc/test/methres1.m
@@ -0,0 +1,78 @@
+#include "Python.h"
+#include "pyobjc-api.h"
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (MethodResolution1)
+{
+}
+-(id)oc_method1;
+-(id)ocmethod2;
+@end
+
+@implementation NSObject (MethodResolution1)
+
+-(id)oc_method1
+{
+	return @"NSObject.oc_method1";
+}
+
+-(id)ocmethod2
+{
+	return @"NSObject.ocmethod2";
+}
+
+@end
+
+
+static PyMethodDef mod_methods[] = {
+    { 0, 0, 0, 0 }
+};
+
+#if PY_VERSION_HEX >= 0x03000000
+
+static struct PyModuleDef mod_module = {
+    PyModuleDef_HEAD_INIT,
+    "methres1",
+    NULL,
+    0,
+    mod_methods,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+
+#define INITERROR() return NULL
+#define INITDONE() return m
+
+PyObject* PyInit_methres1(void);
+
+PyObject* __attribute__((__visibility__("default")))
+PyInit_methres1(void)
+
+#else
+
+#define INITERROR() return
+#define INITDONE() return
+
+void initmethres1(void);
+
+void __attribute__((__visibility__("default")))
+initinitialize(void)
+#endif
+{
+    PyObject* m;
+
+#if PY_VERSION_HEX >= 0x03000000
+    m = PyModule_Create(&mod_module);
+#else
+    m = Py_InitModule4("methres1", mod_methods,
+        NULL, NULL, PYTHON_API_VERSION);
+#endif
+    if (!m) {
+        INITERROR();
+    }
+
+    INITDONE();
+}
---
+++ pyobjc-core/Modules/objc/test/methres2.m
@@ -0,0 +1,78 @@
+#include "Python.h"
+#include "pyobjc-api.h"
+
+#import <Foundation/Foundation.h>
+
+@interface NSURL (MethodResolution2)
+{
+}
+-(id)oc_method1;
+-(id)ocmethod2;
+@end
+
+@implementation NSURL (MethodResolution2)
+
+-(id)oc_method1
+{
+	return @"NSURL.oc_method1";
+}
+
+-(id)ocmethod2
+{
+	return @"NSURL.ocmethod2";
+}
+
+@end
+
+
+static PyMethodDef mod_methods[] = {
+    { 0, 0, 0, 0 }
+};
+
+#if PY_VERSION_HEX >= 0x03000000
+
+static struct PyModuleDef mod_module = {
+    PyModuleDef_HEAD_INIT,
+    "methres2",
+    NULL,
+    0,
+    mod_methods,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+
+#define INITERROR() return NULL
+#define INITDONE() return m
+
+PyObject* PyInit_methres2(void);
+
+PyObject* __attribute__((__visibility__("default")))
+PyInit_methres2(void)
+
+#else
+
+#define INITERROR() return
+#define INITDONE() return
+
+void initmethres2(void);
+
+void __attribute__((__visibility__("default")))
+initinitialize(void)
+#endif
+{
+    PyObject* m;
+
+#if PY_VERSION_HEX >= 0x03000000
+    m = PyModule_Create(&mod_module);
+#else
+    m = Py_InitModule4("methres2", mod_methods,
+        NULL, NULL, PYTHON_API_VERSION);
+#endif
+    if (!m) {
+        INITERROR();
+    }
+
+    INITDONE();
+}
