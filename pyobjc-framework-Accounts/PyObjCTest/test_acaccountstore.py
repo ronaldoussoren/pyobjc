b6fe4b158eec3f8c58b148c8858c8d66ef2592f2
Ronald Oussoren <ronaldoussoren@mac.com>
2013-05-23 15:52:29 +0200

    Add missing metadata for block APIs

--- pyobjc-framework-Accounts/PyObjCTest/test_acaccountstore.py
+++ pyobjc-framework-Accounts/PyObjCTest/test_acaccountstore.py
@@ -9,11 +9,23 @@ if sys.maxsize > 2 ** 32:
     from PyObjCTools.TestSupport import *
     import Accounts
 
+    ACAccountStoreSaveCompletionHandler = b'vZ@'
+    ACAccountStoreRemoveCompletionHandler = b'vZ@'
+    ACAccountStoreRequestAccessCompletionHandler = b'vZ@'
+    ACAccountStoreCredentialRenewalHandler = b'v' + objc._C_NSInteger + b'@'
+
     class TestACAccountStore (TestCase):
         @min_os_level("10.8")
         def testConstants(self):
             self.assertHasAttr(Accounts, "ACAccountStoreDidChangeNotification")
             self.assertIsInstance(Accounts.ACAccountStoreDidChangeNotification, unicode)
 
-    if __name__ == "__main__":
-        main()
+        def testMethods(self):
+            self.assertArgIsBlock(Accounts.ACAccountStore.saveAccount_withCompletionHandler_, 1, ACAccountStoreSaveCompletionHandler)
+            self.assertArgIsBlock(Accounts.ACAccountStore.requestAccessToAccountsWithType_withCompletionHandler_, 1, ACAccountStoreRequestAccessCompletionHandler)
+            self.assertArgIsBlock(Accounts.ACAccountStore.requestAccessToAccountsWithType_options_completion_, 2, ACAccountStoreRequestAccessCompletionHandler)
+            self.assertArgIsBlock(Accounts.ACAccountStore.renewCredentialsForAccount_completion_, 1, ACAccountStoreCredentialRenewalHandler)
+            self.assertArgIsBlock(Accounts.ACAccountStore.removeAccount_withCompletionHandler_, 1, ACAccountStoreRemoveCompletionHandler)
+
+if __name__ == "__main__":
+    main()
