fdf12c3c99ffde0f1a3afec67723150c73591ec9
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:19 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/AppKit/PyObjCLauncher/setup.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/PyObjCLauncher/setup.py
@@ -4,14 +4,19 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 setup(
     name="PyObjC Launcher",
     app=["PyObjCLauncher.py"],
     data_files=["English.lproj"],
-    options=dict(py2app=dict(
-        plist='Info.plist',
-    )),
+    options=dict(
+        py2app=dict(
+            plist="Info.plist",
+        )
+    ),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
