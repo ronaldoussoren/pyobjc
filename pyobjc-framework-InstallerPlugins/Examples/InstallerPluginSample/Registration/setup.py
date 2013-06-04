15324759db10f844efc81ba0bbd1c4daba205101
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:35 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-InstallerPlugins/Examples/InstallerPluginSample/Registration/setup.py
+++ pyobjc-framework-InstallerPlugins/Examples/InstallerPluginSample/Registration/setup.py
@@ -4,22 +4,27 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 plist = dict(
     InstallerSectionTitle="PyRegistration",
-    NSMainNibFile='Registration',
-    NSPrincipalClass='InstallerSection',
-    CFBundleIdentifier='com.MyCompany.Installer.example.Registration',
+    NSMainNibFile="Registration",
+    NSPrincipalClass="InstallerSection",
+    CFBundleIdentifier="com.MyCompany.Installer.example.Registration",
 )
 
 setup(
-    name='Registration',
-    plugin=['RegistrationPane.py'],
-    data_files=['English.lproj'],
+    name="Registration",
+    plugin=["RegistrationPane.py"],
+    data_files=["English.lproj"],
     options=dict(py2app=dict(
-        extension='.bundle',
+        extension=".bundle",
         plist=plist,
     )),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+        "pyobjc-framework-InstallerPlugins",
+    ]
+
 )
