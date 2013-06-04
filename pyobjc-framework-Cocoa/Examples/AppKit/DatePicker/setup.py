c19f37f081e3e9c65614166ab90eb34e80941309
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:09 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/AppKit/DatePicker/setup.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/DatePicker/setup.py
@@ -4,11 +4,14 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 setup(
     name="DatePicker",
     app=["main.py"],
     data_files=["English.lproj"],
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
