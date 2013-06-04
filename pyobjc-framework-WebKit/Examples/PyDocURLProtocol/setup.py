fdd01dc60de87a93f80c1e58e7c6821547f1670e
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:56 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-WebKit/Examples/PyDocURLProtocol/setup.py
+++ pyobjc-framework-WebKit/Examples/PyDocURLProtocol/setup.py
@@ -4,21 +4,26 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 plist = dict(
-            NSMainNibFile='PyDocBrowser',
-            NSAppleScriptEnabled=True,
-            CFBundleURLTypes=[
-                dict(
-                    CFBundleURLName='Python Documention URL',
-                    CFBundleURLSchemes=['pydoc'],
-                )
-            ]
+    NSMainNibFile="PyDocBrowser",
+    NSAppleScriptEnabled=True,
+    CFBundleURLTypes=[
+        dict(
+            CFBundleURLName="Python Documention URL",
+            CFBundleURLSchemes=["pydoc"],
         )
+    ]
+)
+
 setup(
     app=["PyDocBrowser.py"],
     data_files=["PyDocBrowser.nib"],
     options=dict(py2app=dict(plist=plist)),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+        "pyobjc-framework-WebKit",
+    ]
 )
