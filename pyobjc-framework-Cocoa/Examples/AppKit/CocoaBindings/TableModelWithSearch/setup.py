fe68f62d06e115b77a3900f95604796c9784c7fd
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:05 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/AppKit/CocoaBindings/TableModelWithSearch/setup.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/CocoaBindings/TableModelWithSearch/setup.py
@@ -4,11 +4,14 @@ Script for building the example, alternative to the Xcode project
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 setup(
     name="TableModelWithSearch",
     app=["main.py"],
     data_files=["English.lproj"],
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
