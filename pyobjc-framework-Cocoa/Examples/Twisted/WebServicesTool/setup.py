ac33420f815369b6c9f93c0f9cf81c443653ac4b
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:29 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/Twisted/WebServicesTool/setup.py
+++ pyobjc-framework-Cocoa/Examples/Twisted/WebServicesTool/setup.py
@@ -4,14 +4,22 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 setup(
     app=["Main.py"],
-    data_files=["English.lproj", "Preferences.png", "Reload.png", "WST.png"],
+    data_files=[
+        "English.lproj",
+        "Preferences.png",
+        "Reload.png",
+        "WST.png"
+    ],
     options=dict(py2app=dict(
         iconfile="WST.icns",
-        plist=dict(CFBundleName='Web Services Tool'),
+        plist=dict(CFBundleName="Web Services Tool"),
     )),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
