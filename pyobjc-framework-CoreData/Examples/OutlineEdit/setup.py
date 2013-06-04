79de65a72f9bf4fce59168801dfd3b3f4c915c1b
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:31 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-CoreData/Examples/OutlineEdit/setup.py
+++ pyobjc-framework-CoreData/Examples/OutlineEdit/setup.py
@@ -4,51 +4,54 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
-import os
+from setuptools import setup
 
 
 plist = dict(
     NSMainNibFile="MainMenu",
     CFBundleDocumentTypes=[
         dict(
-            CFBundleTypeExtensions=['binary'],
-            CFBundleTypeMIMETypes=['application/octect-stream'],
-            CFBundleTypeName='Binary',
-            CFBundleTypeRole='Editor',
+            CFBundleTypeExtensions=["binary"],
+            CFBundleTypeMIMETypes=["application/octect-stream"],
+            CFBundleTypeName="Binary",
+            CFBundleTypeRole="Editor",
             LSTypeIsPackage=False,
-            NSDocumentClass='MyDocument',
-            NSPersistentStoreTypeKey='Binary',
+            NSDocumentClass="MyDocument",
+            NSPersistentStoreTypeKey="Binary",
         ),
         dict(
-            CFBundleTypeExtensions=['sql'],
-            CFBundleTypeMIMETypes=['application/octect-stream'],
-            CFBundleTypeName='SQL',
-            CFBundleTypeRole='Editor',
+            CFBundleTypeExtensions=["sql"],
+            CFBundleTypeMIMETypes=["application/octect-stream"],
+            CFBundleTypeName="SQL",
+            CFBundleTypeRole="Editor",
             LSTypeIsPackage=False,
-            NSDocumentClass='MyDocument',
-            NSPersistentStoreTypeKey='SQLite',
+            NSDocumentClass="MyDocument",
+            NSPersistentStoreTypeKey="SQLite",
         ),
         dict(
-            CFBundleTypeExtensions=['xml'],
-            CFBundleTypeMIMETypes=['text/xml'],
-            CFBundleTypeName='XML',
-            CFBundleOSTypes=['????'],
-            CFBundleTypeRole='Editor',
+            CFBundleTypeExtensions=["xml"],
+            CFBundleTypeMIMETypes=["text/xml"],
+            CFBundleTypeName="XML",
+            CFBundleOSTypes=["????"],
+            CFBundleTypeRole="Editor",
             LSTypeIsPackage=False,
-            NSDocumentClass='MyDocument',
-            NSPersistentStoreTypeKey='XML',
+            NSDocumentClass="MyDocument",
+            NSPersistentStoreTypeKey="XML",
         ),
     ],
 )
 
 setup(
-    name='PyOutlineEdit',
+    name="PyOutlineEdit",
     app=["main.py"],
     data_files=["English.lproj"],
     options=dict(py2app=dict(
         plist=plist,
-        datamodels=['MyDocument'],
+        datamodels=["MyDocument"],
     )),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+        "pyobjc-framework-CoreData",
+    ]
 )
