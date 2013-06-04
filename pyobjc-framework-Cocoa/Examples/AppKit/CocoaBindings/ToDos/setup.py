e9e6561bfae8bd8c03d26b5cbe681f879640e133
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:08 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/AppKit/CocoaBindings/ToDos/setup.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/CocoaBindings/ToDos/setup.py
@@ -4,25 +4,28 @@ Script for building the example:
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 plist = dict(
     CFBundleDocumentTypes = [
         dict(
-            CFBundleTypeExtensions=[u'ToDos', u'*'],
-            CFBundleTypeName=u'ToDos File',
-            CFBundleTypeRole=u'Editor',
-            NSDocumentClass=u'ToDosDocument',
+            CFBundleTypeExtensions=["ToDos", "*"],
+            CFBundleTypeName="ToDos File",
+            CFBundleTypeRole="Editor",
+            NSDocumentClass="ToDosDocument",
         ),
     ],
 )
 
 setup(
-    name='ToDos',
+    name="ToDos",
     app=["ToDos.py"],
     data_files=["English.lproj"],
     options=dict(py2app=dict(
         plist=plist,
     )),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
