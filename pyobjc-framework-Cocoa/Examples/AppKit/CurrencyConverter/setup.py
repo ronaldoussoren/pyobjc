d1ddbdef71386eb4a9d57636d2e31128fa7afd9f
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:09 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/AppKit/CurrencyConverter/setup.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/CurrencyConverter/setup.py
@@ -4,11 +4,14 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 setup(
-    name='CurrencyConverter',
+    name="CurrencyConverter",
     app=["CurrencyConverter.py"],
     data_files=["English.lproj"],
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
