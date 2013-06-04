5ce942d0279cd267296235964526ce082d48a065
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:26 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/AppKit/Todo/setup.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/Todo/setup.py
@@ -4,33 +4,36 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 import glob
-images = glob.glob('Images/*.tiff')
-icons = glob.glob('Icons/*.icns')
+images = glob.glob("Images/*.tiff")
+icons = glob.glob("Icons/*.icns")
 
 plist = dict(
-    CFBundleShortVersionString='To Do v1',
-    CFBundleIconFile='ToDoApp.icns',
-    CFBundleGetInfoString='To Do v1',
-    CFBundleIdentifier='net.sf.pyobjc.ToDo',
+    CFBundleShortVersionString="To Do v1",
+    CFBundleIconFile="ToDoApp.icns",
+    CFBundleGetInfoString="To Do v1",
+    CFBundleIdentifier="net.sf.pyobjc.ToDo",
     CFBundleDocumentTypes=[
         dict(
-            CFBundleTypeName='To Do list',
-            CFBundleTypeRole='Editor',
-            NSDocumentClass='ToDoDocument',
-            CFBundleTypeIconFile='ToDoDoc.icns',
-            CFBundleTypeExtensions=['ToDo'],
-            CFBundleTypeOSTypes=['ToDo'],
+            CFBundleTypeName="To Do list",
+            CFBundleTypeRole="Editor",
+            NSDocumentClass="ToDoDocument",
+            CFBundleTypeIconFile="ToDoDoc.icns",
+            CFBundleTypeExtensions=["ToDo"],
+            CFBundleTypeOSTypes=["ToDo"],
         ),
     ],
-    CFBundleName='To Do',
+    CFBundleName="To Do",
 )
 
 setup(
     app=["main.py"],
     data_files=["English.lproj" ] + images + icons,
     options=dict(py2app=dict(plist=plist)),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
