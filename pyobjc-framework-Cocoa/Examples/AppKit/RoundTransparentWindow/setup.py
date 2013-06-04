45f97777c2c1d747a7f4bbe1d0a24792d5400e92
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:21 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/AppKit/RoundTransparentWindow/setup.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/RoundTransparentWindow/setup.py
@@ -4,10 +4,13 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 setup(
     app=["RoundTransparentWindow.py"],
     data_files=["MainMenu.nib", "circle.tif", "pentagram.tif" ],
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
