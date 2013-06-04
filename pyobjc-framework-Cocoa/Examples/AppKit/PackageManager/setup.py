def2af2fd68d555eef6edb476e45343538350876
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:16 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/AppKit/PackageManager/setup.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/PackageManager/setup.py
@@ -6,8 +6,7 @@ Usage:
 """
 # This is very likely incorrect, as the application is non-existant at the
 # moment.
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 import os
 import sys
@@ -16,34 +15,42 @@ import glob
 DB_FILE_TYPE="Python Package Database"
 
 plist = dict(
-    CFBundleName='Package Manager',
-    CFBundleIconFile='PackageManager.icns',
+    CFBundleName="Package Manager",
+    CFBundleIconFile="PackageManager.icns",
     CFBundleDocumentTypes=[
         dict(
             CFBundleTypeName=DB_FILE_TYPE,
-            CFBundleTypeRole='Editor',
-            NSDocumentClass='PackageDatabase',
-            # CFBundleTypeIconFile='Package Database.icns',
-            CFBundleTypeExtensions = ['packman', 'plist' ],
+            CFBundleTypeRole="Editor",
+            NSDocumentClass="PackageDatabase",
+            # CFBundleTypeIconFile="Package Database.icns",
+            CFBundleTypeExtensions = ["packman", "plist" ],
             CFBundleTypeOSTypes=[],
         ),
     ],
 
-    CFBundleGetInfoString='1.0, Copyright 2004 Ronald Oussoren',
-    CFBundleIdentifier='net.sf.pyobjc.PackageManager',
-    CFBundleShortVersionString='1.0',
-    CFBundleVersion='1.0',
+    CFBundleGetInfoString="1.0, Copyright 2004 Ronald Oussoren",
+    CFBundleIdentifier="net.sf.pyobjc.PackageManager",
+    CFBundleShortVersionString="1.0",
+    CFBundleVersion="1.0",
 
-    # We need at least Panther, it may work on Jaguar but I've not yet
+    # We need at least Panther, it may work on Jaguar but I"ve not yet
     # verified if it should work.
-    LSMinimumSystemVersion='10.3.0',
+    LSMinimumSystemVersion="10.3.0",
 
-    # We're not apple-scriptable
-    NSAppleScriptEnabled='No',
+    # We"re not apple-scriptable
+    NSAppleScriptEnabled="No",
 )
 
 setup(
     app=["packman.py"],
     data_files=glob.glob("Resources/*"),
-    options=dict(py2app=dict(plist=plist)),
+    options=dict(
+        py2app=dict(
+            plist=plist
+        )
+    ),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
