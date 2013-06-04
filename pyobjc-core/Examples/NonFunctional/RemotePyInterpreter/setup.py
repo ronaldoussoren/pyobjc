3ecb5e91711f7bacb3b01e0042ed1aa40651ed67
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:38:46 +0200

    Cleanup setup.py files in examples

--- pyobjc-core/Examples/NonFunctional/RemotePyInterpreter/setup.py
+++ pyobjc-core/Examples/NonFunctional/RemotePyInterpreter/setup.py
@@ -4,29 +4,39 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 plist = dict(
-    CFBundleIdentifier=u'net.sf.pyobjc.RemotePyInterpreter',
+    CFBundleIdentifier="net.sf.pyobjc.RemotePyInterpreter",
     CFBundleDocumentTypes=[
         dict(
             CFBundleTypeExtensions=[
-                u'RemotePyInterpreter',
-                u'*',
+                "RemotePyInterpreter",
+                "*",
             ],
-            CFBundleTypeName=u'RemotePyInterpreter Session',
-            CFBundleTypeRole=u'Editor',
-            NSDocumentClass=u'RemotePyInterpreterDocument',
+            CFBundleTypeName="RemotePyInterpreter Session",
+            CFBundleTypeRole="Editor",
+            NSDocumentClass="RemotePyInterpreterDocument",
         ),
     ],
 )
 
-REMOTE_REQUIREMENTS = ['tcpinterpreter', 'netrepr', 'remote_console', 'remote_pipe', 'remote_bootstrap']
-DATA_FILES = ['English.lproj'] + [(s + '.py') for s in REMOTE_REQUIREMENTS]
+REMOTE_REQUIREMENTS = [
+    "tcpinterpreter",
+    "netrepr",
+    "remote_console",
+    "remote_pipe",
+    "remote_bootstrap"
+]
+
+DATA_FILES = ["English.lproj"] + [(s + ".py") for s in REMOTE_REQUIREMENTS]
 
 setup(
     app=["RemotePyInterpreter.py"],
     data_files=DATA_FILES,
     options=dict(py2app=dict(plist=plist)),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
