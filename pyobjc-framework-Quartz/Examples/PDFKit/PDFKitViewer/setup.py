2226d1e605278a1673bfaa35cbc91522b0b80db9
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:39:49 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Quartz/Examples/PDFKit/PDFKitViewer/setup.py
+++ pyobjc-framework-Quartz/Examples/PDFKit/PDFKitViewer/setup.py
@@ -4,15 +4,22 @@ Script for building the example.
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 setup(
-    name='PDFKitViewer',
+    name="PDFKitViewer",
     app=["main.py"],
-    data_files=["English.lproj", 'pdfkitviewer.icns'],
+    data_files=[
+        "English.lproj",
+        "pdfkitviewer.icns"
+    ],
     options=dict(
         py2app=dict(
-            plist='Info.plist',
+            plist="Info.plist",
         )),
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+        "pyobjc-framework-Quartz",
+    ]
 )
