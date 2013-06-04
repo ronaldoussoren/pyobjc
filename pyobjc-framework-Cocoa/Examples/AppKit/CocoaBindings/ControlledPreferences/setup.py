8674dcdb95f46987b67d386a5f84bb9e8a9c94e0
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-04 21:38:58 +0200

    Cleanup setup.py files in examples

--- pyobjc-framework-Cocoa/Examples/AppKit/CocoaBindings/ControlledPreferences/setup.py
+++ pyobjc-framework-Cocoa/Examples/AppKit/CocoaBindings/ControlledPreferences/setup.py
@@ -4,11 +4,14 @@ Script for building the example:
 Usage:
     python setup.py py2app
 """
-from distutils.core import setup
-import py2app
+from setuptools import setup
 
 setup(
     name="ControlledPreferences",
     app=["ControlledPreferences.py"],
     data_files=["English.lproj"],
+    setup_requires=[
+        "py2app",
+        "pyobjc-framework-Cocoa",
+    ]
 )
