e12dfe0f6724b526ceb368aa19f1ae9b90d71d9f
Ronald Oussoren <ronaldoussoren@mac.com>
2013-06-06 09:42:45 +0200

    Automaticly install py2app when pyobjc is installed.

--- pyobjc/setup.py
+++ pyobjc/setup.py
@@ -21,6 +21,7 @@ VERSION="3.0a1"
 # packaged into an egg file because of this.
 
 REQUIRES=[
+        'py2app>=0.7',
         'pyobjc-core=='+VERSION,
         'pyobjc-framework-AddressBook=='+VERSION,
         'pyobjc-framework-AppleScriptKit=='+VERSION,
