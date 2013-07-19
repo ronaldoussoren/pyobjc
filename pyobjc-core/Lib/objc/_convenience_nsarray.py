218d75304c6a67599c278b6c34b57cb2b2f845fd
Ronald Oussoren <ronaldoussoren@mac.com>
2013-07-19 16:59:08 +0200

    Cleanup _convenience_nsarray, add tests for NSMutableArray.sort and fix crash in that method

--- pyobjc-core/Lib/objc/_convenience_nsarray.py
+++ pyobjc-core/Lib/objc/_convenience_nsarray.py
@@ -4,7 +4,7 @@ Convenience interface for NSArray/NSMutableArray
 __all__ = ()
 
 from objc._convenience import addConvenienceForClass, container_wrap, container_unwrap
-from objc._objc import lookUpClass, registerMetaDataForSelector
+from objc._objc import lookUpClass, registerMetaDataForSelector, _C_NSInteger, _C_ID
 from objc._objc import _NSNotFound as NSNotFound
 
 import collections
@@ -19,11 +19,42 @@ collections.MutableSequence.register(NSMutableArray)
 if sys.version_info[0] == 2:
     INT_TYPES = (int, long)
     STR_TYPES = (str, unicode)
+
 else:
     INT_TYPES = int
     STR_TYPES = str
 
-def reverse_exchangeObjectAtIndex_withObjectAtIndex_(self):
+
+registerMetaDataForSelector(
+    b"NSObject", b"sortUsingFunction:context:",
+    dict(
+        arguments={
+            2:  {
+                    'callable': {
+                        'retval': { 'type': _C_NSInteger },
+                        'arguments': {
+                            0: { 'type': _C_ID },
+                            1: { 'type': _C_ID },
+                            2: { 'type': _C_ID },
+                        }
+                    },
+                    'callable_retained': False,
+            },
+            3:  { 'type': _C_ID },
+        },
+    )
+)
+
+
+def _ensure_array(anArray):
+    """ Return *anArray* as a list, tuple or NSArray """
+    if not isinstance(anArray, (NSArray, list, tuple)):
+        anArray = list(anArray)
+    return anArray
+
+
+def nsarray_reverse(self):
+    """ Reverse an array """
     begin = 0
     end = len(self) - 1
     while begin < end:
@@ -31,48 +62,45 @@ def reverse_exchangeObjectAtIndex_withObjectAtIndex_(self):
         begin += 1
         end -= 1
 
-def ensureArray(anArray):
-    if not isinstance(anArray, (NSArray, list, tuple)):
-        anArray = list(anArray)
-    return anArray
 
+def nsarray_extend(self, anArray):
+    self.addObjectsFromArray_(_ensure_array(anArray))
 
-def extend_addObjectsFromArray_(self, anArray):
-    self.addObjectsFromArray_(ensureArray(anArray))
 
 _index_sentinel=object()
-def index_indexOfObject_inRange_(self, item, start=0, stop=_index_sentinel):
+def nsarray_index(self, item, start=0, stop=_index_sentinel):
     if start == 0 and stop is _index_sentinel:
         res = self.indexOfObject_(container_wrap(item))
         if res == NSNotFound:
             raise ValueError("%s.index(x): x not in list" % (type(self).__name__,))
+
     else:
-        l = len(self)
+        itemcount = self.count()
         if start < 0:
-            start = l + start
+            start = itemcount + start
             if start < 0:
                 start = 0
 
         if stop is not _index_sentinel:
             if stop < 0:
-                stop = l + stop
+                stop = itemcount + stop
                 if stop < 0:
                     stop = 0
         else:
-            stop = l
-
-        itemcount = len(self)
+            stop = itemcount
 
         if itemcount == 0:
             raise ValueError("%s.index(x): x not in list" % (type(self).__name__,))
 
         if start >= itemcount:
-            start = itemcount - 1
+            raise ValueError("%s.index(x): x not in list" % (type(self).__name__,))
+
         if stop >= itemcount:
             stop = itemcount - 1
 
         if stop <= start:
             ln = 0
+
         else:
             ln = stop - start
 
@@ -85,23 +113,21 @@ def index_indexOfObject_inRange_(self, item, start=0, stop=_index_sentinel):
         res = self.indexOfObject_inRange_(item, (start, ln))
         if res == NSNotFound:
             raise ValueError("%s.index(x): x not in list" % (type(self).__name__,))
+
     return res
 
-def insert_insertObject_atIndex_(self, idx, item):
+
+def nsarray_insert(self, idx, item):
     if idx < 0:
-        idx += len(self)
+        idx += self.count()
         if idx < 0:
             raise IndexError("list index out of range")
     self.insertObject_atIndex_(container_wrap(item), idx)
 
 
-def __getitem__objectAtIndex_(self, idx):
+def nsarray__getitem__(self, idx):
     if isinstance(idx, slice):
         start, stop, step = idx.indices(len(self))
-        #if step == 1:
-        #    m = getattr(self, 'subarrayWithRange_', None)
-        #    if m is not None:
-        #        return m((start, stop - start))
         return [self[i] for i in range(start, stop, step)]
 
     elif not isinstance(idx, INT_TYPES):
@@ -114,70 +140,69 @@ def __getitem__objectAtIndex_(self, idx):
 
     return container_unwrap(self.objectAtIndex_(idx), RuntimeError)
 
-def __getslice__objectAtIndex_(self, i, j):
-    i = max(i, 0); j = max(j, 0)
-    return __getitem__objectAtIndex_(self, slice(i, j))
 
-def __delitem__removeObjectAtIndex_(self, idx):
+def nsarray__delitem__(self, idx):
     if isinstance(idx, slice):
-        start, stop, step = idx.indices(len(self))
+        start, stop, step = idx.indices(self.count())
         if step == 1:
             if start > stop:
                 start, stop = stop, start
-            m = getattr(self, 'removeObjectsInRange_', None)
-            if m is not None:
-                m((start, stop - start))
-                return
+
+            return self.removeObjectsInRange_((start, stop-start))
+
         r = reversed(range(start, stop, step))
         for i in r:
             self.removeObjectAtIndex_(i)
         return
+
     if idx < 0:
-        idx += len(self)
+        idx += self.count()
         if idx < 0:
             raise IndexError("list index out of range")
 
     self.removeObjectAtIndex_(idx)
 
-def __delslice__removeObjectAtIndex_(self, i, j):
-    __delitem__removeObjectAtIndex_(self, slice(i, j))
 
-def pop_removeObjectAtIndex_(self, idx=-1):
-    length = len(self)
+def nsarray_pop(self, idx=-1):
+    length = self.count()
     if length <= 0:
         raise IndexError("pop from empty list")
+
     elif idx >= length or (idx + length) < 0:
         raise IndexError("pop index out of range")
+
     elif idx < 0:
         idx += len(self)
         if idx < 0:
             raise IndexError("list index out of range")
-    rval = self[idx]
+
+    rval = self.objectAtIndex_(idx)
     self.removeObjectAtIndex_(idx)
     return rval
 
-def remove_removeObjectAtIndex_(self, obj):
-    idx = self.index(obj)
+
+def nsarray_remove(self, obj):
+    idx = self.indexOfObject_(obj)
+    if idx == NSNotFound:
+        raise ValueError("%s.remove(x): x not in list" % (type(self).__name__,))
     self.removeObjectAtIndex_(idx)
 
-def __setitem__replaceObjectAtIndex_withObject_(self, idx, anObject):
+
+def nsarray__setitem__(self, idx, anObject):
     if isinstance(idx, slice):
-        start, stop, step = idx.indices(len(self))
+        start, stop, step = idx.indices(self.count())
         if step >=0:
             if stop <= start:
                 # Empty slice: insert values
                 stop = start
+
         elif start <= stop:
             start = stop
 
-        if step == 1:
-            m = getattr(self, 'replaceObjectsInRange_withObjectsFromArray_', None)
-            if m is not None:
-                m((start, stop - start), ensureArray(anObject))
-                return
+        anObject = _ensure_array(anObject)
 
-        if not isinstance(anObject, (NSArray, list, tuple)):
-            anObject = list(anObject)
+        if step == 1:
+            return self.replaceObjectsInRange_withObjectsFromArray_((start, stop - start), anObject)
 
         slice_len = len(range(start, stop, step))
         if slice_len != len(anObject):
@@ -187,8 +212,10 @@ def __setitem__replaceObjectAtIndex_withObject_(self, idx, anObject):
         if step > 0:
             if anObject is self:
                 toAssign = list(anObject)
+
             else:
                 toAssign = anObject
+
             for inIdx, outIdx in enumerate(range(start, stop, step)):
                 self.replaceObjectAtIndex_withObject_(outIdx, toAssign[inIdx])
 
@@ -200,58 +227,33 @@ def __setitem__replaceObjectAtIndex_withObject_(self, idx, anObject):
                 toAssign = list(anObject)
             else:
                 toAssign = anObject
+
             for inIdx, outIdx in enumerate(range(start, stop, step)):
                 self.replaceObjectAtIndex_withObject_(outIdx, toAssign[inIdx])
-
 
     elif not isinstance(idx, INT_TYPES):
         raise TypeError("index is not an integer")
 
     else:
-
         if idx < 0:
-            idx += len(self)
+            idx += self.count()
             if idx < 0:
                 raise IndexError("list index out of range")
 
         self.replaceObjectAtIndex_withObject_(idx, anObject)
 
-def __setslice__replaceObjectAtIndex_withObject_(self, i, j, seq):
-    i = max(i, 0)
-    j = max(j, 0)
-    __setitem__replaceObjectAtIndex_withObject_(self, slice(i, j), seq)
 
-
-
-registerMetaDataForSelector(b"NSObject", b"sortUsingFunction:context:",
-        dict(
-            arguments={
-                2:  {
-                        'callable': {
-                            'reval': 'i',
-                            'arguments': {
-                                0: { 'type': b'@' },
-                                1: { 'type': b'@' },
-                                2: { 'type': b'@' },
-                            }
-                        },
-                        'callable_retained': False,
-                },
-                3:  { 'type': b'@' },
-            },
-        ))
-
-
-NSMutableArray = lookUpClass('NSMutableArray')
 def nsarray_add(self, other):
     result = NSMutableArray.arrayWithArray_(self)
-    result.extend(other)
+    result.addObjectsFromArray_(_ensure_array(other))
     return result
+
 
 def nsarray_radd(self, other):
     result = NSMutableArray.arrayWithArray_(other)
-    result.extend(self)
+    result.addObjectsFromArray_(self)
     return result
+
 
 def nsarray_mul(self, other):
     """
@@ -267,8 +269,8 @@ def nsarray_mul(self, other):
     n = 1
     tmp = self
     while other:
-        if other & n != 0:
-            result.extend(tmp)
+        if (other & n) != 0:
+            result.addObjectsFromArray_(tmp)
             other -= n
 
         if other:
@@ -276,7 +278,6 @@ def nsarray_mul(self, other):
             tmp = tmp.arrayByAddingObjectsFromArray_(tmp)
 
     return result
-
 
 
 def nsarray_new(cls, sequence=None):
@@ -288,8 +289,6 @@ def nsarray_new(cls, sequence=None):
 
     else:
         if not isinstance(sequence, (list, tuple)):
-            # FIXME: teach bridge to treat range and other list-lik
-            # types correctly
             return NSArray.arrayWithArray_(list(sequence))
 
         return NSArray.arrayWithArray_(sequence)
@@ -304,44 +303,122 @@ def nsmutablearray_new(cls, sequence=None):
 
     else:
         if not isinstance(sequence, (list, tuple)):
-            # FIXME: teach bridge to treat range and other list-lik
-            # types correctly
             return NSMutableArray.arrayWithArray_(list(sequence))
 
         return NSMutableArray.arrayWithArray_(sequence)
 
 
-def containsObject_has_key(self, elem):
+def nsarray__contains__(self, elem):
     return bool(self.containsObject_(container_wrap(elem)))
 
 
+def nsarray_append(self, anObject):
+    self.addObject_(container_wrap(anObject))
+
+
+def nsarray_clear(self):
+    self.removeAllObjects()
+
+
+if sys.version_info[0] == 2:
+    def nsarray_sort(self, cmp=cmp, key=None, reverse=False):
+        if key is None:
+            if reverse:
+                def sort_func(a, b, cmp):
+                    return -cmp(a, b)
+
+            else:
+                def sort_func(a, b, cmp):
+                    return cmp(a, b)
+        else:
+            if reverse:
+                def sort_func(a, b, cmp):
+                    return -cmp(key(a), key(b))
+            else:
+                def sort_func(a, b, cmp):
+                    return cmp(key(a), key(b))
+
+        self.sortUsingFunction_context_(sort_func, cmp)
+
+else:
+    def nsarray_sort(self, key=lambda x: x, reverse=False):
+        if reverse:
+            def sort_func(a, b, _):
+                a = key(a)
+                b = key(b)
+                if a < b:
+                    return 1
+
+                elif b < a:
+                    return -1
+
+                else:
+                    return 0
+
+        else:
+            def sort_func(a, b, _):
+                a = key(a)
+                b = key(b)
+
+                if a < b:
+                    return -1
+                elif b < a:
+                    return 1
+                else:
+                    return 0
+
+        self.sortUsingFunction_context_(sort_func, None)
+
+
+
 addConvenienceForClass('NSArray', (
+    ('__new__', staticmethod(nsarray_new)),
     ('__add__', nsarray_add),
     ('__radd__', nsarray_radd),
     ('__mul__', nsarray_mul),
     ('__rmul__', nsarray_mul),
-    ('__new__', staticmethod(nsarray_new)),
     ('__len__', lambda self: self.count()),
-    ('__contains__', containsObject_has_key),
-    ('index', index_indexOfObject_inRange_),
-    ('remove', remove_removeObjectAtIndex_),
-    ('pop', pop_removeObjectAtIndex_),
-    ('__delitem__', __delitem__removeObjectAtIndex_),
-    ('__delslice__', __delslice__removeObjectAtIndex_), # Python 2
+    ('__contains__', nsarray__contains__),
+    ('__getitem__', nsarray__getitem__),
     ('__copy__', lambda self: self.copy()),
-    ('__getitem__', __getitem__objectAtIndex_),
-    ('__getslice__', __getslice__objectAtIndex_),
+    ('index', nsarray_index),
+    ('remove', nsarray_remove),
+    ('pop', nsarray_pop),
 ))
 
+
 addConvenienceForClass('NSMutableArray', (
-    ('__setitem__', __setitem__replaceObjectAtIndex_withObject_),
-    ('__setslice__', __setslice__replaceObjectAtIndex_withObject_),
     ('__new__', staticmethod(nsmutablearray_new)),
-    ('extend', extend_addObjectsFromArray_),
-    ('append', lambda self, item: self.addObject_(container_wrap(item))),
-    #('sort', sort),
-    ('insert', insert_insertObject_atIndex_),
-    ('reverse', reverse_exchangeObjectAtIndex_withObjectAtIndex_),
-    ('clear', lambda self: self.removeAllObjects()),
     ('__copy__', lambda self: self.mutableCopy()),
+    ('__setitem__', nsarray__setitem__),
+    ('__delitem__', nsarray__delitem__),
+    ('extend', nsarray_extend),
+    ('append', nsarray_append),
+    ('sort', nsarray_sort),
+    ('insert', nsarray_insert),
+    ('reverse', nsarray_reverse),
+    ('clear', nsarray_clear),
 ))
+
+
+if sys.version_info[0] == 2:
+    def nsarray__getslice__(self, i, j):
+        i = max(i, 0); j = max(j, 0)
+        return nsarray__getitem__(self, slice(i, j))
+
+    def nsarray__setslice__(self, i, j, seq):
+        i = max(i, 0)
+        j = max(j, 0)
+        nsarray__setitem__(self, slice(i, j), seq)
+
+    def nsarray__delslice__(self, i, j):
+        nsarray__delitem__(self, slice(i, j))
+
+    addConvenienceForClass('NSArray', (
+        ('__getslice__', nsarray__getslice__),
+    ))
+
+    addConvenienceForClass('NSMutableArray', (
+        ('__setslice__', nsarray__setslice__),
+        ('__delslice__', nsarray__delslice__),
+    ))
--- pyobjc-core/PyObjCTest/test_array_interface.py
+++ pyobjc-core/PyObjCTest/test_array_interface.py
@@ -263,8 +263,36 @@ class MutableArrayTest (list_tests.CommonTest):
         pass
 
 
-    # Disabled for now due to crash:
-    def test_sort(self): pass
+    def test_sort(self):
+        base = [2,1,5,4,3]
+
+        l = self.type2test(base)
+        l.sort()
+        self.assertEqual(l, self.type2test([1,2,3,4,5]))
+
+        l = self.type2test(base)
+        l.sort(reverse=True)
+        self.assertEqual(l, self.type2test([5,4,3,2,1]))
+
+        base = [-2,1,-5,4,3]
+        l = self.type2test(base)
+        l.sort(key=lambda x:abs(x))
+        self.assertEqual(l, self.type2test([1,-2,3,4,-5]))
+
+        l = self.type2test(base)
+        l.sort(key=lambda x:abs(x), reverse=True)
+        self.assertEqual(l, self.type2test([-5,4,3,-2,1]))
+
+        if sys.version_info[0] == 2:
+            base = [-2,1,-5,4,3]
+            l = self.type2test(base)
+            l.sort(cmp=lambda x, y: cmp(abs(x), abs(y)))
+            self.assertEqual(l, self.type2test([1,-2,3,4,-5]))
+
+            l = self.type2test(base)
+            l.sort(cmp=lambda x, y: cmp(abs(x), abs(y)), reverse=True)
+            self.assertEqual(l, self.type2test([-5,4,3,-2,1]))
+
 
 if __name__ == "__main__":
     main()
