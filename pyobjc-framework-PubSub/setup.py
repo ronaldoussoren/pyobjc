b0859076fcb5b7718f8fae4996747c7d686a1484
Ronald Oussoren <ronaldoussoren@mac.com>
2013-12-12 18:47:31 +0100

    Note that the framework is deprecated

--- pyobjc-framework-PubSub/setup.py
+++ pyobjc-framework-PubSub/setup.py
@@ -7,6 +7,8 @@ These wrappers don't include documentation, please check Apple's documention
 for information on how to use this framework and PyObjC's documentation
 for general tips and tricks regarding the translation between Python
 and (Objective-)C frameworks
+
+Note that this framework is deprecated in OSX 10.9
 '''
 from pyobjc_setup import setup
 
