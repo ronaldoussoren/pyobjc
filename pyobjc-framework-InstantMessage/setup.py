1b97354c9bbb4384bd668d45dda6771d48b06c5d
Ronald Oussoren <ronaldoussoren@mac.com>
2013-12-12 18:42:54 +0100

    Note that the framework is deprecated

--- pyobjc-framework-InstantMessage/setup.py
+++ pyobjc-framework-InstantMessage/setup.py
@@ -7,6 +7,9 @@ These wrappers don't include documentation, please check Apple's documention
 for information on how to use this framework and PyObjC's documentation
 for general tips and tricks regarding the translation between Python
 and (Objective-)C frameworks
+
+Note that this framework is deprecated in OSX 10.9, use the Social framework
+instead if you target that OSX release.
 '''
 from pyobjc_setup import setup
 setup(
