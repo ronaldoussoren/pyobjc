237337b345e704bffe94eb6250a0f09581752b6c
Ronald Oussoren <ronaldoussoren@mac.com>
2014-05-30 10:53:32 +0200

    Use -O3 instead of -O4 to avoid warnings with Xcode 5.1

--- pyobjc-core/setup.py
+++ pyobjc-core/setup.py
@@ -116,9 +116,9 @@ else:
     cfg_vars = get_config_vars()
     for k in cfg_vars:
         if isinstance(cfg_vars[k], str) and '-O2' in cfg_vars[k]:
-            cfg_vars[k] = cfg_vars[k].replace('-O2', '-O4')
+            cfg_vars[k] = cfg_vars[k].replace('-O2', '-O3')
         elif isinstance(cfg_vars[k], str) and '-O3' in cfg_vars[k]:
-            cfg_vars[k] = cfg_vars[k].replace('-O3', '-O4')
+            cfg_vars[k] = cfg_vars[k].replace('-O3', '-O3')
 
 
 # XXX: bug in CPython 3.4 repository leaks unwanted compiler flag into disutils.
