52e668252154923a716c46134216862986b8819e
Ronald Oussoren <ronaldoussoren@mac.com>
2014-12-31 16:52:20 +0100

    Add missing tests.

---
+++ pyobjc-framework-InstantMessage/PyObjCTest/test_imavcontrol.py
@@ -0,0 +1,17 @@
+from PyObjCTools.TestSupport import *
+from InstantMessage import *
+
+try:
+    unicode
+except NameError:
+    unicode = str
+
+class TestIMControl (TestCase):
+    @min_os_level('10.6')
+    def testMethods(self):
+        self.assertArgIsSEL(IMAVControl.setAction_, 0, b'v@:@')
+        self.assertResultIsBOOL(IMAVControl.isEnabled)
+        self.assertArgIsBOOL(IMAVControl.setEnabled_, 0)
+
+if __name__ == "__main__":
+    main()
