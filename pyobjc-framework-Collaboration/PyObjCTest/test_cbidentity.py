dd6e6d5860e40bc66c8d13dbff6b5e7ed7d709bb
Ronald Oussoren <ronaldoussoren@mac.com>
2015-01-01 21:45:31 +0100

    No updates needed for Collaboration

--- pyobjc-framework-Collaboration/PyObjCTest/test_cbidentity.py
+++ pyobjc-framework-Collaboration/PyObjCTest/test_cbidentity.py
@@ -6,6 +6,8 @@ class TestCBIdentity (TestCase):
     def testMethods(self):
         self.assertResultIsBOOL(CBIdentity.isHidden)
         self.assertResultIsBOOL(CBIdentity.isMemberOfGroup_)
+        self.assertResultIsBOOL(CBUserIdentity.isEnabled)
+        self.assertResultIsBOOL(CBUserIdentity.authenticateWithPassword_)
 
 
 if __name__ == "__main__":
