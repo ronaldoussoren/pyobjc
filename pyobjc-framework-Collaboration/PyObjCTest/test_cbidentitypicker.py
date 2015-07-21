ed311759c3d5b54ad5870a640e8fb30d8c529895
Ronald Oussoren <ronaldoussoren@mac.com>
2015-07-21 09:11:40 +0200

    Test updates

--- pyobjc-framework-Collaboration/PyObjCTest/test_cbidentitypicker.py
+++ pyobjc-framework-Collaboration/PyObjCTest/test_cbidentitypicker.py
@@ -10,6 +10,9 @@ class TestCBIdentityPicker (TestCase):
         self.assertArgIsSEL(CBIdentityPicker.runModalForWindow_modalDelegate_didEndSelector_contextInfo_, 2, b'v@:@' + objc._C_NSInteger + b'^v')
         self.assertArgHasType(CBIdentityPicker.runModalForWindow_modalDelegate_didEndSelector_contextInfo_, 3, b'^v')
 
+    @min_os_level('10.11')
+    def testMethods10_11(self):
+        self.assertArgIsBlock(CBIdentityPicker.runModalForWindow_completionHandler_, 1, b'vI')
 
 
 if __name__ == "__main__":
