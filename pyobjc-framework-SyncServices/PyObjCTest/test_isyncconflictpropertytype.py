64eec6d59ed1f45df79653115fb0c977dc9cc78c
Ronald Oussoren <ronaldoussoren@mac.com>
2015-11-25 12:51:41 +0100

    Add missing files...

---
+++ pyobjc-framework-SyncServices/PyObjCTest/test_isyncconflictpropertytype.py
@@ -0,0 +1,24 @@
+
+from PyObjCTools.TestSupport import *
+import SyncServices
+
+try:
+    unicode
+except NameError:
+    unicode = str
+
+class TestISyncConflictPropertyTypeHelper (SyncServices.NSObject):
+    def isRelationship(self): return 1
+    def isToMany(self): return 1
+    def isRequired(self): return 1
+
+class TestISyncConflictPropertyType (TestCase):
+    def testProtocols(self):
+        objc.protocolNamed('ISyncConflictPropertyType')
+
+        self.assertResultIsBOOL(TestISyncConflictPropertyTypeHelper.isRelationship)
+        self.assertResultIsBOOL(TestISyncConflictPropertyTypeHelper.isToMany)
+        self.assertResultIsBOOL(TestISyncConflictPropertyTypeHelper.isRequired)
+
+if __name__ == "__main__":
+    main()
