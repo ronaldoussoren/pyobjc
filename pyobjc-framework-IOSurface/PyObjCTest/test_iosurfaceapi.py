713a63c25c0b76d3f21e7337201956f9a1d8c0d6
Ronald Oussoren <ronaldoussoren@mac.com>
2016-01-03 13:06:52 +0100

    Add bindings for the IOSurface framwork.

---
+++ pyobjc-framework-IOSurface/PyObjCTest/test_iosurfaceapi.py
@@ -0,0 +1,109 @@
+from PyObjCTools.TestSupport import *
+
+import IOSurface
+
+class TestIOSurfaceAPI (TestCase):
+    def testCFTypes(self):
+        self.assertIsCFType(IOSurface.IOSurfaceRef)
+
+    @min_os_level('10.6')
+    def testConstants(self):
+        self.assertIsInstance(IOSurface.kIOSurfaceAllocSize, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfaceWidth, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfaceHeight, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfaceBytesPerRow, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfaceBytesPerElement, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfaceElementWidth, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfaceElementHeight, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfaceOffset, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneInfo, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneWidth, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneHeight, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneBytesPerRow, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneOffset, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneSize, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneBase, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneBytesPerElement, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneElementWidth, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePlaneElementHeight, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfaceCacheMode, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfaceIsGlobal, unicode)
+        self.assertIsInstance(IOSurface.kIOSurfacePixelFormat, unicode)
+
+        self.assertEqual(IOSurface.kIOSurfaceLockReadOnly, 0x00000001)
+        self.assertEqual(IOSurface.kIOSurfaceLockAvoidSync, 0x00000002)
+
+    @min_os_level('10.6')
+    def testFunctions(self):
+        self.assertIsInstance(IOSurface.IOSurfaceGetTypeID(), (int, long))
+
+        self.assertResultIsCFRetained(IOSurface.IOSurfaceCreate)
+
+        self.assertResultIsCFRetained(IOSurface.IOSurfaceLookup)
+
+        IOSurface.IOSurfaceGetID
+
+        self.assertArgIsInOut(IOSurface.IOSurfaceLock, 2)
+        self.assertArgIsInOut(IOSurface.IOSurfaceUnlock, 2)
+
+        IOSurface.IOSurfaceGetAllocSize
+        IOSurface.IOSurfaceGetWidth
+        IOSurface.IOSurfaceGetHeight
+        IOSurface.IOSurfaceGetBytesPerElement
+        IOSurface.IOSurfaceGetBytesPerRow
+
+        self.assertResultHasType(IOSurface.IOSurfaceGetBaseAddress, b'^v')
+        self.assertResultIsVariableSize(IOSurface.IOSurfaceGetBaseAddress)
+
+        IOSurface.IOSurfaceGetElementWidth
+        IOSurface.IOSurfaceGetElementHeight
+
+        IOSurface.IOSurfaceGetPixelFormat
+
+        IOSurface.IOSurfaceGetSeed
+        IOSurface.IOSurfaceGetPlaneCount
+        IOSurface.IOSurfaceGetWidthOfPlane
+        IOSurface.IOSurfaceGetHeightOfPlane
+        IOSurface.IOSurfaceGetBytesPerElementOfPlane
+        IOSurface.IOSurfaceGetBytesPerRowOfPlane
+
+        self.assertResultHasType(IOSurface.IOSurfaceGetBaseAddressOfPlane, b'^v')
+        self.assertResultIsVariableSize(IOSurface.IOSurfaceGetBaseAddressOfPlane)
+
+        IOSurface.IOSurfaceGetElementWidthOfPlane
+        IOSurface.IOSurfaceGetElementHeightOfPlane
+
+        self.assertArgHasType(IOSurface.IOSurfaceSetValue, 2, objc._C_ID)
+
+        self.assertResultHasType(IOSurface.IOSurfaceCopyValue, objc._C_ID)
+
+        IOSurface.IOSurfaceRemoveValue
+        IOSurface.IOSurfaceSetValues
+
+        self.assertResultIsCFRetained(IOSurface.IOSurfaceCopyAllValues)
+
+        IOSurface.IOSurfaceRemoveAllValues
+
+        IOSurface.IOSurfaceCreateMachPort
+
+        self.assertResultIsCFRetained(IOSurface.IOSurfaceLookupFromMachPort)
+
+        IOSurface.IOSurfaceGetPropertyMaximum
+        IOSurface.IOSurfaceGetPropertyAlignment
+        IOSurface.IOSurfaceAlignProperty
+        IOSurface.IOSurfaceIncrementUseCount
+        IOSurface.IOSurfaceDecrementUseCount
+        IOSurface.IOSurfaceGetUseCount
+
+        self.assertResultIsBOOL(IOSurface.IOSurfaceIsInUse)
+
+    @min_os_level('10.7')
+    def testFunctions10_7(self):
+        IOSurface.IOSurfaceCreateXPCObject
+
+        self.assertResultIsCFRetained(IOSurface.IOSurfaceLookupFromXPCObject)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ pyobjc-framework-IOSurface/setup.py
@@ -0,0 +1,27 @@
+'''
+Wrappers for the "IOSurface" framework on MacOS X.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+'''
+
+from pyobjc_setup import setup
+import os
+
+setup(
+    name='pyobjc-framework-IOSurface',
+    version="3.2a1",
+    description = "Wrappers for the framework IOSurface on Mac OS X",
+    long_description=__doc__,
+    packages = [ "IOSurface" ],
+    setup_requires = [
+        'pyobjc-core>=3.2a1',
+    ],
+    install_requires = [
+        'pyobjc-core>=3.2a1',
+        'pyobjc-framework-Cocoa>=3.2a1',
+    ],
+    min_os_level="10.6",
+)
