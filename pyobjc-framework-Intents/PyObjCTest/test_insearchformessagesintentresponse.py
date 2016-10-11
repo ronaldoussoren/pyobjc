d80eed87d4e3243e4a535a2701d82c78edb716db
Ronald Oussoren <ronaldoussoren@mac.com>
2016-10-12 00:06:23 +0200

    10.12 updates

--- pyobjc-framework-Intents/PyObjCTest/test_insearchformessagesintentresponse.py
+++ pyobjc-framework-Intents/PyObjCTest/test_insearchformessagesintentresponse.py
@@ -7,13 +7,13 @@ if sys.maxsize > 2 ** 32:
     class TestINSearchForMessagesIntentResponse (TestCase):
         @min_os_level('10.12')
         def testConstants(self):
-            self.assertEqual(Intent.INSearchForMessagesIntentResponseCodeUnspecified, 0)
-            self.assertEqual(Intent.INSearchForMessagesIntentResponseCodeReady, 1)
-            self.assertEqual(Intent.INSearchForMessagesIntentResponseCodeInProgress, 2)
-            self.assertEqual(Intent.INSearchForMessagesIntentResponseCodeSuccess, 3)
-            self.assertEqual(Intent.INSearchForMessagesIntentResponseCodeFailure, 4)
-            self.assertEqual(Intent.INSearchForMessagesIntentResponseCodeFailureRequiringAppLaunch, 5)
-            self.assertEqual(Intent.INSearchForMessagesIntentResponseCodeFailureMessageServiceNotAvailable, 6)
+            self.assertEqual(Intents.INSearchForMessagesIntentResponseCodeUnspecified, 0)
+            self.assertEqual(Intents.INSearchForMessagesIntentResponseCodeReady, 1)
+            self.assertEqual(Intents.INSearchForMessagesIntentResponseCodeInProgress, 2)
+            self.assertEqual(Intents.INSearchForMessagesIntentResponseCodeSuccess, 3)
+            self.assertEqual(Intents.INSearchForMessagesIntentResponseCodeFailure, 4)
+            self.assertEqual(Intents.INSearchForMessagesIntentResponseCodeFailureRequiringAppLaunch, 5)
+            self.assertEqual(Intents.INSearchForMessagesIntentResponseCodeFailureMessageServiceNotAvailable, 6)
 
 
 if __name__ == "__main__":
