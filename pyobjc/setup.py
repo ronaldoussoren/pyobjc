ba0fa9fca50f8b46f6d91cc66ef07a1bb3375d19
Lele Gaifax <lele@metapensiero.it>
2018-05-10 21:48:18 +0200

    Fix typos in the package description

--- pyobjc/setup.py
+++ pyobjc/setup.py
@@ -162,7 +162,7 @@ def framework_requires():
     return result
 
 
-# Some PiPy stuff
+# Some PyPI stuff
 LONG_DESCRIPTION="""
 PyObjC is a bridge between Python and Objective-C.  It allows full
 featured Cocoa applications to be written in pure Python.  It is also
@@ -170,7 +170,7 @@ easy to use other frameworks containing Objective-C class libraries
 from Python and to mix in Objective-C, C and C++ source.
 
 This package is a pseudo-package that will install all pyobjc related
-packages (that is, pyobjc-core as well as wrapppers for frameworks on
+packages (that is, pyobjc-core as well as wrappers for frameworks on
 macOS)
 
 Project links
