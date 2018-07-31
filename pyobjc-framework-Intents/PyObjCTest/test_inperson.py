5795f2fb9d14197a64672202a63f577f82e5186b
Ronald Oussoren <ronaldoussoren@mac.com>
2018-07-31 22:15:13 +0200

    Updates for Xcode 10 beta 4

--- pyobjc-framework-Intents/PyObjCTest/test_inperson.py
+++ pyobjc-framework-Intents/PyObjCTest/test_inperson.py
@@ -7,6 +7,7 @@ if sys.maxsize > 2 ** 32:
     class TestINPerson (TestCase):
         @min_os_level('10.12')
         def testConstants(self):
+            self.assertEqual(Intents.INPersonSuggestionTypeNone, 0)
             self.assertEqual(Intents.INPersonSuggestionTypeSocialProfile, 1)
             self.assertEqual(Intents.INPersonSuggestionTypeInstantMessageAddress, 2)
 
