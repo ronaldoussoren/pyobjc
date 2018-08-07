80205478f3455b41f02cc16d9292d20fa8585a11
Ronald Oussoren <ronaldoussoren@mac.com>
2018-08-07 21:03:27 +0200

    Update for Xcode 10 beta 5

--- pyobjc-framework-AVFoundation/PyObjCTest/test_avsamplebufferrendersynchronizer.py
+++ pyobjc-framework-AVFoundation/PyObjCTest/test_avsamplebufferrendersynchronizer.py
@@ -7,5 +7,9 @@ class TestAVSampleBufferRenderSynchronizer (TestCase):
         self.assertArgIsBlock(AVFoundation.AVSampleBufferRenderSynchronizer.addPeriodicTimeObserverForInterval_queue_usingBlock_, 2, b'v{_CMTime=qiIq}')
         self.assertArgIsBlock(AVFoundation.AVSampleBufferRenderSynchronizer.addBoundaryTimeObserverForTimes_queue_usingBlock_, 2, b'v')
 
+    @min_os_level('10.14')
+    def test_constants10_14(self):
+        self.assertIsInstance(AVFoundation.AVSampleBufferRenderSynchronizerRateDidChangeNotification, unicode)
+
 if __name__ == "__main__":
     main()
