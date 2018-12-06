31ad991094ff6d6c1e001f2555c3bc30d9dc8a68
Ronald Oussoren <ronaldoussoren@mac.com>
2018-12-06 18:52:10 +0100

    /usr/bin/python on 10.14 problem with InputMethodKit bindings

--- pyobjc-framework-InputMethodKit/Modules/_InputMethodKit.m
+++ pyobjc-framework-InputMethodKit/Modules/_InputMethodKit.m
@@ -2,6 +2,18 @@
 #include <Python.h>
 #include "pyobjc-api.h"
 
+#if PyObjC_BUILD_RELEASE >= 1014
+/* The SDK included with Xcode 10 no longer includes a number
+ * of header files, but does #incldue them in <oCarbon/Carbon.h>.
+ *
+ * The defines below avoid trying to import these, which is
+ * safe because we don't use any of the definitions from these files.
+ */
+#define __CARBONSOUND__
+#define __NAVIGATIONSERVICES__
+#endif
+
+
 #import <InputMethodKit/InputMethodKit.h>
 
 /* We include the source code here instead of
