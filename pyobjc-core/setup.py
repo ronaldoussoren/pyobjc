d8dd7cac3034babb3540ada03090aaa1c6926826
Ronald Oussoren <ronaldoussoren@mac.com>
2019-02-10 20:13:19 +0100

    Avoid deprecation warning
    This is safe for older python versions as well because the
    sources are stored using native line endings.

--- pyobjc-core/setup.py
+++ pyobjc-core/setup.py
@@ -366,7 +366,7 @@ class oc_egg_info (egg_info.egg_info):
             fp.write('Project-URL: Issue tracker, https://bitbucket.org/ronaldoussoren/pyobjc/issues?status=new&status=open\n')
 
     def write_header(self, basename, filename):
-        with open(os.path.join('Modules/objc/', os.path.basename(basename)), 'rU') as fp:
+        with open(os.path.join('Modules/objc/', os.path.basename(basename)), 'r') as fp:
             data = fp.read()
         if not self.dry_run:
             if not os.path.exists(os.path.dirname(filename)):
