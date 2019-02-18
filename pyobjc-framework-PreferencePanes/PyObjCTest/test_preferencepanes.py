d89608a4ff06c59f0e914a55b5db537e21c2da54
Ronald Oussoren <ronaldoussoren@mac.com>
2019-02-18 16:59:04 +0100

    Avoid DeprecationWarning

--- pyobjc-framework-PreferencePanes/PyObjCTest/test_preferencepanes.py
+++ pyobjc-framework-PreferencePanes/PyObjCTest/test_preferencepanes.py
@@ -4,8 +4,8 @@ from PyObjCTools.TestSupport import *
 
 class TestPreferencePanes (TestCase):
     def testClasses(self):
-        self.assert_( hasattr(PreferencePanes, 'NSPreferencePane') )
-        self.assert_( isinstance(PreferencePanes.NSPreferencePane, objc.objc_class) )
+        self.assertTrue( hasattr(PreferencePanes, 'NSPreferencePane') )
+        self.assertTrue( isinstance(PreferencePanes.NSPreferencePane, objc.objc_class) )
 
 if __name__ == "__main__":
     main()
