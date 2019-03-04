7b56fddda89947eb6d2ce569ee8c5f8e30d172ab
Ronald Oussoren <ronaldoussoren@mac.com>
2019-03-04 14:36:04 +0100

    Make sure PMRetain and PMRelease actually work

--- pyobjc-framework-ApplicationServices/Lib/PrintCore/__init__.py
+++ pyobjc-framework-ApplicationServices/Lib/PrintCore/__init__.py
@@ -21,4 +21,27 @@ sys.modules['PrintCore'] = mod = objc.ObjCLazyModule('PrintCore',
     }, ( Cocoa, ))
 
 import sys
+import functools
 del sys.modules['PrintCore._metadata']
+
+#
+# PMRetain and PMRelease are "generic" functions
+# where the argument can be an instance of a number
+# of PrintCore types.
+#
+# The code below ensures these functions actually
+# work as expected.
+#
+_PMRetain = mod.PMRetain
+_PMRelease = mod.PMRelease
+
+@functools.wraps(_PMRetain)
+def PMRetain(value):
+    return _PMRetain(value.__pointer__)
+
+@functools.wraps(_PMRelease)
+def PMRelease(value):
+    return _PMRelease(value.__pointer__)
+
+mod.PMRetain = PMRetain
+mod.PMRelease = PMRelease
---
+++ pyobjc-framework-ApplicationServices/PyObjCTest/test_printcore.py
@@ -0,0 +1,20 @@
+import PrintCore
+import sys
+from PyObjCTools.TestSupport import *
+
+class TestPrintCore (TestCase):
+    def test_functional(self):
+        res, value = PrintCore.PMCreatePrintSettings(None)
+        self.assertEqual(res, 0)
+
+        self.assertIsInstance(value, PrintCore.PMPrintSettings)
+
+        PrintCore.PMRetain(value)
+        PrintCore.PMRelease(value)
+
+        # Release the final reference
+        PrintCore.PMRelease(value)
+
+
+if __name__ == "__main__":
+    main()
