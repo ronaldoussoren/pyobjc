660c487bd45817cab683b10a858f78958374b0ff
Ronald Oussoren <ronaldoussoren@mac.com>
2019-04-12 15:21:19 +0200

    Add file for google search console

--- docs/conf.py
+++ docs/conf.py
@@ -139,6 +139,7 @@ html_add_permalinks = ""
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ['_extra']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
