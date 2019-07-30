4d79b2a457839cf9e9dd2a0442fb5b8c98aab50e
Ronald Oussoren <ronaldoussoren@mac.com>
2019-07-30 02:26:18 -0700

    Fix issues on 10.15

--- pyobjc-framework-StoreKit/PyObjCTest/test_skpayment.py
+++ pyobjc-framework-StoreKit/PyObjCTest/test_skpayment.py
@@ -6,7 +6,7 @@ class TestSKPayment (TestCase):
     @min_os_level('10.14')
     def test_methods10_14(self):
         self.assertResultIsBOOL(StoreKit.SKPayment.simulatesAskToBuyInSandbox)
-        self.assertArgIsBOOL(StoreKit.SKPayment.setSimulatesAskToBuyInSandbox_, 0)
+        self.assertArgIsBOOL(StoreKit.SKMutablePayment.setSimulatesAskToBuyInSandbox_, 0)
 
 
 if __name__ == "__main__":
