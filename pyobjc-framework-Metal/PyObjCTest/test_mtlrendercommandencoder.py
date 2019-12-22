07b0227c53eb10885d433a8a7cf1217596917ccb
Ronald Oussoren <ronaldoussoren@mac.com>
2019-12-22 11:26:05 -0800

    Finish tests and metadata for the Metal bindings
    Closes #242

--- pyobjc-framework-Metal/PyObjCTest/test_mtlrendercommandencoder.py
+++ pyobjc-framework-Metal/PyObjCTest/test_mtlrendercommandencoder.py
@@ -6,6 +6,37 @@ class TestMTLRenderCommandEncoderHelper (Metal.NSObject):
     def setVertexBytes_length_atIndex_(self, a, b, c): pass
     def setVertexBuffer_offset_atIndex_(self, a, b, c): pass
     def setVertexBufferOffset_atIndex_(self, a, b): pass
+    def setVertexBuffers_offsets_withRange_(self, a, b, c): pass
+    def setVertexTexture_atIndex_(self, a, b): pass
+    def setVertexTextures_withRange_(self, a, b): pass
+    def setVertexSamplerState_atIndex_(self, a, b): pass
+    def setVertexSamplerStates_withRange_(self, a, b): pass
+    def setVertexSamplerState_lodMinClamp_lodMaxClamp_atIndex_(self, a, b, c, d): pass
+    def setVertexSamplerStates_lodMinClamps_lodMaxClamps_withRange_(self, a, b, c, d): pass
+    def setViewport_(self, a): pass
+    def setViewports_count_(self, a, b): pass
+    def setFrontFacingWinding_(self, a): pass
+    def setCullMode_(self, a): pass
+    def setDepthClipMode_(self, a): pass
+    def setDepthBias_slopeScale_clamp_(self, a, b, c): pass
+    def setScissorRect_(self, a): pass
+    def setScissorRects_count_(self, a, b): pass
+    def setTriangleFillMode_(self, a): pass
+    def setFragmentBytes_length_atIndex_(self, a, b, c): pass
+    def setFragmentBuffer_offset_atIndex_(self, a,b, c): pass
+    def setFragmentBufferOffset_atIndex_(self, a, b): pass
+    def setFragmentBuffers_offsets_withRange_(self, a, b, c): pass
+    def setFragmentTexture_atIndex_(self, a, b): pass
+    def setFragmentTextures_withRange_(self, a, b): pass
+    def useResources_count_usage_(self, a, b, c): pass
+    def useHeaps_count_(self, a, b): pass
+    def useHeap_stages_(self, a, b): pass
+    def useHeaps_count_stages_(self, a, b, c): pass
+    def executeCommandsInBuffer_withRange_(self, a, b): pass
+    def executeCommandsInBuffer_indirectBuffer_indirectBufferOffset_(self, a, b, c): pass
+    def memoryBarrierWithScope_afterStages_beforeStages_(self, a, b, c): pass
+    def memoryBarrierWithResources_count_(self, a, b): pass
+    def sampleCountersInBuffer_atSampleIndex_withBarrier_(self, a, b, c): pass
 
 class TestMTLRenderCommandEncoder (TestCase):
     def test_constants(self):
@@ -92,4 +123,111 @@ class TestMTLRenderCommandEncoder (TestCase):
 
         self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexBufferOffset_atIndex_, 0, objc._C_NSUInteger)
         self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexBufferOffset_atIndex_, 1, objc._C_NSUInteger)
-        # XXX
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexBuffers_offsets_withRange_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setVertexBuffers_offsets_withRange_, 0, 2)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexBuffers_offsets_withRange_, 1, b'n^' + objc._C_NSUInteger)
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setVertexBuffers_offsets_withRange_, 1, 2)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexBuffers_offsets_withRange_, 2, Metal.NSRange.__typestr__)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexTexture_atIndex_, 1, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexTextures_withRange_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setVertexTextures_withRange_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexTextures_withRange_, 1, Metal.NSRange.__typestr__)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerState_atIndex_, 1, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_withRange_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_withRange_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_withRange_, 1, Metal.NSRange.__typestr__)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerState_lodMinClamp_lodMaxClamp_atIndex_, 1, objc._C_FLT)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerState_lodMinClamp_lodMaxClamp_atIndex_, 2, objc._C_FLT)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerState_lodMinClamp_lodMaxClamp_atIndex_, 3, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_lodMinClamps_lodMaxClamps_withRange_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_lodMinClamps_lodMaxClamps_withRange_, 0, 3)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_lodMinClamps_lodMaxClamps_withRange_, 1, b'n^f')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_lodMinClamps_lodMaxClamps_withRange_, 1, 3)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_lodMinClamps_lodMaxClamps_withRange_, 2, b'n^f')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_lodMinClamps_lodMaxClamps_withRange_, 2, 3)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setVertexSamplerStates_lodMinClamps_lodMaxClamps_withRange_, 3, Metal.NSRange.__typestr__)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setViewport_, 0, Metal.MTLViewport.__typestr__)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setViewports_count_, 0, b'n^' + Metal.MTLViewport.__typestr__)
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setViewports_count_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setViewports_count_, 1, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFrontFacingWinding_, 0, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setCullMode_, 0, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setDepthClipMode_, 0, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setDepthBias_slopeScale_clamp_, 0, objc._C_FLT)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setDepthBias_slopeScale_clamp_, 1, objc._C_FLT)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setDepthBias_slopeScale_clamp_, 2, objc._C_FLT)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setScissorRect_, 0, Metal.MTLScissorRect.__typestr__)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setScissorRects_count_, 0, b'n^' + Metal.MTLScissorRect.__typestr__)
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setScissorRects_count_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setScissorRects_count_, 1, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setTriangleFillMode_, 0, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBytes_length_atIndex_, 0, b'n^v')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setFragmentBytes_length_atIndex_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBytes_length_atIndex_, 1, objc._C_NSUInteger)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBytes_length_atIndex_, 2, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBuffer_offset_atIndex_, 1, objc._C_NSUInteger)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBuffer_offset_atIndex_, 2, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBufferOffset_atIndex_, 0, objc._C_NSUInteger)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBufferOffset_atIndex_, 1, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBuffers_offsets_withRange_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setFragmentBuffers_offsets_withRange_, 0, 2)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBuffers_offsets_withRange_, 1, b'n^' + objc._C_NSUInteger)
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setFragmentBuffers_offsets_withRange_, 1, 2)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentBuffers_offsets_withRange_, 2, Metal.NSRange.__typestr__)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentTexture_atIndex_, 1, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentTextures_withRange_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.setFragmentTextures_withRange_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.setFragmentTextures_withRange_, 1, Metal.NSRange.__typestr__)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.useResources_count_usage_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.useResources_count_usage_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.useResources_count_usage_, 1, objc._C_NSUInteger)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.useResources_count_usage_, 2, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.useHeaps_count_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.useHeaps_count_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.useHeaps_count_, 1, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.useHeap_stages_, 1, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.useHeaps_count_stages_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.useHeaps_count_stages_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.useHeaps_count_stages_, 1, objc._C_NSUInteger)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.useHeaps_count_stages_, 2, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.executeCommandsInBuffer_withRange_, 1, Metal.NSRange.__typestr__)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.executeCommandsInBuffer_indirectBuffer_indirectBufferOffset_, 2, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.memoryBarrierWithScope_afterStages_beforeStages_, 0, objc._C_NSUInteger)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.memoryBarrierWithScope_afterStages_beforeStages_, 1, objc._C_NSUInteger)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.memoryBarrierWithScope_afterStages_beforeStages_, 2, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.memoryBarrierWithResources_count_, 0, b'n^@')
+        self.assertArgSizeInArg(TestMTLRenderCommandEncoderHelper.memoryBarrierWithResources_count_, 0, 1)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.memoryBarrierWithResources_count_, 1, objc._C_NSUInteger)
+
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.sampleCountersInBuffer_atSampleIndex_withBarrier_, 1, objc._C_NSUInteger)
+        self.assertArgHasType(TestMTLRenderCommandEncoderHelper.sampleCountersInBuffer_atSampleIndex_withBarrier_, 2, objc._C_NSBOOL)
