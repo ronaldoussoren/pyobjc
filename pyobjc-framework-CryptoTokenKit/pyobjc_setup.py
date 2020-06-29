9bd7591c192ba57cf7e341746331d8232613abaf
Ronald Oussoren <ronaldoussoren@mac.com>
2020-06-29 16:49:22 +0200

    Update shared files

--- pyobjc-framework-CryptoTokenKit/pyobjc_setup.py
+++ pyobjc-framework-CryptoTokenKit/pyobjc_setup.py
@@ -180,11 +180,7 @@ License :: OSI Approved :: MIT License
 Natural Language :: English
 Operating System :: MacOS :: MacOS X
 Programming Language :: Python
-Programming Language :: Python :: 2
-Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.4
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
