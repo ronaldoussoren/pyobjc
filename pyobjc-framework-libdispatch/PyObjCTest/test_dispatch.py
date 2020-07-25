2a4de01251eebdb7c6de362dbe4ded0f76d15bf7
Ronald Oussoren <ronaldoussoren@mac.com>
2020-07-25 21:03:16 +0200

    Update the libdispatch bindings for macOS 11

--- pyobjc-framework-libdispatch/PyObjCTest/test_dispatch.py
+++ pyobjc-framework-libdispatch/PyObjCTest/test_dispatch.py
@@ -4,4 +4,4 @@ from PyObjCTools.TestSupport import TestCase
 
 class TestDispatch(TestCase):
     def test_constants(self):
-        self.assertFalse(hasattr(libdispatch, "DISPATCH_API_VERSION"))
+        self.assertNotHasAttr(libdispatch, "DISPATCH_API_VERSION")
