c6c9acf709e1888d450477028916499188ad0975
Ronald Oussoren <ronaldoussoren@mac.com>
2021-05-20 11:14:06 +0200

    Move benchmarks into function
    This makes it easier to disable some of the
    tests while working on a particular optimization.

--- pyobjc-core/Tools/pyobjcbench.py
+++ pyobjc-core/Tools/pyobjcbench.py
@@ -20,6 +20,7 @@ PyObjC primitives:
 """
 from __future__ import print_function
 
+import textwrap
 import timeit
 
 import objc
@@ -32,57 +33,73 @@ def print_bench(title, time):
 NSObject = objc.lookUpClass("NSObject")
 NSArray = objc.lookUpClass("NSArray")
 
-print_bench(
-    "object description lookup", timeit.timeit(setup="o = object()", stmt="o.__repr__")
-)
-print_bench(
-    "NSObject description lookup",
-    timeit.timeit(
-        setup='import objc; NSObject = objc.lookUpClass("NSObject"); '
-        "o = NSObject.alloc().init()",
-        stmt="o.description",
-    ),
-)
-print_bench(
-    "NSArray description lookup",
-    timeit.timeit(
-        setup='import objc; NSArray = objc.lookUpClass("NSArray"); '
-        "o = NSArray.alloc().init()",
-        stmt="o.description",
-    ),
-)
-print()
-print_bench(
-    "object description call",
-    timeit.timeit(setup="m = object().__repr__", stmt="m()"),
-)
-print_bench(
-    "NSObject description call",
-    timeit.timeit(
-        setup='import objc; NSObject = objc.lookUpClass("NSObject"); '
-        "m = NSObject.alloc().init().description",
-        stmt="m()",
-    ),
-)
-print_bench(
-    "NSArray description call",
-    timeit.timeit(
-        setup='import objc; NSArray = objc.lookUpClass("NSArray"); '
-        "m = NSArray.alloc().init().description",
-        stmt="m()",
-    ),
-)
-print()
-print_bench(
-    "python function call",
-    timeit.timeit(setup="import math; f=math.sin", stmt="f(5.0)"),
-)
-setup = """
-import objc
-objc.loadBundleFunctions(None, globals(), [("sin", b"dd")])
-f = sin
-"""
-print_bench(
-    "objc function call",
-    timeit.timeit(setup=setup, stmt="f(5.0)"),
-)
+
+def descriptor_lookup():
+    print_bench(
+        "object description lookup",
+        timeit.timeit(setup="o = object()", stmt="o.__repr__"),
+    )
+    print_bench(
+        "NSObject description lookup",
+        timeit.timeit(
+            setup='import objc; NSObject = objc.lookUpClass("NSObject"); '
+            "o = NSObject.alloc().init()",
+            stmt="o.description",
+        ),
+    )
+    print_bench(
+        "NSArray description lookup",
+        timeit.timeit(
+            setup='import objc; NSArray = objc.lookUpClass("NSArray"); '
+            "o = NSArray.alloc().init()",
+            stmt="o.description",
+        ),
+    )
+    print()
+
+
+def descriptor_call():
+    print_bench(
+        "object description call",
+        timeit.timeit(setup="m = object().__repr__", stmt="m()"),
+    )
+    print_bench(
+        "NSObject description call",
+        timeit.timeit(
+            setup='import objc; NSObject = objc.lookUpClass("NSObject"); '
+            "m = NSObject.alloc().init().description",
+            stmt="m()",
+        ),
+    )
+    print_bench(
+        "NSArray description call",
+        timeit.timeit(
+            setup='import objc; NSArray = objc.lookUpClass("NSArray"); '
+            "m = NSArray.alloc().init().description",
+            stmt="m()",
+        ),
+    )
+    print()
+
+
+def function_call():
+    print_bench(
+        "python function call",
+        timeit.timeit(setup="import math; f=math.sin", stmt="f(5.0)"),
+    )
+    setup = textwrap.dedent(
+        """\
+    import objc
+    objc.loadBundleFunctions(None, globals(), [("sin", b"dd")])
+    f = sin
+    """
+    )
+    print_bench(
+        "objc function call",
+        timeit.timeit(setup=setup, stmt="f(5.0)"),
+    )
+
+
+descriptor_lookup()
+descriptor_call()
+function_call()
