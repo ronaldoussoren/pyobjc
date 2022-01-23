0ff9b94be8250c17c8518a0319e3558b0e2bc0a9
Ronald Oussoren <ronaldoussoren@mac.com>
2022-01-23 22:19:15 +0100

    Add test file

---
+++ pyobjc-framework-Accessibility/PyObjCTest/test_axbraillemap.py
@@ -0,0 +1,42 @@
+from PyObjCTools.TestSupport import TestCase, min_sdk_level
+
+import Accessibility
+import objc
+
+
+class TestAXBrailleMapHelper(Accessibility.NSObject):
+    def accessibilityBrailleMapRenderRegion(self):
+        return 1
+
+    def setAccessibilityBrailleMapRenderRegion_(self, a):
+        pass
+
+    def accessibilityBrailleMapRenderer(self):
+        return 1
+
+    def setAccessibilityBrailleMapRenderer_(self, a):
+        pass
+
+
+class TestAXBrailleMap(TestCase):
+    @min_sdk_level("12.1")
+    def test_protocols(self):
+        objc.protocolNamed("AXBrailleMapRenderer")
+
+    def test_methods(self):
+        self.assertResultHasType(
+            TestAXBrailleMapHelper.accessibilityBrailleMapRenderRegion,
+            Accessibility.CGRect.__typestr__,
+        )
+        self.assertArgHasType(
+            TestAXBrailleMapHelper.setAccessibilityBrailleMapRenderRegion_,
+            0,
+            Accessibility.CGRect.__typestr__,
+        )
+
+        self.assertResultIsBlock(
+            TestAXBrailleMapHelper.accessibilityBrailleMapRenderer, b"v@"
+        )
+        self.assertArgIsBlock(
+            TestAXBrailleMapHelper.setAccessibilityBrailleMapRenderer_, 0, b"v@"
+        )
