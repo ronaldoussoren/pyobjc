457f5da7e8f966a81dd67fa7c42bef9cbbe939c1
Ronald Oussoren <ronaldoussoren@mac.com>
2022-03-08 13:31:12 +0100

    Add helper script to check that LCOV pragma's are on the correct lines

---
+++ development-support/validate-lcov.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Misusage of LCOV_BR_EXCL_LINE:"
+grep LCOV_BR_EXCL_LINE Modules/objc/*.[hm] | grep -v -e 'if (' -e 'while (' -e '} else {' -e ': always non-empty'
+echo
+
+echo "Misusage of LCOV_EXCL_LINE:"
+grep LCOV_EXCL_LINE Modules/objc/*.[hm] | grep -e 'if (' -e 'while (' -e '} else {' -e ': always non-empty'
+echo
