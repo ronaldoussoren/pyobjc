08af087a2fb2bc8fb1f25a76dc2d67ae5d274c89
Ronald Oussoren <ronaldoussoren@mac.com>
2022-08-11 21:56:56 +0200

    Issue #94: Improve test coverage for SIMD support

--- pyobjc-core/PyObjCTest/test_vector_proxy.py
+++ pyobjc-core/PyObjCTest/test_vector_proxy.py
@@ -27,6 +27,12 @@ CASES = [
 ]
 
 
+class Fake:
+    @property
+    def __pyobjc_object__(self):
+        raise TypeError("Cannot proxy")
+
+
 class TestRepythonify(TestCase):
     def test_from_tuple(self):
         for simd_type, args in CASES:
@@ -167,6 +173,7 @@ objc.registerMetaDataForSelector(
 
 
 # XXX: Add test that tries to call before overriding with 'full_signature'
+# XXX: Add tests that use a class method instead of an instance method
 
 
 class TestMethods(TestCase):
@@ -188,6 +195,15 @@ class TestMethods(TestCase):
         result = oc.calcId_andFloat2_("hello", (1.5, 2.5))
         self.assertEqual(result, (("hello", simd.vector_float2(1.5, 2.5))))
 
+        with self.assertRaisesRegex(TypeError, "expected 2 arguments, got 1"):
+            oc.calcId_andFloat2_(42)
+
+        with self.assertRaisesRegex(TypeError, "must be real number, not str"):
+            oc.calcId_andFloat2_("", "aa")
+
+        with self.assertRaisesRegex(TypeError, "Cannot proxy"):
+            oc.calcId_andFloat2_(Fake(), (42.0, 42.5))
+
     def test_calcId_andFloat3(self):
         self.assertArgHasType(
             OC_Vector.calcId_andFloat3_, 1, simd.vector_float3.__typestr__
@@ -195,6 +211,15 @@ class TestMethods(TestCase):
         oc = OC_Vector.alloc().init()
         result = oc.calcId_andFloat3_("hello2", (1.5, 2.5, 3.5))
         self.assertEqual(result, (("hello2", simd.vector_float3(1.5, 2.5, 3.5))))
+
+        with self.assertRaisesRegex(TypeError, "expected 2 arguments, got 1"):
+            oc.calcId_andFloat3_(42)
+
+        with self.assertRaisesRegex(ValueError, "Expecting value with 3 elements"):
+            oc.calcId_andFloat3_("", "aa")
+
+        with self.assertRaisesRegex(TypeError, "Cannot proxy"):
+            oc.calcId_andFloat3_(Fake(), (42.0, 42.5, 99.0))
 
     def test_calcId_andFloat4(self):
         self.assertArgHasType(
@@ -205,6 +230,15 @@ class TestMethods(TestCase):
         self.assertEqual(
             result, (("hello2", simd.vector_float4(-1.5, -2.5, -3.5, -4.5)))
         )
+
+        with self.assertRaisesRegex(TypeError, "expected 2 arguments, got 1"):
+            oc.calcId_andFloat4_(42)
+
+        with self.assertRaisesRegex(ValueError, "Expecting value with 4 elements"):
+            oc.calcId_andFloat4_("", "aa")
+
+        with self.assertRaisesRegex(TypeError, "Cannot proxy"):
+            oc.calcId_andFloat4_(Fake(), (42.0, 42.5, 99.0, 99.5))
 
     def test_setVectorFloat2(self):
         self.assertArgHasType(
@@ -217,6 +251,12 @@ class TestMethods(TestCase):
         value = oc.getAndResetValues()
         self.assertEqual(value, simd.vector_float2(-1.5, -2.5))
 
+        with self.assertRaisesRegex(TypeError, "expected 1 arguments, got 2"):
+            oc.setVectorFloat2_(44, 42)
+
+        with self.assertRaisesRegex(ValueError, "Expecting value with 2 elements"):
+            oc.setVectorFloat2_("aaa")
+
     def test_setVectorFloat3(self):
         self.assertArgHasType(
             OC_Vector.setVectorFloat3_, 0, simd.vector_float3.__typestr__
@@ -227,6 +267,12 @@ class TestMethods(TestCase):
 
         value = oc.getAndResetValues()
         self.assertEqual(value, simd.vector_float3(-1.5, -2.5, 4.5))
+
+        with self.assertRaisesRegex(TypeError, "expected 1 arguments, got 2"):
+            oc.setVectorFloat3_(44, 42)
+
+        with self.assertRaisesRegex(ValueError, "Expecting value with 3 elements"):
+            oc.setVectorFloat3_("aa")
 
     def test_setVectorFloat4(self):
         self.assertArgHasType(
@@ -239,6 +285,12 @@ class TestMethods(TestCase):
         value = oc.getAndResetValues()
         self.assertEqual(value, simd.vector_float4(-1.5, -2.5, 4.5, 99.5))
 
+        with self.assertRaisesRegex(TypeError, "expected 1 arguments, got 2"):
+            oc.setVectorFloat4_(44, 42)
+
+        with self.assertRaisesRegex(ValueError, "Expecting value with 4 elements"):
+            oc.setVectorFloat4_("aa")
+
     def test_getVectorUChar16(self):
         self.assertResultHasType(
             OC_Vector.getVectorUChar16, simd.vector_uchar16.__typestr__
@@ -250,6 +302,9 @@ class TestMethods(TestCase):
             simd.vector_uchar16(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 14, 16),
         )
 
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getVectorUChar16(44, 42)
+
     def test_getVectorDouble2(self):
         self.assertResultHasType(
             OC_Vector.getVectorDouble2, simd.vector_double2.__typestr__
@@ -257,6 +312,9 @@ class TestMethods(TestCase):
         oc = OC_Vector.alloc().init()
         result = oc.getVectorDouble2()
         self.assertEqual(result, simd.vector_double2(-42.9, 42.8))
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getVectorDouble2(44, 42)
 
     def test_getVectorFloat2(self):
         self.assertResultHasType(
@@ -266,6 +324,9 @@ class TestMethods(TestCase):
         result = oc.getVectorFloat2()
         self.assertEqual(result, simd.vector_float2(-9.5, 10.5))
 
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getVectorFloat2(44, 42)
+
     def test_getVectorFloat3(self):
         self.assertResultHasType(
             OC_Vector.getVectorFloat3, simd.vector_float3.__typestr__
@@ -273,6 +334,9 @@ class TestMethods(TestCase):
         oc = OC_Vector.alloc().init()
         result = oc.getVectorFloat3()
         self.assertEqual(result, simd.vector_float3(-8.5, 9.5, 12.5))
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getVectorFloat3(44, 42)
 
     def test_getVectorFloat4(self):
         self.assertResultHasType(
@@ -282,11 +346,17 @@ class TestMethods(TestCase):
         result = oc.getVectorFloat4()
         self.assertEqual(result, simd.vector_float4(-7.5, 13.5, 14.5, 16.5))
 
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getVectorFloat4(44, 42)
+
     def test_getVectorInt2(self):
         self.assertResultHasType(OC_Vector.getVectorInt2, simd.vector_int2.__typestr__)
         oc = OC_Vector.alloc().init()
         result = oc.getVectorInt2()
         self.assertEqual(result, simd.vector_int2(42, 43))
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getVectorInt2(44, 42)
 
     def test_getGKBox(self):
         self.assertResultHasType(OC_Vector.getGKBox, GKBox.__typestr__)
@@ -297,6 +367,9 @@ class TestMethods(TestCase):
             GKBox(simd.vector_float3(1.5, 2.5, 3.5), simd.vector_float3(4.5, 5.5, 6.5)),
         )
 
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getGKBox(44, 42)
+
     def test_getGKQuad(self):
         self.assertResultHasType(OC_Vector.getGKQuad, GKQuad.__typestr__)
         oc = OC_Vector.alloc().init()
@@ -304,6 +377,9 @@ class TestMethods(TestCase):
         self.assertEqual(
             result, GKQuad(simd.vector_float2(7.5, 8.5), simd.vector_float2(9.5, 10.5))
         )
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getGKQuad(44, 42)
 
     def test_getMDLAxisAlignedBoundingBox(self):
         self.assertResultHasType(
@@ -320,6 +396,9 @@ class TestMethods(TestCase):
             ),
         )
 
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getMDLAxisAlignedBoundingBox(44, 42)
+
     def test_getMDLVoxelIndexExtent(self):
         self.assertResultHasType(
             OC_Vector.getMDLVoxelIndexExtent, MDLVoxelIndexExtent.__typestr__
@@ -332,6 +411,9 @@ class TestMethods(TestCase):
                 simd.vector_int4(-1, -2, -3, -4), simd.vector_int4(-5, -6, -7, -8)
             ),
         )
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getMDLVoxelIndexExtent(44, 42)
 
     def test_getMPSAxisAlignedBoundingBox(self):
         self.assertResultHasType(
@@ -347,6 +429,9 @@ class TestMethods(TestCase):
                 simd.vector_float3(-5.5, -6.5, -7.5),
             ),
         )
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getMPSAxisAlignedBoundingBox(44, 42)
 
     def test_getMPSImageHistogramInfo(self):
         self.assertResultHasType(
@@ -364,6 +449,9 @@ class TestMethods(TestCase):
             ),
         )
 
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getMPSImageHistogramInfo(44, 42)
+
     def test_getMatrixDouble4x4(self):
         self.assertResultHasType(
             OC_Vector.getMatrixDouble4x4, simd.matrix_double4x4.__typestr__
@@ -373,14 +461,17 @@ class TestMethods(TestCase):
         self.assertEqual(
             result,
             simd.matrix_double4x4(
-                [
-                    simd.vector_double4(-21.5, -22.5, -23.5, 0),
+                (
+                    simd.vector_double4(-20.5, -21.5, -22.5, -23.5),
                     simd.vector_double4(-30.5, -31.5, -32.5, -33.5),
                     simd.vector_double4(-40.5, -41.5, -42.5, -43.5),
                     simd.vector_double4(-50.5, -51.5, -52.5, -53.5),
-                ]
+                )
             ),
         )
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getMatrixDouble4x4(44, 42)
 
     def test_getMatrixFloat2x2(self):
         self.assertResultHasType(
@@ -391,12 +482,15 @@ class TestMethods(TestCase):
         self.assertEqual(
             result,
             simd.matrix_float2x2(
-                [
+                (
                     simd.vector_float2(-20.5, -21.5),
                     simd.vector_float2(-30.5, -31.5),
-                ]
+                )
             ),
         )
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getMatrixFloat2x2(44, 42)
 
     def test_getMatrixFloat3x3(self):
         self.assertResultHasType(
@@ -407,13 +501,16 @@ class TestMethods(TestCase):
         self.assertEqual(
             result,
             simd.matrix_float3x3(
-                [
+                (
                     simd.vector_float3(-120.5, -121.5, -122.5),
                     simd.vector_float3(-130.5, -131.5, -132.5),
                     simd.vector_float3(-140.5, -141.5, -142.5),
-                ]
+                )
             ),
         )
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getMatrixFloat3x3(44, 42)
 
     def test_getMatrixFloat4x4(self):
         self.assertResultHasType(
@@ -424,14 +521,17 @@ class TestMethods(TestCase):
         self.assertEqual(
             result,
             simd.matrix_float4x4(
-                [
-                    simd.vector_float4(-220.5, -221.5, -222.5, 0),
-                    simd.vector_float4(-230.5, -231.5, -232.5, 0),
-                    simd.vector_float4(-240.5, -241.5, -242.5, 0),
-                    simd.vector_float4(-250.5, -251.5, -252.5, 0),
-                ]
+                (
+                    simd.vector_float4(-220.5, -221.5, -222.5, 10.5),
+                    simd.vector_float4(-230.5, -231.5, -232.5, 11.5),
+                    simd.vector_float4(-240.5, -241.5, -242.5, 12.5),
+                    simd.vector_float4(-250.5, -251.5, -252.5, 13.5),
+                )
             ),
         )
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getMatrixFloat4x4(44, 42)
 
     def test_getSimdFloat4x4(self):
         self.assertResultHasType(
@@ -442,14 +542,17 @@ class TestMethods(TestCase):
         self.assertEqual(
             result,
             simd.simd_float4x4(
-                [
-                    simd.vector_float4(-320.5, -321.5, -322.5, 0),
-                    simd.vector_float4(-330.5, -331.5, -332.5, 0),
-                    simd.vector_float4(-340.5, -341.5, -342.5, 0),
-                    simd.vector_float4(-350.5, -351.5, -352.5, 0),
-                ]
+                (
+                    simd.vector_float4(-320.5, -321.5, -322.5, 1.5),
+                    simd.vector_float4(-330.5, -331.5, -332.5, 2.5),
+                    simd.vector_float4(-340.5, -341.5, -342.5, 3.5),
+                    simd.vector_float4(-350.5, -351.5, -352.5, 4.5),
+                )
             ),
         )
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getSimdFloat4x4(44, 42)
 
     def test_getSimdQuatf(self):
         self.assertResultHasType(OC_Vector.getSimdQuatf, simd.simd_quatf.__typestr__)
@@ -458,6 +561,9 @@ class TestMethods(TestCase):
         self.assertEqual(
             result, simd.simd_quatf(simd.vector_float4(-420.5, -421.5, -422.5, 0))
         )
+
+        with self.assertRaisesRegex(TypeError, "expected no arguments, got 2"):
+            oc.getSimdQuatf(44, 42)
 
 
 class TestIMP(TestCase):
