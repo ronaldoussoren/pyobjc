3dd276356f46a4d8509a380ffe652ac7c1b58686
Ronald Oussoren <ronaldoussoren@mac.com>
2023-02-11 13:35:45 +0100

    Validate lcov annotations at check-in
    Fixes #454

--- development-support/validate-lcov.sh
+++ development-support/validate-lcov.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+set -o pipefail
+
 echo "Misusage of LCOV_BR_EXCL_LINE:"
-grep LCOV_BR_EXCL_LINE */Modules/objc/*.[hm] | grep -v -e 'if (' -e 'while (' -e '} else {' -e ': always non-empty' -e 'switch ('
+grep -n LCOV_BR_EXCL_LINE */Modules/objc/*.[hm] | grep -v -e 'if (' -e 'while (' -e '} else {' -e ': always non-empty' -e 'switch ('
+xit1=$?
 echo
 
 echo "Misusage of LCOV_EXCL_LINE:"
-grep LCOV_EXCL_LINE */Modules/objc/*.[hm] | grep -e 'if (' -e 'while (' -e '} else {' -e ': always non-empty' -e 'switch ('
+grep -n LCOV_EXCL_LINE */Modules/objc/*.[hm] | grep -e 'if (' -e 'while (' -e '} else {' -e ': always non-empty' -e 'switch ('
+xit2=$?
 echo
+
+exit $(expr 2 - $xit1 - $xit2)
