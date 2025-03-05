141a80e9211a1e0728772bf31e182f49a09ce7d6
Ronald Oussoren <ronaldoussoren@mac.com>
2025-03-05 22:47:09 +0100

    Fix a number of build failures on macOS 15.4

--- pyobjc-framework-FSKit/Modules/_FSKit_protocols.m
+++ pyobjc-framework-FSKit/Modules/_FSKit_protocols.m
@@ -7,19 +7,19 @@
 static void __attribute__((__used__)) use_protocols(void)
 {
     PyObject* p __attribute__((__unused__));
-#if PyObjC_BUILD_RELEASE >= 1501
-    p = PyObjC_IdToPython(@protocol(FSBlockDeviceOperations)); Py_XDECREF(p);
+#if PyObjC_BUILD_RELEASE >= 1504
     p = PyObjC_IdToPython(@protocol(FSFileSystemBase)); Py_XDECREF(p);
     p = PyObjC_IdToPython(@protocol(FSManageableResourceMaintenanceOperations)); Py_XDECREF(p);
     p = PyObjC_IdToPython(@protocol(FSUnaryFileSystemOperations)); Py_XDECREF(p);
-    p = PyObjC_IdToPython(@protocol(FSVolumeAccessCheckOperations)); Py_XDECREF(p);
-    p = PyObjC_IdToPython(@protocol(FSVolumeKernelOffloadedIOOperations)); Py_XDECREF(p);
-    p = PyObjC_IdToPython(@protocol(FSVolumeOpenCloseOperations)); Py_XDECREF(p);
-    p = PyObjC_IdToPython(@protocol(FSVolumeOperations)); Py_XDECREF(p);
     p = PyObjC_IdToPython(@protocol(FSVolumePathConfOperations)); Py_XDECREF(p);
-    p = PyObjC_IdToPython(@protocol(FSVolumePreallocateOperations)); Py_XDECREF(p);
-    p = PyObjC_IdToPython(@protocol(FSVolumeReadWriteOperations)); Py_XDECREF(p);
-    p = PyObjC_IdToPython(@protocol(FSVolumeRenameOperations)); Py_XDECREF(p);
+    p = PyObjC_IdToPython(@protocol(FSVolumeOperations)); Py_XDECREF(p);
     p = PyObjC_IdToPython(@protocol(FSVolumeXattrOperations)); Py_XDECREF(p);
+    p = PyObjC_IdToPython(@protocol(FSVolumeOpenCloseOperations)); Py_XDECREF(p);
+    p = PyObjC_IdToPython(@protocol(FSVolumeReadWriteOperations)); Py_XDECREF(p);
+    p = PyObjC_IdToPython(@protocol(FSVolumeAccessCheckOperations)); Py_XDECREF(p);
+    p = PyObjC_IdToPython(@protocol(FSVolumeRenameOperations)); Py_XDECREF(p);
+    p = PyObjC_IdToPython(@protocol(FSVolumePreallocateOperations)); Py_XDECREF(p);
+    p = PyObjC_IdToPython(@protocol(FSVolumeItemDeactivation)); Py_XDECREF(p);
+    p = PyObjC_IdToPython(@protocol(FSVolumeKernelOffloadedIOOperations)); Py_XDECREF(p);
 #endif /* PyObjC_BUILD_RELEASE >= 1501 */
 }
