e9612fcf5d53b7168edbf415dad8eae383a766e5
Ronald Oussoren <ronaldoussoren@mac.com>
2025-11-14 16:10:56 +0100

    Set version to 12.2

--- pyobjc-framework-GameKit/setup.py
+++ pyobjc-framework-GameKit/setup.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from pyobjc_setup import Extension, setup  # noqa: E402
 
-VERSION = "12.1"
+VERSION = "12.2"
 
 setup(
     name="pyobjc-framework-GameKit",
