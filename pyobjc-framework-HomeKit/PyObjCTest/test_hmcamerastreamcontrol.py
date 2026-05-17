c804ff4e8c0879b1f5d18a8fb496d0ec3750ad54
Ronald Oussoren <ronaldoussoren@mac.com>
2026-05-17 11:55:38 +0200

    Revert addition of HomeKit bindings
    Issue #501

--- pyobjc-framework-HomeKit/PyObjCTest/test_hmcamerastreamcontrol.py
+++
@@ -1,9 +0,0 @@
-from PyObjCTools.TestSupport import TestCase, expectedFailure
-
-import HomeKit  # noqa: F401
-
-
-class TestHMCameraStreamControl(TestCase):
-    @expectedFailure
-    def test_protocols(self):
-        self.assertProtocolExists("HMCameraStreamControlDelegate")
--- pyobjc-framework-HomeKit/PyObjCTest/test_hmhomemanager.py
+++
@@ -1,8 +0,0 @@
-from PyObjCTools.TestSupport import TestCase
-
-import HomeKit  # noqa: F401
-
-
-class TestHMHomeManager(TestCase):
-    def test_protocols(self):
-        self.assertProtocolExists("HMHomeManagerDelegate")
