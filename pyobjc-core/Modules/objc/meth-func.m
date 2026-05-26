ronaldoussoren/pyobjc

commit 925338d7288f1ea721cef86e7a53e90e48209585

remove `dis` dependency (#669)

pyobjc-core/Modules/objc/meth-func.m
@@ -60,11 +60,19 @@ PyCodeObject* _Nullable PyObjC_get_code(PyObject* value)
 }
 
 /*
- * XXX: This duplicates code in Lib/objc/_transform.py!
- */
+  Return true if `value` explicitly returns a value.
+*/
 bool
 PyObjC_returns_value(PyObject* value)
 {
+    // This will return False for functions where all explicit
+    // returns are of the form "return" or "return None". The
+    // latter is a false negative, but cannot be avoided with
+    // bytecode inspection.
+    //
+    // Until Python 3.14 constant 0 was always None, due to
+    // changes in the bytecode compiler that's no longer true.
+
     bool      rv = false;
     Py_buffer buf;
 

pyobjc-core/Lib/objc/_transform.py
@@ -10,7 +10,6 @@ performs checks that are needed to avoid crashes.
 import objc
 import types
 import inspect
-import dis
 import keyword
 import warnings
 import sys
@@ -449,7 +448,7 @@ def transformAttribute(name, value, class_object, protocols):
     if signature is None:
         # Calculate a default signature based on the selector shape
         if isinstance(value, (types.FunctionType, types.MethodType)):
-            returns_object = returns_value(value)
+            returns_object = objc._returns_value(value)
         else:
             returns_object = True
 
@@ -572,48 +571,6 @@ def is_generator_or_async(value):
         return True
     elif inspect.isgenerator(value) or inspect.isgeneratorfunction(value):
         return True
-    return False
-
-
-def returns_value(func):
-    """
-    Return True if 'func' explicitly returns a value
-
-    """
-    # This will return False for functions where all explicit
-    # returns are of the form "return" or "return None". The
-    # latter is a false negative, but cannot be avoided with
-    # bytecode inspection.
-    # XXX: This will give a false positive for functions
-    #      that only contain "return None" paths for
-    #      returning a value.
-    #
-    # Until Python 3.14 constant 0 was always None, due to
-    # changes in the bytecode compiler that's no longer true.
-
-    if not isinstance(func.__code__, types.CodeType):
-        return True
-
-    prev = None
-    consts = func.__code__.co_consts
-
-    for inst in dis.get_instructions(func):
-        if inst.opname == "RETURN_VALUE":
-            assert prev is not None
-            if prev.opname == "LOAD_COMMON_CONSTANT" and prev.argval is not None:
-                return True
-            elif prev.opname == "LOAD_CONST" and consts[prev.arg] is not None:
-                return True
-            elif prev.opname not in ("LOAD_CONST", "LOAD_COMMON_CONSTANT"):
-                return True
-
-        elif (  # pragma: no branch
-            inst.opname == "RETURN_CONST" and consts[inst.arg] is not None
-        ):
-            # New in Python 3.12, dropped in 3.14
-            return True  # pragma: no cover
-        prev = inst
-
     return False
 
 

pyobjc-core/Modules/objc/module.m
@@ -1866,6 +1866,13 @@ static PyObject* _Nullable mod_registeredMetadataForSelector(PyObject* _Nullable
     return PyObjCMethodSignature_AsDict(sig);
 }
 
+static PyObject* _Nullable mod_returns_value(PyObject* _Nullable mod
+                                             __attribute__((__unused__)),
+                                             PyObject* value)
+{
+    return PyBool_FromLong(PyObjC_returns_value(value));
+}
+
 static PyMethodDef mod_methods[] = {
     {
         .ml_name  = "propertiesForClass",
@@ -2086,6 +2093,13 @@ static PyMethodDef mod_methods[] = {
         .ml_flags = METH_VARARGS,
         .ml_doc   = "_registeredMetadataForSelector(cls, selname)\n" CLINIC_SEP
                   "\nLook up registered metadata info for a selector.",
+    },
+    {
+        .ml_name  = "_returns_value",
+        .ml_meth  = (PyCFunction)mod_returns_value,
+        .ml_flags = METH_O,
+        .ml_doc   = "_returns_value(func)\n" CLINIC_SEP
+                  "\nReturns True if `func` explicitly returns a value.",
     },
     {
         .ml_name = NULL /* SENTINEL */

commit 60d6388fe671575526cb802f9c5d4e2bd6b6b9a9

Fix regression in detection of 'void' methods
An unintended regression from #669. This exposed a bug in the
C implementation of the functionality. The code works with
objects that are ``isinstance(..., FunctionType)``, but contained
a ``PyFunction_Check`` call to check this and that behaves slightly
different from the ``isinstance`` check.

pyobjc-core/Modules/objc/meth-func.m
@@ -76,7 +76,7 @@ PyObjC_returns_value(PyObject* value)
     bool      rv = false;
     Py_buffer buf;
 
-    if (!PyFunction_Check(value) && !PyMethod_Check(value)) {
+    if (!PyObjC_is_pyfunction(value) && !PyObjC_is_pymethod(value)) {
         return true;
     }
 

pyobjc-core/PyObjCTest/test_internals.py
@@ -64,7 +64,7 @@ class TestMetadataRegistry(TestCase):
             del result["foo"]
             self.assertEqual(extra, result)
 
-            result[next(iter(result.keys()))].append("hello")
+            result["foo"] = 1
 
             self.assertNotEqual(extra, result)
 